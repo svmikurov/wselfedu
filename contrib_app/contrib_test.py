05d53fd157975796256a97e05c983be9fb3a8dd6
Sergei Mikurov <454004@mail.ru>
2023-12-15 14:04:06 +0500

    Fix contrib_app/contrib_test.py

--- contrib_app/contrib_test.py
+++ contrib_app/contrib_test.py
@@ -5,4 +5,4 @@ def flash_message_test(response, expected_message):
     number_message = 1
     current_message = get_messages(response.wsgi_request)
     assert len(current_message) == number_message
-    assert expected_message == current_message
+    assert str(*current_message) == expected_message
