6aa39375bc94e110713bdd085c9673d24407c2f1
Sergei Mikurov <454004@mail.ru>
2023-12-26 20:31:49 +0500

    Fix allowed chapters if user is admin

--- english/views/home.py
+++ english/views/home.py
@@ -4,33 +4,26 @@ from django.views.generic import TemplateView
 class HomeEnglishView(TemplateView):
     """
     Home page for the subject "English".
+
     Navigation on the page through theoretical material and tasks
     is transmitted using dictionaries.
     The dictionary contains the name of the item and a link to it.
     """
 
-    # Theoretical Chapters
-    chapters = {
+    users_chapters = {
         'Словарь': 'eng:word_list',
     }
-
-    # Solutions
+    admin_chapters = {
+        'Добавить слово': 'eng:word_create',
+    }
     solutions = {
         'Повторить слова': 'eng:start_repetition',
-        # 'Тест': '#',
     }
 
-    extra_context = {
-        'title': 'Английский язык',
-        'chapters': chapters,
-        'solutions': solutions,
-    }
-
-    def add_for_admin(self, request, kwargs):
-        """Enable chapters if user is an admin"""
-        if request.user.is_superuser:
-            self.chapters.update(
-                {
-                    'Добавить слово': 'eng:word_create',
-                }
-            )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['chapters'] = self.users_chapters
+        context['solutions'] = self.solutions
+        if self.request.user.is_superuser:
+            context['chapters'].update(self.admin_chapters)
+        return context
