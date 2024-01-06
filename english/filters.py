1f93d1eb77585fee8b79f84f6142132cccbf9141
Sergei Mikurov <454004@mail.ru>
2024-01-06 05:56:49 +0500

    Add to words filter filtered_word_count

--- english/filters.py
+++ english/filters.py
@@ -3,10 +3,21 @@ import django_filters
 from english.models import (
     CategoryModel,
     SourceModel,
+    WordModel, WordUserKnowledgeRelation,
 )
+from users.models import UserModel
 
 
 class WordsFilter(django_filters.FilterSet):
+    """Words filter"""
+
+    WORD_COUNT = (
+        ('OW', 'Слово'),
+        ('CB', 'Словосочетание'),
+        ('PS', 'Часть предложения'),
+        ('ST', 'Предложение'),
+    )
+
     filtered_category = django_filters.ModelChoiceFilter(
         queryset=CategoryModel.objects.all(),
         field_name='category',
@@ -21,3 +32,23 @@ class WordsFilter(django_filters.FilterSet):
         label='',
         empty_label='Источник',
     )
+    filtered_word_count = django_filters.ChoiceFilter(
+        choices=WORD_COUNT,
+        lookup_expr='icontains',
+        field_name='word_count',
+        label='',
+        empty_label='Любое кол-во слов',
+    )
+    # filtered_knowledge_assessment = django_filters.ChoiceFilter(
+    #     field_name='knowledge_assessment',
+    #     method='get_knowledge_assessment_by_user',
+    #     label='',
+    #     empty_label='Оценка',
+    # )
+    #
+    # def get_knowledge_assessment_by_user(self, request, *args, **kwargs):
+    #     """Получи список оценок знания слов текущего пользователя."""
+    #     queryset = WordUserKnowledgeRelation.objects.filter(
+    #         user=UserModel.objects.get(pk=request.user.pk),
+    #     )
+    #     return queryset
