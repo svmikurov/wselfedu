89d9689f5c937cb4a7e463cb20b3977e6fa3391b
Sergei Mikurov <454004@mail.ru>
2024-02-09 11:45:43 +0500

    Add save_lookup_parameters()

--- english/services/serve_request.py
+++ english/services/serve_request.py
@@ -2,16 +2,21 @@ from english.services.serve_query import create_lookup_parameters
 from english.services.serve_task_data import TaskData
 
 
+def save_lookup_parameters(lookup_parameters) -> None:
+    """Сохрани в класс TaskData параметры поиска слова для упражнения."""
+    task_data = TaskData
+    task_data.lookup_parameters = lookup_parameters
+
+
 def set_lookup_parameters(request) -> None:
-    """Сохрани в класс TaskData параметры поиска слова для упражнения.
+    """Установи параметры поиска слова для упражнения.
 
     Создаст параметры фильтрации для поиска.
     Сохранит параметры фильтрации в класс TaskData.
     """
     querydict = dict(request.GET.lists())
     lookup_parameters = create_lookup_parameters(querydict)
-    task_data = TaskData
-    task_data.lookup_parameters = lookup_parameters
+    save_lookup_parameters(lookup_parameters)
 
 
 def get_lookup_parameters() -> dict:
