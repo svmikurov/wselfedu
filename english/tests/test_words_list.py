67fbede1a4e7d8223f561a577dbaccd91e2879bf
Sergei Mikurov <454004@mail.ru>
2024-02-12 20:52:04 +0500

    Add TestWordListView

--- english/tests/test_words_list.py
+++ english/tests/test_words_list.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from english.views import WordListView
 
 
-class TestWordListPage(TestCase):
+class TestWordListView(TestCase):
 
     fixtures = ['english/tests/fixtures/wse-fixtures.json']
 
