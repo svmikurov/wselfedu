134a843c0d70d6e70db3808dba714d6b6db859d5
Sergei Mikurov <454004@mail.ru>
2024-02-22 12:42:18 +0500

    Fix settings, db name

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'db-wse.sqlite3',
         }
     }
 # End Database
