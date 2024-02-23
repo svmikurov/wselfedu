2e56c60ec82d3ed8c40a81a222c25a8120ea7f30
Sergei Mikurov <454004@mail.ru>
2024-02-24 00:29:50 +0500

    Edit english/tests/test_account_word.py

--- english/tests/test_account_word.py
+++ english/tests/test_account_word.py
@@ -3,11 +3,11 @@ from django.urls import reverse_lazy
 
 from users.models import UserModel
 
-WORDS_URL_NAME = 'english:users_words'
+WORD_LIST_URL_NAME = 'english:users_words'
 
 
 class TestAccountWordView(TestCase):
-    """Тест таблицы слов зарегистрированного пользователя."""
+    """Test the account owner's list of words."""
 
     fixtures = ['english/tests/fixtures/wse-fixtures-2.json']
 
@@ -22,21 +22,23 @@ class TestAccountWordView(TestCase):
         """Test users words list view by owner account, page status 200."""
         self.client.force_login(self.user_owner)
         response = self.client.get(reverse_lazy(
-            WORDS_URL_NAME, kwargs={'pk': self.user_owner.pk}))
+            WORD_LIST_URL_NAME, kwargs={'pk': self.user_owner.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_page_status_by_not_owner_account(self):
         """Test users words list view by not owner account, page status 403."""
         self.client.force_login(self.user_not_owner)
         response = self.client.get(reverse_lazy(
-            WORDS_URL_NAME, kwargs={'pk': self.user_owner.pk}))
+            WORD_LIST_URL_NAME, kwargs={'pk': self.user_owner.pk}))
         self.assertEqual(response.status_code, 403)
 
     def test_page_status_by_visitor(self):
         """Test users words list view by not logged-in user, page status 302.
         """
-        # Доработать тест и представление, сейчас тестируется только защита от
-        # неправомерного доступа.
         response = self.client.get(reverse_lazy(
-            WORDS_URL_NAME, kwargs={'pk': self.user_owner.pk}))
+            WORD_LIST_URL_NAME, kwargs={'pk': self.user_owner.pk}))
         self.assertEqual(response.status_code, 302)
+
+    def test_logged_in_table_field_value(self):
+        """Test the logged-in user table field value."""
+        ...
