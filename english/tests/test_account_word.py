3ed9fa2682efd0ba3bafe348b8e909d712a2a38a
Sergei Mikurov <454004@mail.ru>
2024-02-25 00:10:55 +0500

    Update test_account_word.py

--- english/tests/test_account_word.py
+++ english/tests/test_account_word.py
@@ -1,15 +1,24 @@
+"""
+Registered user data confidentiality test module.
+Does not test application presentation to not logged-in user.
+"""
+
 from django.test import Client, TestCase
 from django.urls import reverse_lazy
 
+from english.models import WordModel
 from users.models import UserModel
 
-WORD_LIST_URL_NAME = 'english:users_words'
+WORD_LIST_PATH = 'english:users_words'
+"""Word list path name.
+"""
 
 
-class TestAccountWordView(TestCase):
-    """Test the account owner's list of words."""
+class TestUserWordListPageStatus(TestCase):
+    """Test user word list page status."""
 
     fixtures = ['english/tests/fixtures/wse-fixtures-2.json']
+    TestCase.maxDiff = None
 
     def setUp(self):
         self.client = Client()
@@ -22,23 +31,39 @@ class TestAccountWordView(TestCase):
         """Test users words list view by owner account, page status 200."""
         self.client.force_login(self.user_owner)
         response = self.client.get(reverse_lazy(
-            WORD_LIST_URL_NAME, kwargs={'pk': self.user_owner.pk}))
+            WORD_LIST_PATH, kwargs={'pk': self.user_owner.pk}))
         self.assertEqual(response.status_code, 200)
 
     def test_page_status_by_not_owner_account(self):
         """Test users words list view by not owner account, page status 403."""
         self.client.force_login(self.user_not_owner)
         response = self.client.get(reverse_lazy(
-            WORD_LIST_URL_NAME, kwargs={'pk': self.user_owner.pk}))
+            WORD_LIST_PATH, kwargs={'pk': self.user_owner.pk}))
         self.assertEqual(response.status_code, 403)
 
     def test_page_status_by_visitor(self):
         """Test users words list view by not logged-in user, page status 302.
         """
         response = self.client.get(reverse_lazy(
-            WORD_LIST_URL_NAME, kwargs={'pk': self.user_owner.pk}))
+            WORD_LIST_PATH, kwargs={'pk': self.user_owner.pk}))
         self.assertEqual(response.status_code, 302)
 
-    def test_logged_in_table_field_value(self):
-        """Test the logged-in user table field value."""
-        ...
+
+class TestShowOnlyUserData(TestCase):
+    """Test the user word list confidentiality."""
+
+    fixtures = ['english/tests/fixtures/wse-fixtures-2.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.user_id = 2
+        self.url = reverse_lazy(WORD_LIST_PATH, kwargs={'pk': self.user_id})
+        self.user = UserModel.objects.get(pk=self.user_id)
+
+    def test_show_only_user_word_list(self):
+        """Test show only user word list on page."""
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        current_queryset = response.context.get('words')
+        expected_queryset = WordModel.objects.filter(user_id=self.user_id)
+        self.assertQuerySetEqual(current_queryset, expected_queryset)
