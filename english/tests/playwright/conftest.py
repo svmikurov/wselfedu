d70bb65194f4beffb0141f5dd84e9115eae3baed
Sergei Mikurov <454004@mail.ru>
2024-02-26 16:06:44 +0500

    Edit english/tests/playwright/conftest.py

--- english/tests/playwright/conftest.py
+++ english/tests/playwright/conftest.py
@@ -5,6 +5,13 @@ from django.core.management import call_command
 
 from config import settings
 
+TEST_DB_NAME = 'db-wse-pytest.sqlite3'
+"""Date base name used by pytest.
+"""
+FIXTURE_PATH = 'english/tests/fixtures/wse-fixtures-2.json'
+"""Path to fixture for populate test date base.
+"""
+
 
 # https://pytest-django.readthedocs.io/en/latest/database.html#django-db-modify-db-settings
 def django_db_modify_db_settings():
@@ -14,7 +21,7 @@ def django_db_modify_db_settings():
     """
     settings.DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(settings.BASE_DIR, 'db-wse-pytest.sqlite3')
+        'NAME': os.path.join(settings.BASE_DIR, TEST_DB_NAME)
     }
 
 
@@ -24,4 +31,4 @@ def django_db_modify_db_settings():
 def django_db_setup(django_db_setup, django_db_blocker):
     """Populate the test database from fixtures."""
     with django_db_blocker.unblock():
-        call_command('loaddata', 'english/tests/fixtures/wse-fixtures-2.json')
+        call_command('loaddata', FIXTURE_PATH)
