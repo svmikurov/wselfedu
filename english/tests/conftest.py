6a52c7d519414c962f881c9d1e962acd5f66487b
Sergei Mikurov <454004@mail.ru>
2024-02-28 13:16:25 +0500

    Move tests

--- english/tests/conftest.py
+++ english/tests/conftest.py
@@ -1,16 +1,26 @@
 import os
+from urllib.parse import urljoin
 
 import pytest
 from django.core.management import call_command
+from dotenv import load_dotenv
+from playwright.sync_api import Page, expect
 
 from config import settings
+
+load_dotenv()
 
 TEST_DB_NAME = 'db-wse-pytest.sqlite3'
 """Date base name used by pytest.
 """
 FIXTURE_PATH = 'english/tests/fixtures/wse-fixtures-2.json'
-"""Path to fixture for populate test date base.
-"""
+COOKIES_PATH = 'english/tests/playwright/.auth/cookies.json'
+STATE_PATH = 'english/tests/playwright/.auth/user02_state.json'
+BASE_URL = '127.0.0.1:8000'
+AUTH_PATH = 'users/login/'
+
+TEST_USER_NAME = os.getenv('TEST_USER_NAME')
+TEST_PASSWORD = os.getenv('TEST_PASSWORD')
 
 
 # https://pytest-django.readthedocs.io/en/latest/database.html#django-db-modify-db-settings
@@ -32,3 +42,20 @@ def django_db_setup(django_db_setup, django_db_blocker):
     """Populate the test database from fixtures."""
     with django_db_blocker.unblock():
         call_command('loaddata', FIXTURE_PATH)
+
+
+@pytest.fixture(scope='function')
+def auth_home_page(page: Page, live_server):
+    page.goto(urljoin(live_server.url, AUTH_PATH))
+    # fill form
+    page.get_by_placeholder('Имя пользователя').fill(TEST_USER_NAME)
+    page.get_by_placeholder('Пароль').fill(TEST_PASSWORD)
+    page.get_by_role('button', name='Войти').click()
+    expect(page.get_by_text('Домашняя страница')).to_be_visible()
+    return page
+
+
+@pytest.mark.browser_context_args(storage_state=STATE_PATH)
+def test_state(page: Page):
+    page.goto(BASE_URL)
+    expect(page.get_by_role('link', name='ВыЙтИ')).to_be_visible()
--- english/tests/playwright/test_account_page.py
+++ english/tests/playwright/test_account_page.py
@@ -5,13 +5,7 @@ from playwright.sync_api import Page, expect
 USER_ID = 2
 
 
-def get_account_path():
-    return f'users/{USER_ID}/account/'
-
-
 def test_account_page_status(page: Page, live_server):
-    url = urljoin(live_server.url, get_account_path())
+    url = urljoin(live_server.url, f'users/{USER_ID}/account/')
     response = page.request.get(url)
     expect(response).to_be_ok()
-
-    page.goto(url)
