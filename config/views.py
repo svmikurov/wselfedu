feeeed2a550a5ab02805092defca81b8a8a8d788
Sergei Mikurov <454004@mail.ru>
2024-02-28 23:51:18 +0500

    Fix display of the title on the home page

--- config/views.py
+++ config/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    extra_context = {
+        'title': 'WSE: Домашняя страница',
+    }
