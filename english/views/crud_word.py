ce703cdcaa85abb7c765ee8c84e826ba9615ed04
Sergei Mikurov <454004@mail.ru>
2024-03-10 14:51:29 +0500

    Revert "Add `row_number` to instance of word list."
    Beget host do not allows mysql 8 and do not allows window expressions.
    
    This reverts commit 0ae94110afbde29c84f8ed56731a25ff87b87565.

--- english/views/crud_word.py
+++ english/views/crud_word.py
@@ -1,5 +1,3 @@
-from django.db.models import Window, F
-from django.db.models.functions import RowNumber
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 from django_filters.views import FilterView
@@ -89,11 +87,7 @@ class WordListView(CheckLoginPermissionMixin, FilterView):
     }
 
     def get_queryset(self):
-        """Get a queryset with only user words.
-
-        Add `row_number` to instance of queryset.
-        Add a category and source of word to queryset.
-        """
+        """Get queryset to specific user."""
         queryset = super(WordListView, self).get_queryset(
         ).select_related(
             'category',
@@ -102,13 +96,6 @@ class WordListView(CheckLoginPermissionMixin, FilterView):
             user=self.request.user
         ).order_by(
             '-pk',
-        ).annotate(
-            # https://stackoverflow.com/questions/68500663/how-to-annotate-a-queryset-adding-row-numbers-groupped-by-a-field
-            row_number=Window(
-                # https://docs.djangoproject.com/en/5.0/ref/models/database-functions/#rownumber
-                expression=RowNumber(),
-                order_by=[F('pk')]
-            )
         )
         return queryset
 
