157bcb7525e45254f4f658b1b7721f5011170fc4
Sergei Mikurov <454004@mail.ru>
2024-03-10 23:28:15 +0500

    Fix KeyError at get_words_for_study
    Raised an error if the user has not selected a value level

--- english/tasks/study_words.py
+++ english/tasks/study_words.py
@@ -6,6 +6,8 @@
 
 from random import shuffle
 
+from django.db.models import QuerySet
+
 from english.services.serve_query import (
     get_random_query_from_queryset,
     get_words_for_study,
@@ -18,14 +20,13 @@ def shuffle_sequence(sequence):
     return sequence
 
 
-def create_task_study_words(lookup_params, user_id):
-    """Создай задание пользователю для изучения слов, согласно его фильтрам.
-    """
-    task_study_word = dict()
-    words = get_words_for_study(lookup_params, user_id)
-    word_count = words.count()
+def create_task_study_words(lookup_params: dict, user_id: int) -> dict | None:
+    """Create a task for the user to learn words using his filters."""
+    task_study_word = None
+    words: QuerySet = get_words_for_study(lookup_params, user_id)
 
     if words:
+        word_count = words.count()
         word = get_random_query_from_queryset(words)
         word_translations = [word.words_eng, word.words_rus]
         question, answer = shuffle_sequence(word_translations)
