f28ce4bdc1e3a50e2025b9e476773de2f4f8b2dd
Sergei Mikurov <454004@mail.ru>
2024-03-12 00:13:46 +0500

    Set the local database as db-wse-develop.sqlite3

--- branch.py
+++
@@ -1,3 +0,0 @@
-BRANCH_NAME = 'main'
-"""Constant value for selecting the database in the developer's git branch.
-"""
--- config/settings.py
+++ config/settings.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-from branch import BRANCH_NAME
-
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,18 +116,11 @@ elif os.getenv('DEFAULT_DB') == 'fixtures':
             'NAME': BASE_DIR / 'db-wse-fixtures.sqlite3',
         }
     }
-elif BRANCH_NAME == 'development':
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db-wse-dev.sqlite3',
-        }
-    }
-elif BRANCH_NAME == 'main':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db-wse-main.sqlite3',
+            'NAME': BASE_DIR / 'db-wse-develop.sqlite3',
         }
     }
 # End Database
