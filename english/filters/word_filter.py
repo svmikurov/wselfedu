b8bb5ba802dd5c034d8885444de986655ffe54b5
Sergei Mikurov <454004@mail.ru>
2024-03-12 20:48:30 +0500

    Fix filter only by user items at word_list.html

--- english/filters/word_filter.py
+++ english/filters/word_filter.py
@@ -8,6 +8,18 @@ from english.models import (
 )
 
 
+def category_by_current_user(request):
+    if request is None:
+        return CategoryModel.objects.none()
+    return CategoryModel.objects.filter(user=request.user)
+
+
+def source_by_current_user(request):
+    if request is None:
+        return SourceModel.objects.none()
+    return SourceModel.objects.filter(user=request.user)
+
+
 class WordsFilter(django_filters.FilterSet):
     """Words filter"""
 
@@ -27,14 +39,14 @@ class WordsFilter(django_filters.FilterSet):
         widget=TextInput(attrs={'placeholder': 'Поиск по слову'}),
     )
     filtered_category = django_filters.ModelChoiceFilter(
-        queryset=CategoryModel.objects.all(),
+        queryset=category_by_current_user,
         field_name='category',
         lookup_expr='exact',
         label='',
         empty_label='Категория',
     )
     filtered_source = django_filters.ModelChoiceFilter(
-        queryset=SourceModel.objects.all(),
+        queryset=source_by_current_user,
         field_name='source',
         lookup_expr='exact',
         label='',
