d55e523729d7c5e44fda6289974213098e9288b1
Sergei Mikurov <454004@mail.ru>
2024-04-01 03:27:32 +0500

    Refactoring english/forms/word_choice_form.py

--- english/forms/word_choice_form.py
+++ english/forms/word_choice_form.py
@@ -1,19 +1,25 @@
-from crispy_forms.bootstrap import InlineCheckboxes
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Submit, HTML, Row, Column
+"""
+Form module for obtaining parameters for choosing words
+for word learning exercises.
+
+The form displays words studied by a specific user.
+"""
+
 from django import forms
 from django.urls import reverse_lazy
 
 from english.models import CategoryModel, SourceModel
 
-DEFAULT_CHOICE_VALUE = 0
+from crispy_forms.bootstrap import InlineCheckboxes
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit, HTML, Row, Column
 
 PERIOD_START = (
-    ('DT', 'Сегодня'),
-    ('D3', 'Три дня назад'),
-    ('W1', 'Неделя назад'),
-    ('W4', 'Четыре недели назад'),
-    ('NC', 'Не выбран'),
+    ('DT', 'Сегодня'),                  # day today
+    ('D3', 'Три дня назад'),            # 3 days ago
+    ('W1', 'Неделя назад'),             # 1 week ago
+    ('W4', 'Четыре недели назад'),      # 4 weeks ago
+    ('NC', 'Добавлено'),                # not choised
 )
 PERIOD_END = (
     ('DT', 'Сегодня'),
@@ -21,8 +27,6 @@ PERIOD_END = (
     ('W1', 'Неделя назад'),
     ('W4', 'Четыре недели назад'),
 )
-DEFAULT_START_PERIOD = 'NC'
-DEFAULT_END_PERIOD = 'DT'
 WORD_COUNT = (
     ('OW', 'Слово'),
     ('CB', 'Словосочетание'),
@@ -35,58 +39,54 @@ KNOWLEDGE_ASSESSMENT = (
     ('E', 'Проверяю'),      # examination
     ('K', 'Знаю'),          # know
 )
+DEFAULT_START_PERIOD = 'NC'
+DEFAULT_END_PERIOD = 'DT'
+DEFAULT_KNOWLEDGE_ASSESSMENT = 'S'
+DEFAULT_WORD_COUNT = ('OW', 'CB')
+DEFAULT_CREATE_CHOICE_VALUE = 0
 
 
 def create_choices(model, user_id):
     """Create human-readable choice by model and user_id."""
-    choices = []
-    queryset = model.objects.filter(user_id=user_id)
+    model_name = model._meta.verbose_name
+    default_choice = (DEFAULT_CREATE_CHOICE_VALUE, model_name)
+    choices = [default_choice]
 
     # Populate a list of choices.
+    queryset = model.objects.filter(user_id=user_id)
     for instance in queryset:
         model_value, human_readable_name = instance.pk, str(instance)
         choice = (model_value, human_readable_name)
         choices.append(choice)
 
-    # Add model name to human-readable list as default choice.
-    model_name = model._meta.verbose_name
-    not_choised = (DEFAULT_CHOICE_VALUE, model_name)
-    choices.append(not_choised)
-
-    return choices
-
-
-def create_category_choice(user_id):
-    """Create category choices by ``CategoryModel``."""
-    choices = create_choices(CategoryModel, user_id)
-    return choices
-
-
-def create_source_choice(user_id):
-    """Create source choices by ``SourceModel``."""
-    choices = create_choices(SourceModel, user_id)
     return choices
 
 
 class WordChoiceHelperForm(forms.Form):
-    """Форма получения параметров выборки слов для упражнения изучения слов."""
+    """Form obtaining word choice parameters for a word learning exercise."""
+
+    # The remaining selection fields do not require specific user.
+    only_user_field_choice = {
+        'category': CategoryModel,
+        'source': SourceModel,
+    }
 
     def __init__(self, *args, **kwargs):
         self.user_id = kwargs.pop('user_id', None)
         super(WordChoiceHelperForm, self).__init__(*args, **kwargs)
-        self.fields['category'].choices = create_category_choice(self.user_id)
-        self.fields['source'].choices = create_source_choice(self.user_id)
+        for field, model in self.only_user_field_choice.items():
+            self.fields[field].choices = create_choices(model, self.user_id)
 
     favorites = forms.BooleanField(
         required=False,
     )
     category = forms.TypedChoiceField(
-        initial=DEFAULT_CHOICE_VALUE,
+        initial=DEFAULT_CREATE_CHOICE_VALUE,
         required=False,
         label='',
     )
     source = forms.TypedChoiceField(
-        initial=DEFAULT_CHOICE_VALUE,
+        initial=DEFAULT_CREATE_CHOICE_VALUE,
         required=False,
         label='',
     )
@@ -94,24 +94,24 @@ class WordChoiceHelperForm(forms.Form):
         choices=PERIOD_START,
         initial=DEFAULT_START_PERIOD,
         required=False,
-        label='Начало',
+        label='',
     )
     period_end_date = forms.ChoiceField(
         choices=PERIOD_END,
         initial=DEFAULT_END_PERIOD,
         required=False,
-        label='Конец',
+        label='',
     )
     word_count = forms.MultipleChoiceField(
         choices=WORD_COUNT,
-        initial=('OW', 'CB'),
+        initial=DEFAULT_WORD_COUNT,
         required=False,
         widget=forms.CheckboxSelectMultiple(),
         label='',
     )
     knowledge_assessment = forms.MultipleChoiceField(
         choices=KNOWLEDGE_ASSESSMENT,
-        initial='S',
+        initial=DEFAULT_KNOWLEDGE_ASSESSMENT,
         required=False,
         widget=forms.CheckboxSelectMultiple(),
         label='',
@@ -119,6 +119,7 @@ class WordChoiceHelperForm(forms.Form):
 
     @property
     def helper(self):
+        """Create form."""
         helper = FormHelper()
         helper.form_method = 'post'
         helper.form_action = reverse_lazy('english:word_choice')
@@ -130,10 +131,10 @@ class WordChoiceHelperForm(forms.Form):
                 Column('source', css_class='form-group col-6'),
                 css_class='form-row',
             ),
-            HTML('<label class="h6">Период добавления</label>'),
+            HTML('<label class="h6">Период добавления слова</label>'),
             Row(
-                Column('period_start_date', css_class='form-group col-sm-6'),
-                Column('period_end_date', css_class='form-group col-sm-6'),
+                Column('period_start_date', css_class='form-group col-6'),
+                Column('period_end_date', css_class='form-group col-6'),
                 css_class='form-row',
             ),
             HTML('<label class="h6">Слово, длина выражения</label>'),
