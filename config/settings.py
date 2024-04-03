ba618cd19ffe4eb8bd20edadda712786d0f5c315
Sergei Mikurov <454004@mail.ru>
2024-04-03 21:49:51 +0500

    Add STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -164,11 +164,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [
-    # "/home/special.polls.com/polls/static",
+    os.path.join(BASE_DIR, 'static_src')
 ]
 
 STATICFILES_FINDERS = [
@@ -177,7 +175,6 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 # End static files
 
