9f1244c67f4a85a08347e05fd74c2124c1c8e09d
Sergei Mikurov <454004@mail.ru>
2024-05-03 01:16:06 +0500

    Add tmp/restart.txt

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
-sys.path.insert(0, '/home/d/wselfedu/wselfedu')
-sys.path.insert(1, '/home/d/wselfedu/venv_wselfedu/lib/python3.10/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'wselfedu.settings'
+sys.path.insert(0, '/home/d/d45400kr/wselfedu/')
+sys.path.insert(1, '/home/d/d45400kr/wselfedu/venv_wselfedu/lib/python3.10/site-packages/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
