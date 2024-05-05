223b33f1fbd0c9d10269ec9fc15c9a0210803abe
Sergei Mikurov <454004@mail.ru>
2024-05-05 10:12:09 +0500

    Refactoring task/services/lookup_params.py

--- task/services/lookup_params.py
+++ task/services/lookup_params.py
@@ -5,9 +5,9 @@ class LookupParams:
     """Lookup parameters class."""
 
     ASSESSMENTS = {
-        'S': [*range(7)],
+        'S': [0, 1, 2, 3, 4, 5, 6],
         'R': [7, 8],
-        'E': [9, 11],
+        'E': [9, 10],
         'K': [11],
     }
 
@@ -19,10 +19,10 @@ class LookupParams:
         """lookup parameters."""
         return {
             **self._user,
+            **self._favorite_lookup_param,
             **self._category_lookup_param,
             **self._source_lookup_param,
-            **self._favorite_lookup_param,
-            # **self._knowledge_assessment,
+            **self._knowledge_assessment,
         }
 
     @property
@@ -30,8 +30,7 @@ class LookupParams:
         """Lookup parameter by user."""
         lookup_value = self.form_data.get('user_id')
         lookup_field = 'user'
-        lookup_param = {lookup_field: lookup_value} if lookup_value else {}
-        return lookup_param
+        return {lookup_field: lookup_value} if lookup_value else {}
 
     @property
     def _favorite_lookup_param(self):
@@ -49,37 +48,37 @@ class LookupParams:
         """Lookup parameter by category."""
         lookup_value = self.form_data.get('category')
         lookup_field = 'category_id'
-        lookup_param = {lookup_field: lookup_value} if lookup_value else {}
-        return lookup_param
+        return {lookup_field: lookup_value} if lookup_value else {}
 
     @property
     def _source_lookup_param(self):
         """Lookup parameter by source."""
         lookup_value = self.form_data.get('source')
         lookup_field = 'source_id'
-        lookup_param = {lookup_field: lookup_value} if lookup_value else {}
-        return lookup_param
+        return {lookup_field: lookup_value} if lookup_value else {}
 
     @property
     def _knowledge_assessment(self):
         """Lookup parameter by user knowledge assessment."""
-        form_value = self.form_data.get('knowledge_assessment')
-        lookup_value = self._to_numeric_assessment(self.ASSESSMENTS, form_value)
+        form_value = self.form_data.get('knowledge_assessment', {})
+        if not form_value:
+            return {}
+
+        lookup_value = self._to_numeric(self.ASSESSMENTS, form_value)
         lookup_field = 'worduserknowledgerelation__knowledge_assessment__in'
-        lookup_param = {lookup_field: lookup_value} if lookup_value else {}
-        if lookup_param:
+        if lookup_value:
             return {
-                **lookup_param,
+                lookup_field: lookup_value,
                 'worduserknowledgerelation__word_id': F('pk'),
             }
         return {}
 
     @staticmethod
-    def _to_numeric_assessment(assessments, string_values):
+    def _to_numeric(assessments, string_values):
         """Convert a literal representation of an assessment into a list of
         numeric values.
         """
         numeric_values = []
         for assessment in string_values:
-            numeric_values += assessments[assessment]
+            numeric_values += assessments.get(assessment, [])
         return numeric_values
