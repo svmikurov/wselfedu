a2fd4ddd3b893bfa1572344bb7969e533a4a8083
Sergei Mikurov <454004@mail.ru>
2024-05-12 22:33:04 +0500

    Apply django-crispy-forms for WordForm

--- english/forms/word.py
+++ english/forms/word.py
@@ -1,10 +1,14 @@
-from django.forms import ModelForm, TextInput
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import (
+    Layout, Field, Row, Column, Button, ButtonHolder, Submit, Reset
+)
+from django.forms import ModelForm
 
 from english.models import WordModel
 
 
 class WordForm(ModelForm):
-    """Форма добавления и изменения слова."""
+    """Form for adding and updating a word."""
 
     class Meta:
         model = WordModel
@@ -15,7 +19,39 @@ class WordForm(ModelForm):
             'source',
             'word_count',
         )
-        # https://docs.djangoproject.com/en/5.0/topics/forms/modelforms/#overriding-the-default-fields
-        widgets = {
-            'words_eng': TextInput(attrs={'autofocus': True})
-        }
+
+    @staticmethod
+    def apply_crispy_helper(form):
+        """Apply crispy form helper for specified WordModel form."""
+        form.helper = FormHelper()
+        form.helper.layout = Layout(
+            Field('words_eng', autofocus=True),
+            Field('words_rus'),
+            Row(
+                Column("category"),
+                Column("source"),
+            ),
+            Field('word_count'),
+            ButtonHolder(
+                Submit(
+                    name="Save",
+                    value="Добавить",
+                    css_class="btn-success btn-sm",
+                    style="width:80px",
+                ),
+                Reset(
+                    name="Reset This Form",
+                    value="Сбросить",
+                    css_class="btn-danger btn-sm",
+                    style="width:80px",
+                ),
+                Button(
+                    name="Back",
+                    value="Назад",
+                    css_class="btn-primary btn-sm",
+                    style="width:80px",
+                    onclick="history.back();",
+                ),
+            ),
+        )
+        return form
