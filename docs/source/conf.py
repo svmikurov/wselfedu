c9a7df6f2c167743da68c99c0bf113e1219521dc
Sergei Mikurov <454004@mail.ru>
2024-06-08 23:51:34 +0500

    Add sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'wselfedu'
+copyright = '2024, Sergei Mikurov'
+author = 'Sergei Mikurov'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    # https://www.sphinx-doc.org/en/master/tutorial/describing-code.html#including-doctests-in-your-documentation
+    'sphinx.ext.doctest',
+    # https://www.sphinx-doc.org/en/master/tutorial/automatic-doc-generation.html#reusing-signatures-and-docstrings-with-autodoc
+    'sphinx.ext.autodoc',
+    # https://www.sphinx-doc.org/en/master/tutorial/automatic-doc-generation.html#generating-comprehensive-api-references
+    'sphinx.ext.autosummary',
+    # https://myst-parser.readthedocs.io/en/v0.17.1/index.html
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/usage.rst
@@ -0,0 +1,20 @@
+Usage
+=====
+
+Installation
+------------
+
+.. code-block:: console
+
+   (.venv) $ git clone git@github.com:svmikurov/wselfedu.git
+   (.venv) $ pip install poetry
+   (.venv) $ poetry shell
+   (.venv) $ poetry install
+   (.venv) $ make migrate
+
+Run app
+-------
+
+.. code-block:: console
+
+   (.venv) $ make start
