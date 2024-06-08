8d19c5a7ea103d4970a266afb438a933ae85f7ba
Sergei Mikurov <454004@mail.ru>
2024-06-09 04:09:44 +0500

    Fix beget static

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if os.getenv('ENVIRONMENT') == 'beget':
-    STATIC_URL = '/public_html/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'public_html/static')
 
 STATICFILES_DIRS = [
