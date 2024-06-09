05a5de566781d895d4e31a47a16c55375234e81d
Sergei Mikurov <454004@mail.ru>
2024-06-09 20:03:54 +0500

    Edit docstring

--- english/analytics/english_analytics.py
+++ english/analytics/english_analytics.py
@@ -6,13 +6,13 @@ from english.models import WordLearningStories, WordModel
 def collect_statistics(task) -> None:
     """Add word display to statistics.
 
+    Count word display at ``display_count`` fild of
+    :func:`english.models.word_analytic.WordLearningStories` model.
+
     Parameters
     ----------
     task : `english.EnglishTranslateExercise`
         Contain task data.
-
-    Count word display at ``english.models.word_analytic.WordLearningStories``
-    model ``display_count`` fild .
     """
     story, _ = WordLearningStories.objects.get_or_create(
         word=WordModel.objects.get(id=task.word_id)
