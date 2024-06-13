9d47e2459c4a459a989b9fed45ced4ac3d52bd70
Sergei Mikurov <454004@mail.ru>
2024-06-14 02:10:54 +0500

    Add exclude created_at

--- glossary/admin.py
+++ glossary/admin.py
@@ -5,9 +5,10 @@ from glossary.models import Glossary, GlossaryCategory
 
 @admin.register(Glossary)
 class GlossaryAdmin(admin.ModelAdmin):
-    list_display = ['term', 'definition', 'created_at']
+    list_display = ['term', 'definition']
+    exclude = ['created_at']
 
 
 @admin.register(GlossaryCategory)
 class GlossaryCategoryAdmin(admin.ModelAdmin):
-    pass
+    exclude = ['created_at']
