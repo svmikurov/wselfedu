9dcc6eee5b16134126dfa870bc55086e8d3c971b
Sergei Mikurov <454004@mail.ru>
2024-06-14 02:27:53 +0500

    Set Textarea widget for definition field

--- glossary/admin.py
+++ glossary/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from glossary.models import Glossary, GlossaryCategory
@@ -7,6 +8,13 @@ from glossary.models import Glossary, GlossaryCategory
 class GlossaryAdmin(admin.ModelAdmin):
     list_display = ['term', 'definition']
     exclude = ['created_at']
+
+    def get_form(self, request, obj=None, **kwargs):
+        """Set Textarea widget for ``definition`` field."""
+        kwargs['widgets'] = {
+            'definition': forms.Textarea,
+        }
+        return super().get_form(request, obj, **kwargs)
 
 
 @admin.register(GlossaryCategory)
