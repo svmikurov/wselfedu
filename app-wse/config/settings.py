e8fcae2534cad150189efea7f32b85c87b0ba145
Sergei Mikurov <454004@mail.ru>
2024-06-30 14:53:16 +0500

    Mv project to app-wse/ dir

--- app-wse/config/settings.py
+++ app-wse/config/settings.py
@@ -102,7 +102,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_src')
+    os.path.join(BASE_DIR, 'static_src'),
 ]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
