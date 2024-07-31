899f582ece213addca16973cd16a49fade4c530b
Sergei Mikurov <454004@mail.ru>
2024-08-01 00:00:46 +0500

    chore(logging): add logging

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,24 @@ CACHES = {
         'LOCATION': 'redis://redis:6379',
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
