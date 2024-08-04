ccbc289f3ac38a579805cb633ba0c76121f57e7a
Sergei Mikurov <454004@mail.ru>
2024-08-05 00:30:12 +0500

    test(profile page): add username on page

--- tests_e2e/tests/test_profile.py
+++ tests_e2e/tests/test_profile.py
@@ -1,10 +1,12 @@
+from playwright.sync_api import expect
+
 from tests_e2e.pages.account import AccountPage
 from tests_e2e.pages.math_calculate_solution import MathCalculateSolutionPage
 from tests_e2e.pages.user import authorize_the_page
 from tests_e2e.tests.base import POMBaseTest
 
 
-class TestAccount(POMBaseTest):
+class TestProfile(POMBaseTest):
     """Test account page."""
 
     fixtures = ['tests_e2e/fixtures/fixture-db-user.json']
@@ -12,10 +14,15 @@ class TestAccount(POMBaseTest):
     def setUp(self):
         host = str(self.live_server_url)
         authorize_the_page(self.page, host)
-        self.test_page = AccountPage(self.page)
-        self.test_page.navigate(url=f'{host}/users/2/account')
+        self.profile = AccountPage(self.page)
+        self.profile.navigate(url=f'{host}/users/2/account')
 
     def test_mult_table_page(self):
         """Test click button for start Table mult exercise."""
-        self.test_page.mult_table_btn.click()
-        self.test_page.test_title(MathCalculateSolutionPage.title)
+        self.profile.mult_table_btn.click()
+        self.profile.test_title(MathCalculateSolutionPage.title)
+
+    def test_page_content(self):
+        """Test user pofile page content."""
+        # The user profile page contains the text of the username.
+        expect(self.profile.page.get_by_text('test-user')).to_be_visible()
