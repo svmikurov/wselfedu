83d82b617cb0e7b0d9b3b01485c80ef7dd771ab4
Sergei Mikurov <454004@mail.ru>
2024-08-07 01:02:32 +0500

    docs(tests_e2e): update __init__.py

--- tests_e2e/__init__.py
+++ tests_e2e/__init__.py
@@ -5,4 +5,7 @@ Uses Page Object Model.
 See also:
     * `Playwright Page Object Model <https://playwright.dev/python/docs/pom>`_
     * `Best Practices <https://playwright.dev/docs/best-practices>`_
+    * `playwright-python-tutorial <https://github.com/mxschmitt/playwright-python-tutorial.git>`_
+    * `python-django-playwright <https://github.com/mxschmitt/python-django-playwright.git>`_
+    * `playwright-python-docker-example <https://github.com/mxschmitt/playwright-python-docker-example.git>`_
 """
