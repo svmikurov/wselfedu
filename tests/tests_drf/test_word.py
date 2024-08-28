d9d5363112d83f20b890a33e7da0f2e8ff7cfe79
Sergei Mikurov <454004@mail.ru>
2024-08-28 12:22:56 +0500

    refactor(test): tests_drf/test_word.py

--- tests/tests_drf/test_word.py
+++ tests/tests_drf/test_word.py
@@ -1,47 +1,56 @@
 """Test Create List API views."""
 
+from urllib.parse import urljoin
+
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
-from english.models import WordModel
 from users.models import UserModel
 
 
 class TestWordList(APITestCase):
     """Test word list api view."""
 
-    words = [
-        {'word_eng': 'red', 'word_rus': 'красный'},
-        {'word_eng': 'white', 'word_rus': 'белый'},
-        {'word_eng': 'green', 'word_rus': 'зеленый'},
-        {'word_eng': 'blue', 'word_rus': 'синий'},
-        {'word_eng': 'black', 'word_rus': 'черный'},
-    ]
+    fixtures = ['tests/tests_drf/fixtures/words.json']
 
     def setUp(self) -> None:
         """Set up data."""
-        self.owner = UserModel.objects.create_user(username='owner')
-        self.user = UserModel.objects.create_user(username='user')
-        for word in self.words:
-            WordModel.objects.create(**word, user=self.owner)
+        self.api_client = APIClient()
         self.url = reverse('api-word')
+        self.user1 = UserModel.objects.get(username='user1')
+        self.user2 = UserModel.objects.get(username='user2')
 
-    def test_get_word_list_by_owner(self) -> None:
-        """Test list api view for owner."""
-        client = APIClient()
-        client.force_authenticate(user=self.owner)
-        response = client.get(self.url)
-        assert response.status_code == 200
+    def test_words_permission_for_owner(self) -> None:
+        """Test words permission for owner."""
+        self.api_client.force_authenticate(user=self.user1)
+        response = self.api_client.get(self.url)
+        assert response.status_code == status.HTTP_200_OK
 
-    # def test_get_word_list_by_anonymous(self) -> None:
-    #     """Test list api view for anonymous."""
-    #     client = APIClient()
-    #     response = client.get(self.url)
-    #     assert response.status_code == 200
+    def test_words_permission_for_anonymous(self) -> None:
+        """Test words permission for anonymous."""
+        response = self.api_client.get(self.url)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_get_word_list_by_not_owner(self) -> None:
-        """Test list api view for not owner."""
-        client = APIClient()
-        client.force_authenticate(user=self.user)
-        response = client.get(self.url)
-        assert response.status_code == 200
+    def test_words_permission_for_other_user(self) -> None:
+        """Test words permission for other user."""
+        self.api_client.force_authenticate(user=self.user2)
+        response = self.api_client.get(self.url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_word_permission_for_owner(self) -> None:
+        """Test word permission for owner."""
+        self.api_client.force_authenticate(user=self.user1)
+        response = self.api_client.get(urljoin(self.url, '2/'))
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_word_permission_for_other_user(self) -> None:
+        """Test word permission for other user."""
+        self.api_client.force_authenticate(user=self.user2)
+        response = self.api_client.get(urljoin(self.url, '2/'))
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_word_permission_for_anonymous(self) -> None:
+        """Test word permission for anonymous."""
+        response = self.api_client.get(urljoin(self.url, '2/'))
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
