00792a99c92286ba5d27debabc6b74c704588174
Sergei Mikurov <454004@mail.ru>
2024-09-06 19:43:41 +0500

    feat(glossary): add GlossaryExercise

---
+++ task/tasks/glossary_exercise.py
@@ -0,0 +1,29 @@
+"""Glossary exercise.
+
+Specific for rest api requests.
+"""
+
+
+class GlossaryExercise:
+    """Glossary exercise."""
+
+    def __init__(self, exercise_conditions: dict) -> None:
+        """Construct the exercise."""
+        self.exercise_conditions = exercise_conditions
+
+    @property
+    def data(self) -> dict:
+        """Exercise data."""
+        # Return task data or condition
+        results = {
+            'errors': [
+                'Нет терминов, соответствующих условиям задачи',
+            ],
+            'task': {
+                'term_id': '',
+                'term': '',
+                'definition': '',
+                'category': '',
+            }
+        }
+        return results
