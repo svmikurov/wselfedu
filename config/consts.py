d51835100cd89fb93c00586d5f4314c4bdf4082c
Sergei Mikurov <454004@mail.ru>
2024-09-20 04:53:22 +0500

    feat(const): update

--- config/consts.py
+++ config/consts.py
@@ -3,9 +3,56 @@
 GET = 'GET'
 POST = 'POST'
 
+########################################################################
+# Attributes
+########################################################################
 
 ALIAS = 'alias'
+ACTION = 'action'
 HUMANLY = 'humanly'
+CATEGORY = 'category'
+CATEGORIES = 'categories'
+PERIOD_START_DATE = 'period_start_date'
+PERIOD_END_DATE = 'period_end_date'
+PROGRES = 'progres'
+ID = 'id'
+KNOW = 'know'
+NOT_KNOW = 'not_know'
+USER = 'user'
+TERM = 'term'
+
+########################################################################
+# Variables
+########################################################################
+
+DEFAULT_PERIOD_START_INDEX = -1
+DEFAULT_PERIOD_END_INDEX = 0
+DEFAULT_CATEGORY = None
+
+INCREMENT_STEP = 1   # the step value does not change
+DECREMENT_STEP = -1  # the step value does not change
+
+TERM_PROGRES_MIN = 0
+TERM_PROGRES_MAX = 11
+
+########################################################################
+# Collections
+########################################################################
+
+DEFAULT_GLOSSARY_PARAMS = {
+    PERIOD_START_DATE: DEFAULT_PERIOD_START_INDEX,
+    PERIOD_END_DATE: DEFAULT_PERIOD_END_INDEX,
+    CATEGORY: DEFAULT_CATEGORY,
+}
+
+PROGRES_STEPS = {
+    KNOW: INCREMENT_STEP,
+    NOT_KNOW: DECREMENT_STEP,
+}
+
+########################################################################
+# Collections for Model choices
+########################################################################
 
 EDGE_PERIOD_ITEMS = [
     {ALIAS: 'DT', HUMANLY: 'Сегодня'},
@@ -20,23 +67,13 @@ EDGE_PERIOD_ITEMS = [
     {ALIAS: 'M9', HUMANLY: 'Девять месяцев назад'},
     {ALIAS: 'NC', HUMANLY: 'Добавлено'},
 ]
+"""Edge periods, for choice (`dict[str, str]`).
+"""
 PROGRES_STAGES = [
     {ALIAS: 'S', HUMANLY: 'Изучаю'},
     {ALIAS: 'R', HUMANLY: 'Повторяю'},
     {ALIAS: 'E', HUMANLY: 'Проверяю'},
     {ALIAS: 'K', HUMANLY: 'Знаю'},
 ]
-
-CATEGORY = 'category'
-CATEGORIES = 'categories'
-PERIOD_START_DATE = 'period_start_date'
-PERIOD_END_DATE = 'period_end_date'
-DEFAULT_PERIOD_START_INDEX = -1
-DEFAULT_PERIOD_END_INDEX = 0
-DEFAULT_CATEGORY = None
-
-DEFAULT_GLOSSARY_PARAMS = {
-    PERIOD_START_DATE: DEFAULT_PERIOD_START_INDEX,
-    PERIOD_END_DATE: DEFAULT_PERIOD_END_INDEX,
-    CATEGORY: DEFAULT_CATEGORY,
-}
+"""Study progres stages, for choice (`dict[str, str]`).
+"""
