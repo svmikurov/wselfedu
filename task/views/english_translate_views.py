ffb77052afd0ecb9b004e0aefecde627cdd9f8fd
Sergei Mikurov <454004@mail.ru>
2024-09-28 11:50:19 +0500

    docs(task): update english exercise view docstrings

--- task/views/english_translate_views.py
+++ task/views/english_translate_views.py
@@ -11,46 +11,47 @@ from django.views import View
 from django.views.decorators.http import require_POST
 from django.views.generic import TemplateView
 
+from config.constants import PROGRES_MAX, PROGRES_MIN
 from contrib.mixins_views import (
     CheckLoginPermissionMixin,
 )
 from english.analytics.english_analytics import collect_statistics
-from english.orm_queries import (
-    get_knowledge_assessment,
-    update_word_knowledge_assessment,
-)
+from english.models import WordModel, WordUserKnowledgeRelation
 from task.forms import EnglishTranslateChoiceForm
 from task.tasks import EnglishTranslateExercise
 
 
 class EnglishTranslateChoiceView(CheckLoginPermissionMixin, TemplateView):
-    """English translate choice View.
+    """English exercise conditions choice view.
 
-    Notes
-    -----
-    Task conditions may be
+    .. note::
+        Task conditions may be:
 
-    .. code-block: python
+        .. code-block:: python
 
-        ``task_conditions`` = {
-            'favorites': `True`,
-            'language_order': 'RN',
-            'category': 0,
-            'source': 0,
-            'period_start_date': 'NC',
-            'period_end_date': 'DT',
-            'word_count': ['OW', 'CB'],
-            'knowledge_assessment': ['S'],
-            'timeout': 5,
-            'user_id': 1,
-        }
-
+           task_conditions = {
+               'favorites': True,
+               'language_order': 'RN',
+               'category': 0,
+               'source': 0,
+               'period_start_date': 'NC',
+               'period_end_date': 'DT',
+               'word_count': ['OW', 'CB'],
+               'knowledge_assessment': ['S'],
+               'timeout': 5,
+               'user_id': 1,
+           }
     """
 
     template_name = 'task/english/english_translate_choice.html'
+    """The view template path (`str`).
+    """
 
     def get_context_data(self, **kwargs: object) -> Dict[str, object]:
-        """Add english word translation conditions choice form."""
+        """Add a form with a choice of exercise conditions.
+
+        Adds :py:class:`~task.forms.english_translate_choice_form.EnglishTranslateChoiceForm`
+        """  # noqa: E501, W505
         context = super().get_context_data()
         context['form'] = EnglishTranslateChoiceForm(request=self.request)
         return context
@@ -79,8 +80,14 @@ class EnglishTranslateExerciseView(CheckLoginPermissionMixin, View):
     """English word translate exercise view."""
 
     template_name = 'task/english/english_translate_demo.html'
-    msg_key_error = 'Не задан таймаут или порядок перевода слов'
+    """The view template path (`str`).
+    """
+    msg_key_error = 'Не все условия упражнения заданы'
+    """Error message in condition ('str').
+    """
     msg_no_words = 'По заданным условиям слов не найдено'
+    """Message no words found (`str`).
+    """
     redirect_no_words = {
         'redirect_no_words': reverse_lazy('task:english_translate_choice'),
     }
@@ -102,7 +109,7 @@ class EnglishTranslateExerciseView(CheckLoginPermissionMixin, View):
             return render(request, self.template_name)
 
     def post(self, request: HttpRequest) -> JsonResponse:
-        """Get new word for English word translate exercise page."""
+        """Render the task."""
         task_conditions = request.session['task_conditions']
         task = EnglishTranslateExercise(**task_conditions)
 
@@ -133,19 +140,15 @@ def update_words_knowledge_assessment_view(
 ) -> JsonResponse:
     """Update user word knowledge assessment view.
 
-    Parameters
-    ----------
-    request : `HttpRequest`
-        Request to update user word knowledge assessment.
-    kwargs : object
-        - ``word_id``: ID of the word whose rating will be updated
-          (`str`).
+    :param HttpRequest request: Request to update user word knowledge
+     assessment.
+    :param object kwargs: ID of the word whose progress will be updated.
 
-    Returns
-    -------
-    `JsonResponse`
-        Response without data, with status 201.
+     ``kwargs`` fields:
+       - ``'word_id'``: word ID (`int`).
 
+    :return: Response without data, with status 201.
+    :rtype: JsonResponse
     """
     assessment = request.POST['assessment']
     word_pk = kwargs['word_id']
