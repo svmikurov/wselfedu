bd965fb4bc7674bf9e4f57e1d44fed8eb7c8cd7a
Sergei Mikurov <454004@mail.ru>
2024-09-28 07:11:42 +0500

    perf(test): add expectations

--- tests_plw/pages/english_exercise_conditions.py
+++ tests_plw/pages/english_exercise_conditions.py
@@ -16,20 +16,37 @@ class EnglishExerciseConditionsChoicePage(POMPage):
         self.page = page
         self.path = '/task/english-translate-choice/'
 
+        # Favorites word checkbox.
         self.favorites_choice = page.get_by_label('Favorites')
+        # Translate order choice.
         self.language_order_choice = page.locator('#id_language_order')
+        # Word category choice.
         self.category_choice = page.locator('#id_category')
+        # Word source choice.
         self.source_choice = page.locator('#id_source')
+        # Word adding edge period choice.
         self.start_period_choice = page.locator('#id_period_start_date')
         self.end_period_choice = page.locator('#id_period_end_date')
+        # Study progress checkboxes.
+        self.study_progres_label = page.get_by_label('Изучаю')
+        self.repeat_progres_label = page.get_by_label('Повторяю')
+        self.repeat_progres_label = page.get_by_label('Проверяю')
+        self.examination_progres_label = page.get_by_label('Знаю')
         self.study_progres_choice = page.locator('#id_knowledge_assessment_0')
         self.repeat_progres_choice = page.locator('#id_knowledge_assessment_1')
         self.examination_progres_choice = page.locator(
             '#id_knowledge_assessment_2'
         )
         self.know_progres_choice = page.locator('#id_knowledge_assessment_3')
+        # Answer timeout
         self.timeout_choice = page.get_by_label('Время на ответ (сек)*')
-        self.ow_length_choice = page.get_by_label('Слово')
-        self.cb_length_choice = page.get_by_label('Словосочетание')
-        self.ps_length_choice = page.get_by_label('Часть предложения')
-        self.st_length_choice = page.get_by_label('Предложение')
+        # Word length checkboxes.
+        self.ow_length_label = page.get_by_label('Слово', exact=True)
+        self.cb_length_label = page.get_by_label('Словосочетание')
+        self.ps_length_label = page.get_by_label('Часть предложения')
+        self.st_length_label = page.get_by_label('Предложение')
+        self.ow_length_choice = page.locator('#id_word_count_0')
+        self.cb_length_choice = page.locator('#id_word_count_1')
+        self.ps_length_choice = page.locator('#id_word_count_2')
+        self.st_length_choice = page.locator('#id_word_count_3')
+
--- tests_plw/tests/test_english_exercise_conditions.py
+++ tests_plw/tests/test_english_exercise_conditions.py
@@ -1,13 +1,11 @@
 """Test English exercise conditions choice page."""
-import logging
-import re
+
 from http import HTTPStatus
 from unittest import skip
 from urllib.parse import urlparse
 
 from playwright.sync_api import expect
 
-import config.constants as const
 from tests_plw.pages.english_exercise_conditions import (
     EnglishExerciseConditionsChoicePage,
 )
@@ -25,6 +23,7 @@ class TestEnglishExerciseConditionsChoicePage(POMTest):
         self.authorize_test_page()
         self.response = self.test_page.navigate(page_url=self.page_url)
 
+    @skip
     def test_http_status(self) -> None:
         """Test http status."""
         response_path = urlparse(self.response.url).path
@@ -34,5 +33,35 @@ class TestEnglishExerciseConditionsChoicePage(POMTest):
     def test_default_exercise_conditions(self) -> None:
         """Test default exercise conditions."""
         page = self.test_page
+
+        # Favorites word checkbox.
         expect(page.favorites_choice).not_to_be_checked()
+        # Translate order choice.
         expect(page.language_order_choice).to_have_value('RN')
+        # Word category choice.
+        expect(page.category_choice).to_have_value('0')
+        # Word source choice.
+        expect(page.source_choice).to_have_value('0')
+        # Word adding edge period choice.
+        expect(page.start_period_choice).to_have_value('NC')
+        expect(page.end_period_choice).to_have_value('DT')
+        # Study progress checkboxes.
+        expect(page.study_progres_label).to_be_visible()
+        expect(page.repeat_progres_label).to_be_visible()
+        expect(page.repeat_progres_label).to_be_visible()
+        expect(page.examination_progres_label).to_be_visible()
+        expect(page.study_progres_choice).to_be_checked()
+        expect(page.repeat_progres_choice).not_to_be_checked()
+        expect(page.examination_progres_choice).not_to_be_checked()
+        expect(page.know_progres_choice).not_to_be_checked()
+        # Answer timeout choice.
+        expect(page.timeout_choice).to_have_value('5')
+        # Word length checkboxes.
+        expect(page.ow_length_label).to_be_visible()
+        expect(page.cb_length_label).to_be_visible()
+        expect(page.ps_length_label).to_be_visible()
+        expect(page.st_length_label).to_be_visible()
+        expect(page.ow_length_choice).to_be_checked()
+        expect(page.cb_length_choice).to_be_checked()
+        expect(page.ps_length_choice).not_to_be_checked()
+        expect(page.st_length_choice).not_to_be_checked()
