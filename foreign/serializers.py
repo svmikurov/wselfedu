12112251f363a07459756e9fa3fc566db11f022e
Sergei Mikurov <454004@mail.ru>
2024-10-12 04:08:13 +0500

    feat(foreign): add serializers

--- foreign/serializers.py
+++ foreign/serializers.py
@@ -2,8 +2,12 @@
 
 from rest_framework import serializers
 
-from config.constants import FOREIGN_WORD, ID, RUSSIAN_WORD
-from foreign.models import Word
+from config.constants import (
+    FOREIGN_WORD,
+    ID,
+    RUSSIAN_WORD,
+)
+from foreign.models import TranslateParams, Word, WordCategory
 
 
 class WordSerializer(serializers.ModelSerializer):
@@ -14,3 +18,23 @@ class WordSerializer(serializers.ModelSerializer):
 
         model = Word
         fields = [ID, FOREIGN_WORD, RUSSIAN_WORD]
+
+
+class TranslateParamsSerializer(serializers.ModelSerializer):
+    """Translate foreign word exercise serializer."""
+
+    class Meta:
+        """Construct serializer."""
+
+        model = TranslateParams
+        fields = '__all__'
+
+
+class WordCategorySerializer(serializers.ModelSerializer):
+    """Foreign word Category serializer."""
+
+    class Meta:
+        """Setup serializer."""
+
+        model = WordCategory
+        fields = '__all__'
