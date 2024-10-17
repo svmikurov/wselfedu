b159129b5949ec032da0c779f04b01cb67e0ace6
Sergei Mikurov <454004@mail.ru>
2024-10-17 10:47:06 +0500

    test: add fixture path

--- config/settings.py
+++ config/settings.py
@@ -183,5 +183,7 @@ REST_FRAMEWORK = {
     # Pagination
     # https://www.django-rest-framework.org/api-guide/pagination/#setting-the-pagination-style
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',  # noqa: E501
-    'PAGE_SIZE': 20,  # Ebd Pagination
+    'PAGE_SIZE': 10,  # Ebd Pagination
 }
+
+FIXTURE_DIRS = ['tests/fixtures/']
