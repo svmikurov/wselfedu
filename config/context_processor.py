19fc43c1d50ca1bcbb120cbe28ac263d16246b9f
Sergei Mikurov <454004@mail.ru>
2024-10-28 13:52:20 +0500

    feat(config): add ENVIRONMENT to context_processor

--- config/context_processor.py
+++ config/context_processor.py
@@ -8,16 +8,19 @@ from django.http import HttpRequest
 def pass_var_to_template(request: HttpRequest) -> dict[str, str | bool]:
     """Pass variables to the template.
 
-    ENVIRONMENT_VARIABLES:
+    ENVIRONMENT VARIABLES:
         -- IS_TEST
             Let start pytest without load bootstrap at templates.
             Load bootstrap makes tests unstable.
 
-    Returns
-    -------
-    result : `dict`
+        -- ENVIRONMENT
+            Current mode.
 
+    :params HttpRequest request:
+    :return: dict of variables
+    :rtype: dict
     """
     return {
         'IS_TEST': os.getenv('IS_TEST'),
+        'ENVIRONMENT': os.getenv('ENVIRONMENT'),
     }
