a091e5aa6104e219bf80c5293aa159a9a7c0c87d
Sergei Mikurov <454004@mail.ru>
2024-11-02 22:41:18 +0500

    perf(foreign): add render id on foreign list request

--- foreign/serializers.py
+++ foreign/serializers.py
@@ -20,7 +20,7 @@ class WordSerializer(serializers.ModelSerializer):
         """Serializer settings."""
 
         model = Word
-        fields = ['foreign_word', 'native_word']
+        fields = ['id', 'foreign_word', 'native_word']
         """Fields (`list[str]`).
         """
 
