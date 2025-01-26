5aade26ac00efb30770fdbcc2cfa9a2b273d980b
Sergei Mikurov <454004@mail.ru>
2025-01-26 19:53:10 +0500

    Refactor foreign exercise

--- foreign/exercise/base.py
+++ foreign/exercise/base.py
@@ -1,5 +1,10 @@
 """Exercise elements."""
 
+from random import choice
+from typing import Any, Iterable, Type
+
+from django.db.models import Model, Q
+
 from config.constants import PROGRES_STEPS, PROGRESS_MAX, PROGRESS_MIN
 from foreign.models import Word, WordProgress
 from users.models import UserApp
@@ -25,3 +30,60 @@ class WordAssessment:
         if PROGRESS_MIN <= updated_assessment <= PROGRESS_MAX:
             progress.progress = updated_assessment
             progress.save(update_fields=['progress'])
+
+
+# fmt: off
+class ExerciseItems:
+    """Get exercise items methods."""
+
+    @staticmethod
+    def _get_item_ids(
+        model: Model,
+        lookup_params: Iterable[Q],
+        id_field: str,
+    ) -> list[int]:
+        item_ids = (
+            model
+            .objects
+            .filter(*lookup_params)
+            .values_list(id_field, flat=True)
+        )
+        return list(item_ids)
+
+    @staticmethod
+    def _get_random_item_ids(
+        item_count: int,
+        item_ids: list[int],
+    ) -> list[int]:
+        ids = [i for i in item_ids]
+        task_ids = []
+        # Random item ids have a certain amount in the task.
+        for _ in range(item_count):
+            if not ids:
+                break
+            item_id = choice(ids)
+            task_ids.append(item_id)
+            ids.remove(item_id)
+        return task_ids
+
+    @staticmethod
+    def _get_random_item_id(task_ids: list[int]) -> int:
+        return choice(task_ids)
+
+    @staticmethod
+    def _get_item(model: Type[Model], pk: int) -> Model:
+        return model.objects.get(pk=pk)
+
+    @staticmethod
+    def _get_items(
+        model: Type[Model],
+        item_ids: Iterable[int],
+        fields: Iterable[str],
+    ) -> list[tuple[Any, ...]]:
+        return list(
+            model
+            .objects
+            .filter(pk__in=item_ids)
+            .values_list(*fields)
+        )
+# fmt: on
--- foreign/exercise/testing.py
+++ foreign/exercise/testing.py
@@ -1,16 +1,13 @@
 """Foreign word testing exercise."""
 
-# fmt: off
-from random import choice
-from typing import Type
-
 from django.db.models import Model, Q
 
+from foreign.exercise.base import ExerciseItems
 from foreign.models.word import AssignedWord, Word
 from users.models import UserApp
 
 
-class ItemTesting:
+class ItemTesting(ExerciseItems):
     """Item testing exercise."""
 
     _model: Model = AssignedWord
@@ -23,57 +20,25 @@ class ItemTesting:
         self.task_item_id: int | None = None
         self.task_item_ids: list[int] | None = None
 
-    @staticmethod
-    def _get_item_ids(
-        model: Model, lookup_params: tuple[Q, ...]
-    ) -> list[int]:
-        item_ids = (
-            model.objects
-            .filter(*lookup_params)
-            .values_list('word__pk', flat=True)
-        )
-        return list(item_ids)
-
-    def _get_random_item_ids(self, item_ids: list[int]) -> list[int]:
-        ids = [i for i in item_ids]
-        task_ids = []
-        # Random item ids have a certain amount in the task.
-        for _ in range(self._item_count):
-            if not ids:
-                break
-            item_id = choice(ids)
-            task_ids.append(item_id)
-            ids.remove(item_id)
-        return task_ids
-
-    @staticmethod
-    def _get_random_item_id(task_ids: list[int]) -> int:
-        return choice(task_ids)
-
-    @staticmethod
-    def _get_item(model: Type[Model], pk: int) -> Model:
-        return model.objects.get(pk=pk)
-
     def create_task(self) -> None:
         """Create task."""
-        params = Q(student=self._user),
-        item_ids = self._get_item_ids(self._model, params)
-        self.task_item_ids = self._get_random_item_ids(item_ids)
+        params = (Q(student=self._user),)
+        item_ids = self._get_item_ids(self._model, params, id_field='word__pk')
+        # fmt: off
+        self.task_item_ids = self._get_random_item_ids(self._item_count, item_ids)  # noqa: E501
         self.task_item_id = self._get_random_item_id(self.task_item_ids)
-        self.task_item = self._get_item(model=Word, pk=self.task_item_id)  # noqa: E501
+        self.task_item = self._get_item(model=Word, pk=self.task_item_id)
+        # fmt: on
 
     @property
     def task_data(self) -> dict:
         """Task data (`dict`, reqe-only)."""
         self.create_task()
-        items = (
-            Word.objects
-            .filter(pk__in=self.task_item_ids)
-            .values_list('id', 'foreign_word')
-        )
+        fields = 'id', 'foreign_word'
+        items = self._get_items(Word, self.task_item_ids, fields)
         results = {
             'question': self.task_item.native_word,
             'answer': self.task_item_id,
-            'choices': list(items),
+            'choices': items,
         }
         return results
