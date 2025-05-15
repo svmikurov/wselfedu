13425f5f424da6943341a15e49e9521eabd3b8a5
Sergei Mikurov <454004@mail.ru>
2025-05-16 03:19:54 +0500

    chore: Add allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -21,8 +21,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 # adds path to projects apps
