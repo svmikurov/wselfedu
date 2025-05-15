884b1e5f057f89f7c1dc448f5bec987d8ebb7e11
Sergei Mikurov <454004@mail.ru>
2025-05-16 02:26:30 +0500

    feat: Start project with math application

---
+++ apps/core/apps.py
@@ -0,0 +1,10 @@
+"""Defines Core application config."""
+
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    """Core application config."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
---
+++ apps/features/mathem/apps.py
@@ -0,0 +1,10 @@
+"""Defines Mathematical application config."""
+
+from django.apps import AppConfig
+
+
+class MathemConfig(AppConfig):
+    """Mathematical application config."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mathem'
