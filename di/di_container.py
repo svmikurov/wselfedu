51bf5fa350e4e1ab6984e63a1ec83a848f9f0b97
Sergei Mikurov <454004@mail.ru>
2025-05-25 20:45:23 +0500

    feat(di): Add DI container initialization

--- di/di_container.py
+++ di/di_container.py
@@ -2,15 +2,12 @@
 
 from dependency_injector import containers, providers
 
-from src.apps.mathem.di_container import MathContainer
+from apps.mathem.di_container import MathContainer
 
 
 class MainContainer(containers.DeclarativeContainer):
     """Main DI container."""
 
-    math_container = providers.Container(
+    math = providers.Container(
         MathContainer,
     )
-
-    # API
-    math_exercise = math_container.exercise
