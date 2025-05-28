9d68b8b7bd20c969f30da0d42203dd827a6e07cb
Sergei Mikurov <454004@mail.ru>
2025-05-29 00:50:51 +0500

    feat(services): Add `ExerciseConditionsException` exeption

--- services/exceptions.py
+++ services/exceptions.py
@@ -9,6 +9,14 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 
 
+class ExerciseConditionsException(APIException):
+    """Raised when requested exercise with invalid conditions."""
+
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Invalid exercise conditions'
+    default_code = 'invalid_exercise_conditions'
+
+
 class ExerciseServiceException(APIException):
     """Base exception for exercise-related errors.
 
