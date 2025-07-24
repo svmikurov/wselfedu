8c9cdb8004f9a45dbba2556f39e6e1d1e2857e88
Sergei Mikurov <454004@mail.ru>
2025-07-24 13:52:36 +0500

    test: Add conftest
    - Moved `debug_reporter` fixture to root of tests

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+"""Defines configuration for tests."""
+
+from typing import Generator
+
+import pytest
+from django.conf import settings
+from django.core.management import call_command
+
+from utils.sql.sql_reporter import SQLReporter
+
+
+@pytest.fixture
+def debug_reporter(
+    request: pytest.FixtureRequest,
+    monkeypatch: pytest.MonkeyPatch,
+) -> Generator[SQLReporter, None, None]:
+    """Output the SQL queries."""
+    monkeypatch.setattr(settings, 'DEBUG', True)
+
+    reporter = SQLReporter(
+        test_name=request.node.name,
+        test_doc=request.node.function.__doc__,
+    )
+
+    yield reporter
+
+    reporter.print_report()
+
+
+@pytest.fixture(scope='session', autouse=True)
+def call_create_db_tables(  # type: ignore[no-untyped-def]
+    django_db_setup: None,
+    django_db_blocker,  # noqa: ANN001
+) -> None:
+    """Call management command to create database tables.
+
+    Creates the database tables that are not managed by Django.
+    """
+    with django_db_blocker.unblock():
+        call_command('create_tables')
--- tests/query/conftest.py
+++ tests/query/conftest.py
@@ -1,12 +1,8 @@
 """Defines configuration for tests."""
 
-from typing import Generator
-
 import pytest
-from django.conf import settings
 
 from apps.users.models import Balance, CustomUser
-from utils.sql.sql_reporter import SQLReporter
 
 
 @pytest.fixture
@@ -19,21 +15,3 @@ def user() -> CustomUser:
 def balance(user: CustomUser) -> Balance:
     """Fixture providing balance."""
     return Balance.objects.create(user=user)
-
-
-@pytest.fixture
-def debug_reporter(
-    request: pytest.FixtureRequest,
-    monkeypatch: pytest.MonkeyPatch,
-) -> Generator[SQLReporter, None, None]:
-    """Output the SQL queries."""
-    monkeypatch.setattr(settings, 'DEBUG', True)
-
-    reporter = SQLReporter(
-        test_name=request.node.name,
-        test_doc=request.node.function.__doc__,
-    )
-
-    yield reporter
-
-    reporter.print_report()
