86c09848a42826981360b4344f074899daec2c1b
Sergei Mikurov <454004@mail.ru>
2025-07-26 17:40:57 +0500

    test(db): Update test
    - Updated test of increment user reward procedure

--- tests/db/procedures/test_add_reward.py
+++ tests/db/procedures/test_add_reward.py
@@ -2,9 +2,10 @@
 
 import pytest
 from django.db import connection
+from django.db.models import Sum
 
+from apps.math.models.transaction import MathTransaction
 from apps.users.models import Balance, Transaction
-from utils.sql.report.reporter import SQLReporter
 
 USER_ID = 1
 AMOUNT = 33
@@ -24,34 +25,60 @@ class TestIncrementUserRewardProcedure:
     """Test `increment_user_reward` Postgres procedure."""
 
     @pytest.mark.django_db
-    def test_transaction_added(
-        self,
-        debug_reporter: SQLReporter,
-    ) -> None:
+    def test_transaction_added(self) -> None:
         """Test that the transaction was added."""
         # Act
-        debug_reporter.start_act()
         add_math_reward()
-        debug_reporter.end_act()
 
         # Assertion
 
         # Get data for assertions
         transaction = Transaction.objects.filter(user_id=USER_ID)
 
-        print(f':::: {transaction = }')
-
         # Transaction was added once
-        # assert transaction.count() == 1
+        assert transaction.count() == 1
 
         # Transaction was added with amount
-        # assert transaction.last().amount == AMOUNT
+        assert transaction.last().amount == AMOUNT  # type: ignore[union-attr]
 
     @pytest.mark.django_db
-    def test_balance_increased(
-        self,
-        debug_reporter: SQLReporter,
-    ) -> None:
+    def test_math_transaction_added(self) -> None:
+        """Test that the transaction was added to math transaction."""
+        # Act
+        add_math_reward()
+
+        # Assertion
+
+        # Get data for assertions
+        transaction = MathTransaction.objects.filter(user_id=USER_ID)
+
+        # Transaction was added once
+        assert transaction.count() == 1
+
+        # Transaction was added with amount
+        assert transaction.last().amount == AMOUNT  # type: ignore[union-attr]
+
+    @pytest.mark.django_db
+    def test_transaction_added_twice(self) -> None:
+        """Test that the transaction was added twice."""
+        # Act
+        add_math_reward()
+        add_math_reward()
+
+        # Assertion
+
+        # Get data for assertions
+        transaction = Transaction.objects.filter(user_id=USER_ID)
+        total_amount = transaction.aggregate(total_amount=Sum('amount'))
+
+        # Transaction was added once
+        assert transaction.count() == 2
+
+        # Transaction was added with amount
+        assert total_amount.get('total_amount') == AMOUNT * 2
+
+    @pytest.mark.django_db
+    def test_balance_increased(self) -> None:
         """Test that balance increased."""
         # Act
         add_math_reward()
@@ -65,10 +92,7 @@ class TestIncrementUserRewardProcedure:
         assert balance.total == AMOUNT
 
     @pytest.mark.django_db
-    def test_balance_increased_twice(
-        self,
-        debug_reporter: SQLReporter,
-    ) -> None:
+    def test_balance_increased_twice(self) -> None:
         """Test that balance increased, called twice."""
         # Setup
         double_reward = AMOUNT * 2
