959e77564f68d9cd5c6ebf65a3119826523b1fb1
Sergei Mikurov <454004@mail.ru>
2025-07-27 18:03:17 +0500

    fig(urls): Fix url path
    - Fixed url path to 'main' app

--- wselfedu/urls/api_v1.py
+++ wselfedu/urls/api_v1.py
@@ -3,12 +3,13 @@
 from django.urls import include, path
 
 from apps.lang.api.v1.urls import router as lang_router
+from apps.main.api.v1.urls import router as main_router
 from apps.math.api.v1.urls import router as math_router
 from apps.users.api.v1.urls import router as users_router
 
 urlpatterns = [
-    path('main/', include(users_router.urls)),
-    path('users/', include(users_router.urls)),
-    path('math/', include(math_router.urls)),
     path('lang/', include(lang_router.urls)),
+    path('main/', include(main_router.urls)),
+    path('math/', include(math_router.urls)),
+    path('users/', include(users_router.urls)),
 ]
