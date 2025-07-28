91d26c041754ef8795caac3c784c6e957ebdb228
Sergei Mikurov <454004@mail.ru>
2025-07-28 20:01:32 +0500

    feat(utils/db): Add db user creation

--- utils/load.py
+++ utils/load.py
@@ -1,6 +1,7 @@
-"""Defines utility methods to load data."""
+"""Defines utility methods to load or get data."""
 
 import os
+import re
 
 
 def get_boolean_value(env: str) -> bool:
@@ -11,3 +12,17 @@ def get_boolean_value(env: str) -> bool:
         DEBUG = getenv_bool('DEBUG')
     """
     return os.getenv(env, 'False').lower() in ('true', 't', '1')
+
+
+def get_db_user() -> str:
+    """Get database user."""
+    db_user = os.getenv('DB_USER')
+
+    if db_user is None:
+        raise ValueError('Database user not configured')
+
+    # Validate user name
+    if not re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', db_user):
+        raise ValueError(f'Invalid database username: {db_user}')
+
+    return db_user
