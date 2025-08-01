408c8213f848e3559c18f67452a69f6b80c0aa36
Sergei Mikurov <454004@mail.ru>
2025-08-01 13:29:16 +0500

    feat(apps/core): Add home page
    - Added 'base.html' template into 'templates'
    - Added 'home.html' template into 'templates'
    - Added 'urls' module to Core app
    - Added '/' url path into Core app web urls with `TemplateView'

---
+++ apps/core/urls.py
@@ -0,0 +1,9 @@
+"""Defines Core app web urls."""
+
+from django.urls import path
+
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+]
--- wselfedu/urls/web.py
+++ wselfedu/urls/web.py
@@ -1,9 +1,10 @@
 """Defines apps web url paths."""
 
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('apps.core.urls')),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
