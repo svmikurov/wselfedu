26d2de5d166de946d6482b6e9d8e69bddc7322f3
Sergei Mikurov <454004@mail.ru>
2025-08-13 00:27:08 +0500

    docs(apps/core): Add serializer data exemple
    - Added example data for `AssignedMentorSerializer`

--- apps/core/api/v1/serializers/exercises.py
+++ apps/core/api/v1/serializers/exercises.py
@@ -1,11 +1,30 @@
 """Defines serializers for assigned exercises."""
 
 from django.db.models import QuerySet
+from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 
 from apps.users.models import ExerciseAssigned
 
 
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            'Valid data',
+            value={
+                'mentorship_id': 1,
+                'exercise_id': 1,
+                'exercise_name': 'Таблица умножения',
+                'count': 20,
+                'award': 3,
+                'is_active': True,
+                'is_daily': True,
+                'expiration': '2025-08-12T00:00:00Z',
+            },
+            description='Example of assigned exercise',
+        )
+    ]
+)
 class AssignedMentorSerializer(
     serializers.Serializer[QuerySet[ExerciseAssigned]]
 ):
@@ -24,7 +43,7 @@ class AssignedMentorSerializer(
     award = serializers.IntegerField(read_only=True)
     is_active = serializers.BooleanField(read_only=True)
     is_daily = serializers.BooleanField(read_only=True)
-    expiration = serializers.DateField(read_only=True)
+    expiration = serializers.DateTimeField(read_only=True)
 
     class Meta:
         """Serializer configration."""
