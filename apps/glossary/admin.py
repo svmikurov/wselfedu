67767348f6fb1ea9eba56c7c380a5593ff829f74
Sergei Mikurov <454004@mail.ru>
2025-09-26 19:28:06 +0500

    feat(glossary): Add model administration
    - Added `Term` model administration

--- apps/glossary/admin.py
+++ apps/glossary/admin.py
@@ -1,1 +1,12 @@
 """Glossary app model management."""
+
+from django.contrib import admin
+
+from apps.glossary.models import Term
+
+
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    """Term model administration."""
+
+    list_display = ['user', 'name', 'definition']
