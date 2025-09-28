2f8eca8ad0a6fcd600103fb28c1f46ba6d08a20c
Sergei Mikurov <454004@mail.ru>
2025-09-28 22:18:45 +0500

    feat(lang/presenters): Add presenter
    - Added `EnglishTranslationPresenter` simple presenter to get translations

---
+++ apps/lang/presenters/__init__.py
@@ -0,0 +1,11 @@
+"""Language application presenters."""
+
+__all__ = [
+    'EnglishTranslationPresenter',
+    'TranslationParams',
+]
+
+from .translation import (
+    EnglishTranslationPresenter,
+    TranslationParams,
+)
