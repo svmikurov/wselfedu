b307d8d2d64b2f94346e950f0010ac6f18e1d86e
Sergei Mikurov <454004@mail.ru>
2025-09-28 22:22:48 +0500

    feat(lang/di): Add DI provider
    - Added provider for `EnglishTranslationPresenter` to get list of
      english translations

--- apps/lang/di_container.py
+++ apps/lang/di_container.py
@@ -3,6 +3,7 @@
 from dependency_injector import containers, providers
 
 from .orchestrators import CreateEnglishTranslation
+from .presenters import EnglishTranslationPresenter
 
 
 class LanguageContainer(containers.DeclarativeContainer):
@@ -10,4 +11,7 @@ class LanguageContainer(containers.DeclarativeContainer):
 
     translation_orchestrator = providers.Factory(
         CreateEnglishTranslation,
+    )
+    translation_presenter = providers.Factory(
+        EnglishTranslationPresenter,
     )
