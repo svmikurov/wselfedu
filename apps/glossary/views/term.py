fa02c7a421acccaa8af2c9a1dc8f5bcc18ad9888
Sergei Mikurov <454004@mail.ru>
2025-09-28 15:05:42 +0500

    refactor(glossary/views): Update Term views via mixin
    - Updated Term views via `OwnershipRequiredMixin` mixin

--- apps/glossary/views/term.py
+++ apps/glossary/views/term.py
@@ -1,25 +1,21 @@
 """Term view."""
 
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
 from django.http.response import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    DetailView,
-    ListView,
-    UpdateView,
-)
+from django.views import generic
 
-from ...users.models import CustomUser
+from apps.core.generic.views.auth import OwnershipRequiredMixin
+from apps.users.models import CustomUser
+
 from ..models import Term
 
 
 class TermCreateView(
     LoginRequiredMixin,
-    CreateView,  # type: ignore[type-arg]
+    generic.CreateView,  # type: ignore[type-arg]
 ):
     """Create Term view."""
 
@@ -33,53 +29,39 @@ class TermCreateView(
         return super().form_valid(form)
 
 
-class TermDetailView(
-    UserPassesTestMixin,
-    LoginRequiredMixin,
-    DetailView,  # type: ignore[type-arg]
-):
-    """Detail Term view."""
-
-    model = Term
-    _object: Term | None = None
-
-    def get_object(self, queryset: QuerySet[Term] | None = None) -> Term:
-        """Cache the object after the first query."""
-        if self._object is None:
-            self._object = super().get_object(queryset)
-        return self._object
-
-    def test_func(self) -> bool:
-        """Check that user is object owner."""
-        return bool(self.request.user == self.get_object().user)
-
-
 class TermUpdateView(
-    UserPassesTestMixin,
-    LoginRequiredMixin,
-    UpdateView,  # type: ignore[type-arg]
+    OwnershipRequiredMixin[Term],
+    generic.UpdateView,  # type: ignore[type-arg]
 ):
     """Update Term view."""
 
     model = Term
     fields = ['name', 'definition']
     success_url = reverse_lazy('glossary:term_list')
-    _object: Term | None = None
 
-    def get_object(self, queryset: QuerySet[Term] | None = None) -> Term:
-        """Cache the object after the first query."""
-        if self._object is None:
-            self._object = super().get_object(queryset)
-        return self._object
 
-    def test_func(self) -> bool:
-        """Check that user is object owner."""
-        return bool(self.request.user == self.get_object().user)
+class TermDeleteView(
+    OwnershipRequiredMixin[Term],
+    generic.DeleteView,  # type: ignore[type-arg]
+):
+    """Delete Term view."""
+
+    model = Term
+    success_url = reverse_lazy('glossary:term_list')
+
+
+class TermDetailView(
+    OwnershipRequiredMixin[Term],
+    generic.DetailView,  # type: ignore[type-arg]
+):
+    """Detail Term view."""
+
+    model = Term
 
 
 class TermListView(
     LoginRequiredMixin,
-    ListView,  # type: ignore[type-arg]
+    generic.ListView,  # type: ignore[type-arg]
 ):
     """List Term view."""
 
@@ -91,25 +73,3 @@ class TermListView(
         if isinstance(self.request.user, CustomUser):
             return Term.objects.filter(user=self.request.user)
         return Term.objects.none()
-
-
-class TermDeleteView(
-    LoginRequiredMixin,
-    UserPassesTestMixin,
-    DeleteView,  # type: ignore[type-arg]
-):
-    """Delete Term view."""
-
-    model = Term
-    success_url = reverse_lazy('glossary:term_list')
-    _object: Term | None = None
-
-    def get_object(self, queryset: QuerySet[Term] | None = None) -> Term:
-        """Cache the object after the first query."""
-        if self._object is None:
-            self._object = super().get_object(queryset)
-        return self._object
-
-    def test_func(self) -> bool:
-        """Check that user is object owner."""
-        return bool(self.request.user == self.get_object().user)
