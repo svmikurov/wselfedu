23ebee3d684ae2927959f6ec3f0cee0eab2fb5d6
Sergei Mikurov <454004@mail.ru>
2025-12-06 03:51:36 +0500

    chore(core/storage): Update init module export
    - Added cache ABC to __init__.py export

--- apps/core/storage/clients/__init__.py
+++ apps/core/storage/clients/__init__.py
@@ -1,7 +1,11 @@
 """Contains storage clients."""
 
 __all__ = [
+    # ABC
+    'CacheABC',
+    # Implementation
     'DjangoCache',
 ]
 
 from .django_cache import DjangoCache
+from .iabc import CacheABC
