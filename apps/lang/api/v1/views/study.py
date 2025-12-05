40f93f2d03da61c0e197bb21b31422f9f87645a8
Sergei Mikurov <454004@mail.ru>
2025-12-06 04:10:07 +0500

    refactor(lang/api): Update word study view

--- apps/lang/api/v1/views/study.py
+++ apps/lang/api/v1/views/study.py
@@ -1,11 +1,13 @@
 """Word study API view."""
 
 import logging
-import uuid
 from http import HTTPStatus
 
 from dependency_injector import wiring
-from drf_spectacular.utils import OpenApiExample, extend_schema
+from drf_spectacular.utils import (
+    OpenApiExample,
+    extend_schema,
+)
 from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.request import Request
@@ -14,7 +16,6 @@ from rest_framework.viewsets import ViewSet
 
 import di
 from apps.core.api import renderers
-from apps.lang import types
 from apps.lang.repos.abc import WordStudyParamsRepositoryABC
 from apps.lang.services.abc import (
     WordPresentationServiceABC,
@@ -33,10 +34,24 @@ class WordStudyViewSet(ViewSet):
     renderer_classes = [renderers.WrappedJSONRenderer]
     permission_classes = [permissions.IsAuthenticated]
 
+    # -----------------------
+    # Word study presentation
+    # -----------------------
+
     @extend_schema(
-        summary='Word study through the presentation',
-        request={status.HTTP_200_OK: ser.WordStudyParamsChoicesSerializer},
-        responses={status.HTTP_200_OK: ser.WordStudyCaseSerializer},
+        summary='Word study presentation',
+        request=ser.WordParametersSerializer,
+        responses={
+            status.HTTP_200_OK: ser.WordStudyCaseSerializer,
+        },
+        examples=[
+            OpenApiExample(
+                'Request data example',
+                value=examples.WORD_STUDY_PARAMETERS_DATA,
+                request_only=True,
+                status_codes=[HTTPStatus.OK],
+            ),
+        ],
         tags=['Lang'],
     )
     @action(methods=['post'], detail=False)
@@ -47,36 +62,26 @@ class WordStudyViewSet(ViewSet):
             di.MainContainer.lang.word_presentation_service,
         ],
     ) -> Response:
-        """Render the word presentation."""
-        study_params = ser.WordStudyInitialChoicesSerializer(data=request.data)
-        study_params.is_valid(raise_exception=True)
-        try:
-            study_data = presentation_services.get_presentation_case(
-                self.request.user,  # type: ignore[arg-type]
-                study_params.validated_data,
-            )
-        except LookupError:
-            return self._render_no_words()
+        """Render the Word study presentation case."""
+        parameters = ser.WordParametersSerializer(data=request.data)
+        parameters.is_valid(raise_exception=True)
+        study_data = presentation_services.get_presentation_case(
+            self.request.user,  # type: ignore[arg-type]
+            parameters.validated_data,
+        )
         return Response(ser.WordStudyCaseSerializer(study_data).data)
 
-    # TODO: Move to service
-    def _render_no_words(self) -> Response:
-        """Render no words to study for request params."""
-        no_data: types.PresentationCaseT = {
-            'case_uuid': uuid.uuid4(),
-            'definition': '',
-            'explanation': '',
-            'info': None,
-        }
-        return Response(ser.WordStudyCaseSerializer(no_data).data)
+    # ---------------------
+    # Word study parameters
+    # ---------------------
 
     @extend_schema(
-        summary='Word study params',
-        responses={HTTPStatus.OK: ser.WordStudyPresentationParamsSerializer},
+        summary='Word study parameters',
+        responses={HTTPStatus.OK: ser.SetParametersSerializer},
         examples=[
             OpenApiExample(
                 'Success response data example',
-                value=examples.WORD_STUDY_PARAMETERS_DATA,
+                value=examples.SET_WORD_STUDY_PARAMETERS_DATA,
                 response_only=True,
                 status_codes=[HTTPStatus.OK],
             ),
@@ -84,35 +89,37 @@ class WordStudyViewSet(ViewSet):
         tags=['Lang'],
     )
     @action(methods=['get'], detail=False)
-    def params(
+    def parameters(
         self,
         request: Request,
         repository: WordStudyParamsRepositoryABC = wiring.Provide[
             di.MainContainer.lang.params_repo,
         ],
     ) -> Response:
-        """Render initial Word study params."""
+        """Render initial Word study parameters."""
         payload = repository.fetch(self.request.user)  # type: ignore[arg-type]
-        return Response(
-            ser.WordStudyPresentationParamsSerializer(payload).data
-        )
+        return Response(ser.SetParametersSerializer(payload).data)
+
+    # ----------------------------
+    # Update word study parameters
+    # ----------------------------
 
     # TODO: Feat error handling for response
     @extend_schema(
-        summary='Word study params update',
-        request=ser.UpdateParametersSerializer,
+        summary='Update word study parameters',
+        request=ser.StudyParametersSerializer,
         tags=['Lang'],
     )
-    @action(methods=['put'], detail=False, url_path='params/update')
-    def update_params(
+    @action(methods=['put'], detail=False, url_path='parameters/update')
+    def update_parameters(
         self,
         request: Request,
         repository: WordStudyParamsRepositoryABC = wiring.Provide[
             di.MainContainer.lang.params_repo,
         ],
     ) -> Response:
-        """Update initial Word study params."""
-        params = ser.UpdateParametersSerializer(data=request.data)
+        """Update initial Word study parameters."""
+        params = ser.StudyParametersSerializer(data=request.data)
         params.is_valid()
         try:
             payload = repository.update(
@@ -124,9 +131,11 @@ class WordStudyViewSet(ViewSet):
                 data={'detail': str(exc)},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        return Response(
-            ser.WordStudyPresentationParamsSerializer(payload).data
-        )
+        return Response(ser.SetParametersSerializer(payload).data)
+
+    # --------------------------
+    # Update word study progress
+    # --------------------------
 
     # TODO: Add status codes with exceptions
     # TODO: Update response status to 200?
