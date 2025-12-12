03b63ea829ad3c6b095efb9401fc7688f4c10df9
Sergei Mikurov <454004@mail.ru>
2025-12-13 01:15:59 +0500

    feat(core/generic): Add ownership mixin
    - Added mixin provides model object owner

--- apps/core/generic/views/htmx.py
+++ apps/core/generic/views/htmx.py
@@ -11,6 +11,14 @@ from django.views.generic import DeleteView
 from apps.users.models import Person
 
 
+class SimpleOwnerMixin:
+    """Mixin provides object owner."""
+
+    def _get_owner(self) -> Person:
+        owner: Person = self.get_object().user  # type: ignore[attr-defined]
+        return owner
+
+
 class HtmxDeleteView(
     LoginRequiredMixin,
     UserPassesTestMixin,
@@ -39,3 +47,7 @@ class HtmxDeleteView(
 
     def _get_owner(self) -> Person:
         raise NotImplementedError('Must implement `_get_owner()` in subclass')
+
+
+class HtmxOwnerDeleteView(SimpleOwnerMixin, HtmxDeleteView):
+    """Delete the object with HTMX request by owner."""
