f1c84fbeef73b61802c2dfe18804616301dfd4c3
Sergei Mikurov <454004@mail.ru>
2025-12-13 19:58:41 +0500

    feat(lang/views): Improve translation views
    - Added extra context
    - Added filters

--- apps/lang/views/translation.py
+++ apps/lang/views/translation.py
@@ -2,20 +2,22 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
+from django_filters import views as filter_views
 
 from apps.core.generic.views import auth, htmx
 
-from .. import forms, models
-from . import mixins
+from .. import filters, forms, models
+from . import context, mixins
 
 if TYPE_CHECKING:
     from django.db.models import QuerySet
     from django.http import HttpResponse
+    from django_filters.filterset import FilterSet
 
 
 class EnglishTranslationCreateView(
@@ -25,33 +27,41 @@ class EnglishTranslationCreateView(
 ):
     """English translation create view."""
 
-    form_class = forms.EnglishTranslationCreateForm
-    template_name = 'lang/translation_form.html'
+    form_class = forms.EnglishCreateForm
+    template_name = 'lang/form.html'
     success_url = reverse_lazy('lang:translation_english_create')
+    extra_context = context.TRANSLATION_CONTEXT['create']
 
-    def form_valid(
-        self,
-        form: forms.EnglishTranslationCreateForm,
-    ) -> HttpResponse:
+    def form_valid(self, form: forms.EnglishCreateForm) -> HttpResponse:
         """Save translation."""
-        self.repository.create(self.user, **form.cleaned_data)
+        self.repository.create(user=self.user, **form.cleaned_data)
         return super().form_valid(form)
 
 
 class EnglishTranslationListView(
     mixins.TranslationViewMixin,
     LoginRequiredMixin,
-    generic.ListView,  # type: ignore[type-arg]
+    filter_views.FilterView,
 ):
     """English translation list view."""
 
     template_name = 'lang/translation_english_list.html'
+    filterset_class = filters.TranslationFilter
     context_object_name = 'translations'
     paginate_by = 20
+    extra_context = context.TRANSLATION_CONTEXT['list']
 
     def get_queryset(self) -> QuerySet[models.EnglishTranslation]:
         """Get translation queryset."""
         return self.repository.get_translations(self.user)
+
+    def get_filterset_kwargs(
+        self, filterset_class: type[FilterSet]
+    ) -> dict[str, Any]:
+        """Add user to filter."""
+        kwargs = super().get_filterset_kwargs(filterset_class)
+        kwargs['user'] = self.user
+        return kwargs
 
 
 class EnglishTranslationUpdateView(
@@ -62,14 +72,12 @@ class EnglishTranslationUpdateView(
     """English translation update view."""
 
     model = models.EnglishTranslation
-    form_class = forms.EnglishTranslationUpdateForm
-    template_name = 'lang/translation_form.html'
+    form_class = forms.EnglishUpdateForm
+    template_name = 'lang/form.html'
     success_url = reverse_lazy('lang:translation_english_list')
+    extra_context = context.TRANSLATION_CONTEXT['update']
 
-    def form_valid(
-        self,
-        form: forms.EnglishTranslationUpdateForm,
-    ) -> HttpResponse:
+    def form_valid(self, form: forms.EnglishUpdateForm) -> HttpResponse:
         """Save translation."""
         instance = self.get_object()
         self.repository.update(self.user, instance, **form.cleaned_data)
