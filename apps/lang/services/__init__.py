d5f419c89bfaa54260881b33a0336d3dc290c201
Sergei Mikurov <454004@mail.ru>
2025-12-15 03:52:13 +0500

    chore(lang/services): Improve ABC export

--- apps/lang/services/__init__.py
+++ apps/lang/services/__init__.py
@@ -1,10 +1,11 @@
 """Language discipline services."""
 
 __all__ = [
+    'WordPresentationServiceABC',
     'UpdateWordProgressService',
     'WordPresentationService',
 ]
 
-
+from .abc import WordPresentationServiceABC
 from .presentation import WordPresentationService
 from .progress import UpdateWordProgressService
