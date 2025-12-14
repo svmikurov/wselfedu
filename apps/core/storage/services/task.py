4016bf6eb29301d7888c98f7c2365b07b806ae73
Sergei Mikurov <454004@mail.ru>
2025-12-14 21:47:53 +0500

    feat(core/storage): Improve task storage
    - Add default ttl
    - Make task storage as generic

--- apps/core/storage/services/task.py
+++ apps/core/storage/services/task.py
@@ -2,36 +2,40 @@
 
 import logging
 import uuid
-
-from typing_extensions import override
-from wse_exercises import TaskT
+from typing import Generic, TypeVar, override
 
 from ..clients.django_cache import DjangoCache
 from .iabc import TaskStorageABC
 
 logger = logging.getLogger(__name__)
 
+T = TypeVar('T')
 
-class TaskStorage(TaskStorageABC[TaskT]):
+
+class TaskStorage(Generic[T], TaskStorageABC[T]):
     """Django cache storage task."""
+
+    DEFAULT_TTL = 3600
 
     def __init__(
         self,
-        storage: DjangoCache[TaskT],
+        storage: DjangoCache[T],
+        ttl: int | None = None,
     ) -> None:
         """Construct the storage."""
         self._storage = storage
+        self.ttl = ttl or self.DEFAULT_TTL
 
     @override
-    def save_task(self, obj: TaskT, ttl: int | None = None) -> uuid.UUID:
+    def save_task(self, obj: T, ttl: int | None = None) -> uuid.UUID:
         """Save task to cache."""
         if ttl is None:
-            ttl = 3600
+            ttl = self.ttl
         uid = self._storage.set(obj, ttl)
         return uid
 
     @override
-    def retrieve_task(self, uid: uuid.UUID) -> TaskT:
+    def retrieve_task(self, uid: uuid.UUID) -> T:
         """Retrieve task."""
         try:
             task = self._storage.pop(uid)
