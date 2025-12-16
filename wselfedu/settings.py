6d8d757649082361ccedd7dab99c4e8be57e245d
Sergei Mikurov <454004@mail.ru>
2025-12-16 13:48:35 +0500

    feat(settings): Add session authentication

--- wselfedu/settings.py
+++ wselfedu/settings.py
@@ -163,8 +163,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # djangorestframework-simplejwt
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     # Pagination
     # https://www.django-rest-framework.org/tutorial/quickstart/#pagination
