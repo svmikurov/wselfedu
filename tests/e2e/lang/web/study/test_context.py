39b1f62ecf342e57b9cff65903733143a75c36f8
Sergei Mikurov <454004@mail.ru>
2025-12-19 02:55:41 +0500

    test(e2e/lang): Add expected data
    - Added expected study settings for
      - default settings
      - user saved settings

--- tests/e2e/lang/web/study/test_context.py
+++ tests/e2e/lang/web/study/test_context.py
@@ -14,6 +14,9 @@ if TYPE_CHECKING:
     from django.http.response import HttpResponse
     from django.test import Client
 
+    from apps.lang import types
+
+# TODO: Fix type ignore
 
 STUDY_URL_PATH = reverse('lang:translation_english_study')
 STUDY_CASE_URL_PATH = reverse('lang:translation_english_study_case')
@@ -23,11 +26,43 @@ STUDY_CASE_URL_PATH = reverse('lang:translation_english_study_case')
 #      data-question="{{ task.question_timeout }}"
 #       ...>
 SETTINGS_TAG_ID = 'settings-block'
-EXPECTED_STUDY_SETTINGS = {
-    'answer': '3',
-    'question': '3',
+DEFAULT_SETTINGS = {
+    # Translation parameters
+    'category': '',
+    'mark': '',
+    'source': '',
+    'start-period': '',
+    'end-period': '',
+    # Translation settings
+    'translation-order': '',
+    'word-count': '',
+    # Presentation settings
+    'answer': str(models.PresentationSettings.DEFAULT_TIMEOUT),
+    'question': str(models.PresentationSettings.DEFAULT_TIMEOUT),
     'url': STUDY_CASE_URL_PATH,
 }
+
+
+@pytest.fixture
+def user_settings(
+    parameters_db_data: types.SetStudyParameters,
+) -> dict[str, Any]:
+    """Provide user settings."""
+    return {
+        # Translation parameters
+        'category': str(parameters_db_data['category']['id']),  # type: ignore[index]
+        'mark': str(parameters_db_data['mark']['id']),  # type: ignore[index]
+        'source': str(parameters_db_data['word_source']['id']),  # type: ignore[index]
+        'start-period': str(parameters_db_data['start_period']['id']),  # type: ignore[index]
+        'end-period': str(parameters_db_data['end_period']['id']),  # type: ignore[index]
+        # Translation settings
+        'translation-order': '',
+        'word-count': '',
+        # Presentation settings
+        'answer': str(parameters_db_data['answer_timeout']),
+        'question': str(parameters_db_data['question_timeout']),
+        'url': STUDY_CASE_URL_PATH,
+    }
 
 
 def extract_attributes(response: HttpResponse, tag_id: str) -> dict[str, Any]:
@@ -54,7 +89,20 @@ class TestSettingsContext:
 
         # Assert
         data = extract_attributes(response, SETTINGS_TAG_ID)  # type: ignore[arg-type]
-        assert data == EXPECTED_STUDY_SETTINGS
+        assert data == DEFAULT_SETTINGS
+
+    def test_contains_settings(
+        self,
+        auth_client: Client,
+        user_settings: types.SetStudyParameters,
+    ) -> None:
+        """Context contains current study settings data."""
+        # Act
+        response = auth_client.get(STUDY_URL_PATH)
+
+        # Assert
+        data = extract_attributes(response, SETTINGS_TAG_ID)  # type: ignore[arg-type]
+        assert data == user_settings
 
 
 @pytest.mark.django_db
