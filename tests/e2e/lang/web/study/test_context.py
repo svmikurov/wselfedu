9e5d3cf98f91ca61de59a94fbebd3ea84216189e
Sergei Mikurov <454004@mail.ru>
2025-12-19 01:33:09 +0500

    test(e2e/lang): Add test
    - Added translation study settings context tests

--- tests/e2e/lang/web/study/test_context.py
+++ tests/e2e/lang/web/study/test_context.py
@@ -2,24 +2,73 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pytest
+from bs4 import BeautifulSoup
 from django.urls import reverse
 
 from apps.lang import models
 
 if TYPE_CHECKING:
+    from django.http.response import HttpResponse
     from django.test import Client
 
     from apps.users.models import Person
 
-URL_PATH = reverse('lang:translation_english_study_case')
+
+STUDY_URL_PATH = reverse('lang:translation_english_study')
+STUDY_CASE_URL_PATH = reverse('lang:translation_english_study_case')
+
+# Study settings are stored in the tag data-* attributes
+# <div id="..."
+#      data-question="{{ task.question_timeout }}"
+#       ...>
+SETTINGS_TAG_ID = 'settings-block'
+EXPECTED_STUDY_SETTINGS = {
+    'answer': '3',
+    'question': '3',
+    'url': STUDY_CASE_URL_PATH,
+}
+
+
+def extract_attributes(response: HttpResponse, tag_id: str) -> dict[str, Any]:
+    """Extract data-* attributes."""
+    soup = BeautifulSoup(response.content, 'html.parser')
+    tag = soup.find('div', {'id': tag_id})
+    if tag is None:
+        raise ValueError(f'Page content have no {tag_id!r} ID')
+    return {
+        key[5:]: value
+        for key, value in tag.attrs.items()
+        if key.startswith('data-')
+    }
 
 
 @pytest.mark.django_db
-class TestContext:
-    """Translation study render context tests."""
+class TestSettingsContext:
+    """Translation study settings context tests."""
+
+    def test_contains_default_settings(
+        self,
+        user: Person,
+        client: Client,
+    ) -> None:
+        """Context contains current study settings default data."""
+        # Arrange
+        client.force_login(user)
+
+        # Act
+        response = client.get(STUDY_URL_PATH)
+
+        # Assert
+        data = extract_attributes(response, SETTINGS_TAG_ID)  # type: ignore[arg-type]
+        assert data == EXPECTED_STUDY_SETTINGS
+
+
+@pytest.mark.django_db
+class TestInfoContext:
+    """Translation study info context tests."""
 
     def test_info(
         self,
@@ -27,12 +76,12 @@ class TestContext:
         client: Client,
         english_progress: models.EnglishProgress,
     ) -> None:
-        """Translation info test."""
+        """Test info content."""
         # Arrange
         client.force_login(user)
 
         # Act
-        response = client.get(URL_PATH)
+        response = client.get(STUDY_CASE_URL_PATH)
 
         # Assert
         assert 'info' in response.context.keys()
