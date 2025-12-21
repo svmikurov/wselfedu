4f7e5f7cf91b227c0fd6b80e04b0e20047434f2d
Sergei Mikurov <454004@mail.ru>
2025-12-21 08:32:48 +0500

    feat(lang/views): Add base view
    - Added Study case base view

--- apps/lang/views/base.py
+++ apps/lang/views/base.py
@@ -7,11 +7,12 @@ from typing import TYPE_CHECKING
 from dependency_injector.wiring import Provide, inject
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from django.views.generic import edit
 
 from apps.core import views as core_views
 from di import MainContainer
 
-from .. import repositories as repos
+from .. import services
 
 if TYPE_CHECKING:
     from django.http.request import HttpRequest
@@ -25,25 +26,58 @@ class SettingsBaseView(
 ):
     """Settings base view."""
 
-    _repository: repos.StudyParametersRepositoryABC | None = None
+    _service: services.StudySettingsServiceABC | None = None
 
     @inject
     def dispatch(
         self,
         request: HttpRequest,
         *args: object,
-        repository: repos.StudyParametersRepository = Provide[
-            MainContainer.lang.parameters_repository,
+        service: services.StudySettingsServiceABC = Provide[
+            MainContainer.lang.settings_service,
         ],
         **kwargs: object,
     ) -> HttpResponseBase:
         """Inject settings repository."""
-        self._repository = repository
+        self._service = service
         return super().dispatch(request, *args, **kwargs)
 
     @property
-    def repository(self) -> repos.StudyParametersRepository:
+    def service(self) -> services.StudySettingsServiceABC:
         """Get settings repository."""
-        if not isinstance(self._repository, repos.StudyParametersRepository):
+        if not isinstance(self._service, services.StudySettingsServiceABC):
             raise AttributeError('Repository not initialized')
-        return self._repository
+        return self._service
+
+
+class CaseBaseView(
+    core_views.UserRequestMixin,
+    LoginRequiredMixin,
+    edit.BaseFormView,  # type: ignore[type-arg]
+):
+    """Study case base view."""
+
+    _service: services.WordPresentationServiceABC | None = None
+
+    @inject
+    def dispatch(
+        self,
+        request: HttpRequest,
+        *args: object,
+        service: services.WordPresentationServiceABC = Provide[
+            MainContainer.lang.word_presentation_service,
+        ],
+        **kwargs: object,
+    ) -> HttpResponseBase:
+        """Inject case service."""
+        self._service = service
+        if request.method != 'POST':
+            return self.http_method_not_allowed(request)
+        return super().dispatch(request, *args, **kwargs)
+
+    @property
+    def service(self) -> services.WordPresentationServiceABC:
+        """Get case service."""
+        if not isinstance(self._service, services.WordPresentationServiceABC):
+            raise AttributeError('Service not initialized')
+        return self._service
