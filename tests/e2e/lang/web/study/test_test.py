1e710ed0c70411f61cf41393086524a80072c36c
Sergei Mikurov <454004@mail.ru>
2025-12-28 16:00:43 +0500

    test(e2e/lang): Add test
    - Added 'Translation study test' response status success test

---
+++ tests/e2e/lang/web/study/test_test.py
@@ -0,0 +1,47 @@
+"""Translation study test exercise tests."""
+
+from __future__ import annotations
+
+from http import HTTPStatus
+from typing import TYPE_CHECKING
+
+import pytest
+from django.urls import reverse_lazy
+
+if TYPE_CHECKING:
+    from django.test import Client
+
+TRANSLATION_TEST_URL = reverse_lazy('lang:translation_english_test')
+TRANSLATION_TEST_CASE_URL = reverse_lazy('lang:translation_english_test_case')
+
+
+@pytest.mark.django_db
+class TestHttpStatus:
+    """Translation test exercise http status tests."""
+
+    def test_render_template_success(
+        self,
+        auth_client: Client,
+    ) -> None:
+        """Translation test exercise http status is success."""
+        # Act
+        response = auth_client.get(TRANSLATION_TEST_URL)
+
+        # Assert
+        assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+class TestCaseHttpStatus:
+    """Translation test case http status tests."""
+
+    def test_render_template_success(
+        self,
+        auth_client: Client,
+    ) -> None:
+        """Translation test case http status is success."""
+        # Act
+        response = auth_client.post(TRANSLATION_TEST_CASE_URL)
+
+        # Assert
+        assert response.status_code == HTTPStatus.OK
