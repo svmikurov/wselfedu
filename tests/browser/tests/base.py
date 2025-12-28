9ccb1480894c71336f081a3519e74de98f7ae821
Sergei Mikurov <454004@mail.ru>
2025-12-28 13:35:47 +0500

    test(browser): Fix base POM test
    - Fixe type annotation
    - Updated docstings

--- tests/browser/tests/base.py
+++ tests/browser/tests/base.py
@@ -1,28 +1,27 @@
-"""Base test for POM browser testing."""
+"""Base Playwright POM test."""
+
+from __future__ import annotations
 
 import os
-from typing import Generic, Type, TypeVar
+from typing import TYPE_CHECKING
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from playwright.sync_api import Browser, Page, Playwright, sync_playwright
+from playwright.sync_api import sync_playwright
 
-from tests.browser.pages.base import BasePage
+if TYPE_CHECKING:
+    from playwright.sync_api import Browser, Page, Playwright
 
-T = TypeVar('T', bound=BasePage)
 
-
-class BaseTest(StaticLiveServerTestCase, Generic[T]):
-    """Base test for POV browser testing."""
+class BaseTest(StaticLiveServerTestCase):
+    """Base Playwright POM test."""
 
     playwright: Playwright
     browser: Browser
     _page: Page
 
-    page_type: Type[T]
-
     @classmethod
     def setUpClass(cls) -> None:
-        """Set browser."""
+        """Start the Playwright and launch the browser."""
         os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = 'true'
         super().setUpClass()
         cls.playwright = sync_playwright().start()
@@ -30,13 +29,11 @@ class BaseTest(StaticLiveServerTestCase, Generic[T]):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        """Close browser."""
+        """Stop the playwright and close the browser."""
         super().tearDownClass()
         cls.browser.close()
         cls.playwright.stop()
 
     def setUp(self) -> None:
         """Set up page with base url."""
-        self._page = self.browser.new_page(
-            base_url=str(self.live_server_url),
-        )
+        self._page = self.browser.new_page(base_url=str(self.live_server_url))
