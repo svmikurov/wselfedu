f1af61ae6cc1f5191777a1bc8402ceb3b9cc259c
Sergei Mikurov <454004@mail.ru>
2025-12-30 17:32:22 +0500

    feat(lang/validators): Add validator
    - Added web request translation study test validator
    - Added request data type annotation

--- apps/lang/validators/web_validator.py
+++ apps/lang/validators/web_validator.py
@@ -4,16 +4,27 @@ from typing import Any
 
 from .. import schemas, types
 
+type RequestData = dict[str, Any]
+
 
 class WebPresentationValidator(
-    types.Validator[dict[str, Any], schemas.PresentationRequest]
+    types.Validator[RequestData, schemas.PresentationRequest]
 ):
     """Web request presentation validator."""
 
     @classmethod
-    def validate(cls, raw_data: dict[str, Any]) -> schemas.PresentationRequest:
+    def validate(cls, raw_data: RequestData) -> schemas.PresentationRequest:
         """Validate the web request presentation data."""
         return schemas.PresentationRequest(
             parameters=schemas.ParametersSchema(**raw_data),
             settings=schemas.SettingsSchema(**raw_data),
         )
+
+
+class WebTestValidator(types.Validator[RequestData, schemas.TestRequestDTO]):
+    """Web request test validator."""
+
+    @classmethod
+    def validate(cls, raw_data: RequestData) -> schemas.TestRequestDTO:
+        """Validate the web request data."""
+        return schemas.TestRequestDTO(**raw_data)
