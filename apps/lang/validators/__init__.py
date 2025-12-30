bfe5f93af0522d1279f732ef35d925affd00b033
Sergei Mikurov <454004@mail.ru>
2025-12-30 15:32:58 +0500

    feat(lang/validators): Extend package export
    - Added web translation test exercise validator to package export

--- apps/lang/validators/__init__.py
+++ apps/lang/validators/__init__.py
@@ -6,7 +6,8 @@ Delete after refactoring is complete.
 __all__ = [
     'ApiPresentationValidator',
     'WebPresentationValidator',
+    'WebTestValidator',
 ]
 
 from .api_validator import ApiPresentationValidator
-from .web_validator import WebPresentationValidator
+from .web_validator import WebPresentationValidator, WebTestValidator
