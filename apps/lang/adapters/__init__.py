8d387e3972c3656725f96712fd6d204ff9b8c37d
Sergei Mikurov <454004@mail.ru>
2025-12-30 15:16:23 +0500

    feat(lang/adapters): Extend package export
    - Added web test adapter to package export

--- apps/lang/adapters/__init__.py
+++ apps/lang/adapters/__init__.py
@@ -3,7 +3,9 @@
 __all__ = [
     'WebPresentationAdapter',
     'ApiPresentationAdapter',
+    'WebTestAdapter',
 ]
 
 from .api_presentation import ApiPresentationAdapter
+from .test import WebTestAdapter
 from .web_presentation import WebPresentationAdapter
