a7af763c3bbfa1e983a8be158db7403a8015d7ad
Sergei Mikurov <454004@mail.ru>
2026-01-08 22:07:30 +0500

    refactor(lang/adapters): Allocate DTO
    - Allocated the language rule web DTO to a separate module

--- apps/lang/adapters/rule.py
+++ apps/lang/adapters/rule.py
@@ -1,77 +1,64 @@
 """Language rule web adapter."""
 
-from ..models import Rule, RuleExample
-from typing import Any
-from pydantic import BaseModel
 from django.db.models import QuerySet
 
+from .. import models
+from . import dto
+
 # HACK: Refactor the web rule DTO building
-
-
-class ClauseSchema(BaseModel):
-    """Language rule clauses web schema."""
-
-    content: str
-    examples: str
-    exception_content: str | None
-    exceptions: str
-
-
-class RuleSchema(BaseModel):
-    """Language rule web schema."""
-
-    title: str
-    clauses: list[ClauseSchema]
-    exceptions: str
 
 
 class WebRuleAdapter:
     """Language rule web adapter."""
 
     @classmethod
-    def to_response(cls, query: Rule) -> RuleSchema:
+    def to_response(cls, query: models.Rule) -> dto.RuleSchema:
         """Convert rule queryset to web representation context."""
-
         title = query.title
         clauses_qs = query.clauses
         exceptions_qs = query.exceptions
 
         clauses = []
         for clause in clauses_qs.all():
-            
             examples, exceptions = [], []
             for item in clause.examples.all():
-                if item.example_type == RuleExample.ExampleType.EXAMPLE:
+                if item.example_type == models.RuleExample.ExampleType.EXAMPLE:
                     examples.append(cls._create_example(item))
-                elif item.example_type == RuleExample.ExampleType.EXCEPTION:
+                elif (
+                    item.example_type
+                    == models.RuleExample.ExampleType.EXCEPTION
+                ):
                     exceptions.append(cls._create_example(item))
 
             clauses.append(
-                ClauseSchema(
+                dto.ClauseSchema(
                     content=clause.content,
                     examples=cls._join_examples(examples),
                     exception_content=clause.exception_content,
                     exceptions=cls._join_examples(exceptions),
                 )
             )
-            
-        return RuleSchema(
+
+        return dto.RuleSchema(
             title=title,
             clauses=clauses,
-            exceptions=cls._build_examples_string(exceptions_qs)
+            exceptions=cls._build_examples_string(exceptions_qs),
         )
 
     @classmethod
-    def _build_examples_string(cls, examples: QuerySet[Any]) -> str:
+    def _build_examples_string(cls, examples: QuerySet[models.Rule]) -> str:
         examples = [cls._create_example(item) for item in examples.all()]
 
         return cls._join_examples(examples)
 
     @staticmethod
-    def _create_example(example: QuerySet[Any]) -> str:
-        return f'{example.question_translation.english} - {example.answer_translation.english}'
-    
+    def _create_example(example: QuerySet[models.Rule]) -> str:
+        return (
+            f'{example.question_translation.english} - '
+            f'{example.answer_translation.english}'
+        )
+
     @staticmethod
     def _join_examples(items: list[str]) -> str:
-        count = 5
-        return ', '.join(items[:count])
+        example_count = 5
+        return ', '.join(items[:example_count])
