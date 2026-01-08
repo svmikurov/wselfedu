2053bc3c70e9e2ddc2572b7498aa8cc77911a1f4
Sergei Mikurov <454004@mail.ru>
2026-01-08 22:01:41 +0500

    feat(lang/adapters): Extend package export
    - Added to language adapter package export:
      - language rule web adapter ABC
      - language rule web adapter
      - language rule web DTO

--- apps/lang/adapters/__init__.py
+++ apps/lang/adapters/__init__.py
@@ -1,11 +1,24 @@
 """Language discipline API & WEB response adapters."""
 
+# REVIEW: Package export, export organization.
+
 __all__ = [
+    # Base
+    'WebRuleAdapterABC',
+    # Presentation
     'WebPresentationAdapter',
     'ApiPresentationAdapter',
+    # Test
     'WebTestAdapter',
+    # Rule
+    'WebRuleAdapter',
+    # DTO
+    'RuleSchema',
 ]
 
 from .api_presentation import ApiPresentationAdapter
+from .base import WebRuleAdapterABC
+from .dto import RuleSchema
+from .rule import WebRuleAdapter
 from .test import WebTestAdapter
 from .web_presentation import WebPresentationAdapter
