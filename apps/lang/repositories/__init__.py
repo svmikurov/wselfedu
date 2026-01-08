1476707575b1266f099246ff1fee2dec827575b3
Sergei Mikurov <454004@mail.ru>
2026-01-08 19:54:52 +0500

    feat(lang/repositories): Extend package export
    - Added rule repository ABC to repository package export

--- apps/lang/repositories/__init__.py
+++ apps/lang/repositories/__init__.py
@@ -2,6 +2,7 @@
 
 __all__ = [
     # ABC
+    'RuleRepositoryABC',
     'PresentationABC',
     'ProgressABC',
     'TranslationRepoABC',
@@ -12,6 +13,7 @@ __all__ = [
     'EnglishTranslation',
     'Progress',
     'TranslationRepository',
+    'RuleRepository',
     # Functions
     'get_period_delta',
 ]
@@ -19,10 +21,12 @@ __all__ = [
 from .abc import (
     PresentationABC,
     ProgressABC,
+    RuleRepositoryABC,
     StudyParametersRepositoryABC,
     TranslationRepoABC,
 )
 from .parameters import StudyParametersRepository
 from .presentation import EnglishTranslation, get_period_delta
 from .progress import Progress
+from .rule import RuleRepository
 from .translation import TranslationRepository
