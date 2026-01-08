78212139e27e2697bbfd3fb6d65e8a7e30fd667f
Sergei Mikurov <454004@mail.ru>
2026-01-08 20:07:11 +0500

    feat(lang/views): Prototype English rules view
    - Added initial implementation of language rule detail view
      with repository dependency injection
    - TODO: Extract repository injection to base view class

--- apps/lang/views/rule/rule.py
+++ apps/lang/views/rule/rule.py
@@ -1,12 +1,26 @@
 """Language rule views."""
 
+from functools import cached_property
+from typing import Any
+
+from dependency_injector.providers import Container
+from dependency_injector.wiring import Provide, inject
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Prefetch, QuerySet
+from django.db.models import QuerySet
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseBase
 from django.views import generic
 
+from apps.lang.adapters.rule import WebRuleAdapter, RuleSchema
+from apps.core.views.auth import UserRequestMixin
 from apps.lang import models
+from apps.lang.di import LanguageContainer
+from apps.lang.repositories import RuleRepositoryABC
+from di import MainContainer
 
 from ._data import CONTEXT
+
+CONTAINER: Container[LanguageContainer] = MainContainer.lang
 
 
 class RuleIndexView(generic.TemplateView):
@@ -22,32 +36,40 @@ class RuleCreateView(generic.TemplateView):
     template_name = 'lang/rule/create/index.html'
 
 
-class RuleDetailView(generic.DetailView):  # type: ignore[type-arg]
+class RuleDetailView(UserRequestMixin, generic.TemplateView):  # type: ignore[type-arg]
     """Language rule detail view."""
 
     template_name = 'lang/rule/detail/index.html'
     extra_context = CONTEXT['rule_detail']
-    context_object_name = 'rule'
-    model = models.Rule
 
-    def get_queryset(self) -> QuerySet[models.Rule]:
-        """Get rule clauses with examples and exceptions."""
-        examples_qs = models.RuleExample.objects.select_related(
-            'question_translation__english',
-            'answer_translation__english',
-        )
-        return (
-            super()
-            .get_queryset()
-            .prefetch_related(
-                Prefetch(
-                    'clauses',
-                    queryset=models.RuleClause.objects.prefetch_related(
-                        Prefetch('examples', queryset=examples_qs)
-                    ),
-                )
-            )
-        )
+    _repo: RuleRepositoryABC | None = None
+
+    @inject
+    def dispatch(
+        self,
+        request: HttpRequest,
+        *args: object,
+        repo: RuleRepositoryABC = Provide[CONTAINER.rule_repository],
+        **kwargs: object,
+    ) -> HttpResponseBase:
+        """Inject rule repository."""
+        self._repo = repo
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        """Add data to context."""
+        context = super().get_context_data(**kwargs)
+        context['rule'] = self.rule
+        return context
+
+    @cached_property
+    def rule(self) -> RuleSchema:
+        """Get rule repository."""
+        if self._repo is None:
+            raise AttributeError('Repository not initialized')
+        rule_q = self._repo.get_for_user(self.user, self.kwargs['pk'])
+        rule_dto = WebRuleAdapter.to_response(rule_q)
+        return rule_dto
 
 
 class RuleUpdateView(generic.TemplateView):
