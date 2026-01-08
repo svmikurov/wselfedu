f0be38217c0e05ffb13faa0f70147e20f53e5d53
Sergei Mikurov <454004@mail.ru>
2026-01-08 06:04:31 +0500

    feat(lang/views): Implement language rule list view

--- apps/lang/views/rule/rule.py
+++ apps/lang/views/rule/rule.py
@@ -1,12 +1,19 @@
 """Language rule views."""
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import QuerySet
 from django.views import generic
+
+from apps.lang import models
+
+from ._data import CONTEXT
 
 
 class RuleIndexView(generic.TemplateView):
     """Language rule create view."""
 
-    template_name = 'lang/rule/index/index.html'
+    template_name = 'lang/rule/index.html'
+    extra_context = CONTEXT['user_rule_index']
 
 
 class RuleCreateView(generic.TemplateView):
@@ -27,10 +34,18 @@ class RuleUpdateView(generic.TemplateView):
     template_name = 'lang/rule/update/index.html'
 
 
-class RuleListView(generic.TemplateView):
+class RuleListView(LoginRequiredMixin, generic.ListView):  # type: ignore[type-arg]
     """Language rule list view."""
 
     template_name = 'lang/rule/list/index.html'
+    extra_context = CONTEXT['rule_list']
+    context_object_name = 'rules'
+
+    def get_queryset(self) -> QuerySet[models.Rule]:
+        """Get rule queryset."""
+        return models.Rule.objects.filter(
+            user=self.request.user,
+        )
 
 
 class RuleDeleteView(generic.TemplateView):
