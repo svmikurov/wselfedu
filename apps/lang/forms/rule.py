9c3dbb33827b7f096e480edbd366ee63fd478f90
Sergei Mikurov <454004@mail.ru>
2026-01-10 16:03:45 +0500

    feat(lang/forms): Update rule forms

--- apps/lang/forms/rule.py
+++ apps/lang/forms/rule.py
@@ -14,12 +14,33 @@ from django.db import transaction
 from django.urls import reverse_lazy
 
 from apps.core import models as core_models
+from apps.users.models import Mentorship
 
 from .. import models
+from . import layouts
+
+
+class TranslationExampleFieldsMixin:
+    """Provides fields for translation."""
+
+    MAX_WORD_LENGTH = models.AbstractWordModel.WORD_LENGTH
+
+    question_foreign_word = forms.CharField(
+        max_length=MAX_WORD_LENGTH, label='Английское слово (вопрос)'
+    )
+    question_native_word = forms.CharField(
+        max_length=MAX_WORD_LENGTH, label='Родное слово (вопрос)'
+    )
+    answer_foreign_word = forms.CharField(
+        max_length=MAX_WORD_LENGTH, label='Английское слово (ответ)'
+    )
+    answer_native_word = forms.CharField(
+        max_length=MAX_WORD_LENGTH, label='Родное слово (ответ)'
+    )
 
 
 class RuleForm(forms.ModelForm):  # type: ignore[type-arg]
-    """English language rule forms."""
+    """Rule forms."""
 
     class Meta:
         """From configuration."""
@@ -31,11 +52,85 @@ class RuleForm(forms.ModelForm):  # type: ignore[type-arg]
             'source',
             'tag',
             'code',
-            'title',
         ]
+        widgets = {
+            'title': forms.Textarea(attrs={'rows': 3}),
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        """Construct the form."""
+        super().__init__(*args, **kwargs)  # type: ignore[arg-type]
+
+        self.helper = FormHelper()
+        self.helper.form_id = 'rule-form'
+        self.helper.layout = Layout(
+            'title',
+            'description',
+            'source',
+            Row(Column('tag'), Column('code')),
+            layouts.create_button_row(self.helper.form_id),
+        )
 
 
-class RuleExampleForm(forms.ModelForm):  # type: ignore[type-arg]
+class ClauseForm(forms.ModelForm):  # type: ignore[type-arg]
+    """Rule clause forms."""
+
+    class Meta:
+        """From configuration."""
+
+        model = models.RuleClause
+        fields = [
+            'rule',
+            'parent',
+            'ordinal',
+            'content',
+            'exception_content',
+        ]
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4}),
+            'exception_content': forms.Textarea(attrs={'rows': 4}),
+        }
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        """Construct the form."""
+        self.user = kwargs.pop('user', None)
+        rule = kwargs.pop('rule', None)
+        form_action = kwargs.pop('form_action', None)
+        super().__init__(*args, **kwargs)  # type: ignore
+
+        if rule:
+            self.initial['rule'] = rule
+
+        self.fields['parent'].queryset = models.RuleClause.objects.filter(  # type: ignore[attr-defined]
+            rule=rule,
+            parent=None,
+        )
+
+        self.helper = FormHelper()
+        self.helper.form_id = 'clause-form'
+        self.helper.form_action = form_action
+        self.helper.layout = Layout(
+            'rule',
+            'parent',
+            'ordinal',
+            'content',
+            'exception_content',
+            layouts.create_button_row(self.helper.form_id),
+        )
+
+    def save(self, commit: bool = True) -> models.RuleClause:
+        """Save rule clause."""
+        clause = super().save(commit=False)
+        clause.validate_depth()
+        clause.user = self.user
+        if commit:
+            clause.save()
+            self.save_m2m()
+        return clause  # type: ignore[no-any-return]
+
+
+class ClauseExampleForm(forms.ModelForm):  # type: ignore[type-arg]
     """Rule examples form."""
 
     MAX_WORD_LENGTH = models.AbstractWordModel.WORD_LENGTH
@@ -97,12 +192,8 @@ class RuleExampleForm(forms.ModelForm):  # type: ignore[type-arg]
             required=False,
         )
 
-        # Crispy form helper
         self.helper = FormHelper()
         self.helper.form_action = form_action
-        # self.helper.form_class = 'form-horizontal'
-        # self.helper.label_class = 'col-md-3'
-        # self.helper.field_class = 'col-md-9'
         self.helper.layout = Layout(
             'title',
             'clause',
@@ -258,6 +349,7 @@ class RuleExceptionForm(forms.ModelForm):  # type: ignore[type-arg]
         # Crispy form helper
         self.helper = FormHelper()
         self.helper.form_action = form_action
+        self.helper.form_id = 'exception-form'
         self.helper.layout = Layout(
             'title',
             'source',
@@ -275,10 +367,7 @@ class RuleExceptionForm(forms.ModelForm):  # type: ignore[type-arg]
                     'answer_native_word',
                 ),
             ),
-            Div(
-                Submit('submit', 'Сохранить', css_class='wse-btn'),
-                css_class='d-flex justify-content-end pt-3',
-            ),
+            layouts.create_button_row(self.helper.form_id),
         )
 
     # HACK: Fix user getting
@@ -350,3 +439,42 @@ class RuleExceptionForm(forms.ModelForm):  # type: ignore[type-arg]
             )
 
         return rule  # type: ignore[no-any-return]
+
+
+class RuleAssignmentForm(forms.ModelForm):  # type: ignore[type-arg]
+    """Rule assignation form for mentorship."""
+
+    class Meta:
+        """Form configuration."""
+
+        model = models.MentorshipEnglishRule
+        fields = ['mentorship', 'rule']
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        """Construct the form."""
+        user = kwargs.pop('user')
+        rule = kwargs.pop('rule')
+        super().__init__(*args, **kwargs)  # type: ignore
+
+        form_action = reverse_lazy(
+            'lang:english_rule_assignment_create',
+            kwargs={'pk': rule.pk},  # type: ignore[attr-defined]
+        )
+
+        mentorships = Mentorship.objects.select_related(  # type: ignore[misc]
+            'student', 'mentor'
+        ).filter(mentor=user)
+
+        self.fields['mentorship'].queryset = mentorships  # type: ignore[attr-defined]
+
+        if rule:
+            self.initial['rule'] = rule
+
+        self.helper = FormHelper()
+        self.helper.form_action = form_action
+        self.helper.form_id = 'exception-form'
+        self.helper.layout = Layout(
+            'mentorship',
+            'rule',
+            layouts.create_button_row(self.helper.form_id),
+        )
