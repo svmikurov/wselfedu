10bbe162dbb620054338bc3cd3bf61aba6ae12a6
Sergei Mikurov <454004@mail.ru>
2026-01-10 15:59:42 +0500

    feat(lang/adapters): Add schema fields
    - Added id into rula and rule clause fields
      to rule and rule clase models
    - Added children to lang clause schema

--- apps/lang/adapters/dto.py
+++ apps/lang/adapters/dto.py
@@ -1,20 +1,26 @@
 """Language application web adapter DTO."""
 
+from __future__ import annotations
+
 from pydantic import BaseModel
 
 
 class ClauseSchema(BaseModel):
     """Language rule clauses web schema."""
 
+    id: int
     content: str
     examples: str
     exception_content: str | None
     exceptions: str
 
+    children: list[ClauseSchema]
+
 
 class RuleSchema(BaseModel):
     """Language rule web schema."""
 
+    id: int
     title: str
     clauses: list[ClauseSchema]
     exceptions: str
