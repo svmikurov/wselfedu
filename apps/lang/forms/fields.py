70695c64cd646bb48c001955ffcefa20fc44e524
Sergei Mikurov <454004@mail.ru>
2026-01-12 03:49:01 +0500

    feat(lang/forms): Relocate field creation
    - Relocated general field creation to the separated module

---
+++ apps/lang/forms/fields.py
@@ -0,0 +1,48 @@
+"""English language form fields."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django import forms
+
+from .. import models
+from . import queries
+
+if TYPE_CHECKING:
+
+    from apps.users.models import Person
+
+
+def create_clause_field(user: Person, pk: int) -> forms.ModelChoiceField:
+    """Create clause field."""
+    return forms.ModelChoiceField(
+        queryset=queries.get_clauses_qs(user, pk),
+        label='Пункт правила',
+    )
+
+
+def create_example_type_field() -> forms.ChoiceField:
+    """Create example type field."""
+    return forms.ChoiceField(
+        choices=models.ExampleType,
+        label='Пример / Исключение',
+    )
+
+
+def create_source_field(user: Person) -> forms.ModelChoiceField:
+    """Create source field."""
+    return forms.ModelChoiceField(
+        queryset=queries.get_source_qs(user),
+        label='Источник',
+        required=False,
+    )
+
+
+def create_marks_field(user: Person) -> forms.ModelMultipleChoiceField:
+    """Create marks field."""
+    return forms.ModelMultipleChoiceField(
+        queryset=queries.get_marks_qs(user),
+        label='Маркер примера',
+        required=False,
+    )
