535e3b3f7ad1cdd25d0cf511b6ec1d8cb59c952d
Sergei Mikurov <454004@mail.ru>
2026-01-12 16:31:43 +0500

    feat(lang/views): Add views
    - Added list & delete views for
      rule clause task example edit

--- apps/lang/views/rule/example.py
+++ apps/lang/views/rule/example.py
@@ -52,6 +52,37 @@ class WordExampleDeleteView(HtmxOwnerDeleteView):
     model = models.RuleExample
 
 
+class TaskExampleListView(UserRequestMixin, generic.ListView):  # type: ignore[type-arg]
+    """Rule clause task example list view."""
+
+    template_name = 'lang/rule/detail/_task_examples_edit.html'
+    context_object_name = 'examples'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[models.RuleTaskExample]:
+        """Add filters and optimize query."""
+        return (
+            models.RuleTaskExample.objects.filter(
+                clause=self.kwargs['pk'],
+                user=self.user,
+            )
+            .select_related(
+                'question_translation__foreign',
+                'answer_translation__foreign',
+            )
+            .only(
+                'question_translation__foreign__word',
+                'answer_translation__foreign__word',
+            )
+        )
+
+
+class TaskExampleDeleteView(HtmxOwnerDeleteView):
+    """Rule clause task example delete view."""
+
+    model = models.RuleExample
+
+
 class ExceptionAddView(BaseAddView):
     """Add rule clause word example view."""
 
