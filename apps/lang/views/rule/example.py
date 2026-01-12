9760ce4a6640979955ecf2a58a127f44ecc3e4f9
Sergei Mikurov <454004@mail.ru>
2026-01-12 15:05:45 +0500

    feat(lang/views): Add views
    - Added views to render the rule clause word examples
      and delete them

--- apps/lang/views/rule/example.py
+++ apps/lang/views/rule/example.py
@@ -1,7 +1,12 @@
 """Rule clause edit example/exception views."""
 
+from django.db.models.query import QuerySet
+from django.views import generic
+
+from apps.core.views.auth import UserRequestMixin
 from apps.core.views.edit import BaseAddView
-from apps.lang import forms
+from apps.core.views.htmx import HtmxOwnerDeleteView
+from apps.lang import forms, models
 
 
 class TaskExampleAddView(BaseAddView):
@@ -14,6 +19,37 @@ class WordExampleAddView(BaseAddView):
     """Add rule clause word example view."""
 
     form_class = forms.WordExampleForm
+
+
+class WordExampleListView(UserRequestMixin, generic.ListView):  # type: ignore[type-arg]
+    """Rule clause word example list view."""
+
+    template_name = 'lang/rule/detail/_examples_edit.html'
+    context_object_name = 'examples'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[models.RuleExample]:
+        """Add filters and optimize query."""
+        return (
+            models.RuleExample.objects.filter(
+                clause=self.kwargs['pk'],
+                user=self.user,
+            )
+            .select_related(
+                'translation__foreign',
+                'translation__native',
+            )
+            .only(
+                'translation__foreign__word',
+                'translation__native__word',
+            )
+        )
+
+
+class WordExampleDeleteView(HtmxOwnerDeleteView):
+    """Rule clause word example delete view."""
+
+    model = models.RuleExample
 
 
 class ExceptionAddView(BaseAddView):
