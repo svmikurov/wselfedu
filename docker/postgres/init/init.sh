47dc82d0277878e80bdf8484db06a7a99be2efc9
Sergei Mikurov <454004@mail.ru>
2026-01-17 13:49:42 +0500

    fix(docker/postgres): Remove scripts

--- docker/postgres/init/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 \
-     -v db_name="$POSTGRES_DB" \
-     -v db_user="$POSTGRES_USER" \
-     -v db_password="'$POSTGRES_PASSWORD'" \
-     --username "postgres" \
-     -f /docker-entrypoint-initdb.d/init.sql
