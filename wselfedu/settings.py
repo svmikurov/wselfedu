df2f7659d91d2256d996cf1b11a51877d397ba82
Sergei Mikurov <454004@mail.ru>
2026-01-17 11:28:04 +0500

    refactor(settings): Update database settings

--- wselfedu/settings.py
+++ wselfedu/settings.py
@@ -107,16 +107,16 @@ WSGI_APPLICATION = 'wselfedu.wsgi.application'
 
 
 # Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv(
-            'DB_HOST', 'wse-db-postgres'
-        ),  # Default is the Docker Compose service name
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
         'CONN_MAX_AGE': 600,
         'CONN_HEALTH_CHECKS': True,
     }
