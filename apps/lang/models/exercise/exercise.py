1fcf9364dfebefb78ab09ab8395a8946f56de5e5
Sergei Mikurov <454004@mail.ru>
2026-01-21 15:33:08 +0500

    feat(lang/models): Add field
    - Added 'user' filed into Lang app exercise model

--- apps/lang/models/exercise/exercise.py
+++ apps/lang/models/exercise/exercise.py
@@ -1,14 +1,25 @@
 """Defines lang app exercise model."""
 
+from django.db import models
+
 from apps.core.models.base import BaseExercise
 
 
 class LangExercise(BaseExercise):
     """Lang app exercise model."""
 
+    user = models.ForeignKey(
+        'users.Person',
+        on_delete=models.CASCADE,
+        verbose_name='Создавший упражнение',
+    )
+
     class Meta:
         """Model configuration."""
 
         verbose_name = 'Упражнение'
         verbose_name_plural = 'Упражнения'
+
+        unique_together = ['name', 'user']
+
         db_table = 'lang_exercise'
