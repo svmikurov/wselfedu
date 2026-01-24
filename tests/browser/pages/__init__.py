68b7470ed4bbc100033ecaa2d4f52d8ff8d1419b
Sergei Mikurov <454004@mail.ru>
2026-01-24 14:49:35 +0500

    test(lang/browser): Add pages
    - Added pages for POM test:
      - Language exercise assignments from mentor fo student
      - Translation test exercise

--- tests/browser/pages/__init__.py
+++ tests/browser/pages/__init__.py
@@ -1,1 +1,11 @@
 """Contains pages for browser POM testing."""
+
+__all__ = [
+    # Student pages (mentorship)
+    'AssignmentsPage',
+    # Exercise pages
+    'TranslationTestPage',
+]
+
+from .lang.student.assignments import AssignmentsPage
+from .lang.student.translation_test import TranslationTestPage
