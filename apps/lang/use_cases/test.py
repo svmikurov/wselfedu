7085f5741fc47294efb5137a79e1488dcaa13265
Sergei Mikurov <454004@mail.ru>
2026-01-24 23:37:54 +0500

    fix(lang/use_cases): Fix assigned test exercise

--- apps/lang/use_cases/test.py
+++ apps/lang/use_cases/test.py
@@ -1,55 +1,29 @@
 """Language discipline test exercise UseCase."""
 
-from __future__ import annotations
+from typing import Any
 
-from typing import TYPE_CHECKING, Any
-
-from .. import schemas
-from . import UseCase
-
-if TYPE_CHECKING:
-    from apps.users.models import Person
-
-    from ..types.use_case import (
-        BusinessService,
-        DetailValidator,
-        ResponseAdapter,
-    )
+from ..schemas import (
+    Case,
+    DetailTestRequestDTO,
+    Explanation,
+    TestRequestDTO,
+    TestResponseData,
+)
+from . import DetailUseCase, UseCase
 
 type RequestData = dict[str, Any]
-type RequestDTO = schemas.DetailTestRequestDTO
-type DomainResult = schemas.Case | schemas.Explanation
-type ResponseData = schemas.TestResponseData
+type DomainResult = Case | Explanation
 
 
 class WebTestUseCase(
-    UseCase[RequestData, RequestDTO, DomainResult, ResponseData]
+    UseCase[RequestData, TestRequestDTO, DomainResult, TestResponseData]
 ):
     """Web translation study test exercise UseCase."""
 
 
-class AssignmentUseCase:
+class AssignmentUseCase(
+    DetailUseCase[
+        RequestData, DetailTestRequestDTO, DomainResult, TestResponseData
+    ]
+):
     """Web translation test exercise UseCase with assignment ID."""
-
-    def __init__(
-        self,
-        validator: DetailValidator[RequestData, RequestDTO],
-        service: BusinessService[RequestDTO, DomainResult],
-        response_adapter: ResponseAdapter[DomainResult, ResponseData],
-    ) -> None:
-        """Construct the UseCase."""
-        self._validator = validator
-        self._service = service
-        self._response_adapter = response_adapter
-
-    def execute(
-        self,
-        user: Person,
-        request_data: RequestData,
-        assignment_id: int,
-    ) -> ResponseData:
-        """Execute the UseCase."""
-        validated = self._validator.validate(request_data, assignment_id)
-        domain_result = self._service.execute(user, validated)
-        result = self._response_adapter.to_response(domain_result)
-        return result
