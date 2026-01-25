a5562d0443a92329c68f98c3aa982b8edd7ab750
Sergei Mikurov <454004@mail.ru>
2026-01-25 19:54:09 +0500

    refactor(lang/di): Test exercise DI container
    - Added type hint for container dependencies
    
    HACK:
      - Replace Any type to hint container dependency types
      - Replace the workaround of the exercise configuration
    
    REVIEW:
      - Relocate the type hint definition to a separate module?

--- apps/lang/di/translation/test.py
+++ apps/lang/di/translation/test.py
@@ -1,38 +1,83 @@
-"""Translation study test exercise DI container."""
+"""DI container for translation study via Test exercises.
 
-from dependency_injector import containers, providers
+Provides dependencies for handling WEB and API requests for exercises,
+including:
+    - Input validation for WEB and API requests
+    - Exercise creation and progress tracking
+    - Domain result adaptation for WEB and API responses
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from dependency_injector.containers import DeclarativeContainer
+from dependency_injector.providers import Configuration, Factory
 
 from apps.core.storage import services as storage
 from apps.core.storage.clients import DjangoCache
+from apps.lang import adapters, repositories, services, use_cases, validators
+from apps.lang.schemas.test import StoryDomainResult
 
-from ... import adapters, repositories, services, use_cases, validators
-from ...schemas.test import StoryDomainResult
+# HACK: Replace Any type to hint container dependency types
+# REVIEW: Relocate the type hint definition to a separate module?
+if TYPE_CHECKING:
+    from apps.lang.use_cases import types
 
-# TODO: Move case storage ttl to test settings.
-CASE_STORAGE_TTL = 600
+    # --------------------------------
+    # Container dependencies type hint
+    # --------------------------------
+
+    # Validators
+    type RegularValidatorFactory = Factory[Any]
+    type AssignedValidatorFactory = Factory[Any]
+
+    # Service dependencies
+    type TranslationRepositoryFactory = Factory[Any]
+    type ProgressRepositoryFactory = Factory[Any]
+    type StorageFactory = Factory[Any]
+
+    # Services
+    type RegularServiceFactory = Factory[Any]
+    type AssignedServiceFactory = Factory[Any]
+    type ProgressServiceFactory = Factory[Any]
+
+    # Adapters
+    type WebAdapterFactory = Factory[Any]
+
+    # UseCases
+    type WebRegularFactory = Factory[types.WebTest]
+    type WebAssignedFactory = Factory[types.WebAssignedTest]
+    type WebProgressFactory = Factory[Any]
 
 
-class TranslationTestContainer(containers.DeclarativeContainer):
-    """Translation study test exercise DI container."""
+# HACK: Split a container by a group of functional dependencies
+class TranslationTestContainer(DeclarativeContainer):
+    """Dependency injection container for translation test exercises."""
+
+    # ---------------------------
+    # Test exercise configuration
+    # ---------------------------
+
+    config = Configuration()
+
+    # HACK: Replace the workaround of the exercise configuration
+    config.from_dict(
+        {
+            'option_count': 5,
+            'limit': 100,
+            'case_storage_ttl': 600,
+        }
+    )
 
     # ------------------
-    # Test configuration
+    # Request Validators
     # ------------------
 
-    # TODO: Replace temporary workaround - Apply pydantic model
-    config = {
-        'option_count': 7,
-        'limit': 100,
-    }
-
-    # ------------------
-    # Request validators
-    # ------------------
-
-    web_validator = providers.Factory(
+    web_regular_validator: RegularValidatorFactory = Factory(
         validators.WebTestValidator,
     )
-    web_validator_assigned = providers.Factory(
+    web_assigned_validator: AssignedValidatorFactory = Factory(
         validators.WebAssignedExerciseValidator,
     )
 
@@ -40,76 +85,75 @@ class TranslationTestContainer(containers.DeclarativeContainer):
     # Service dependencies
     # --------------------
 
-    repository = providers.Factory(
+    translation_repository: TranslationRepositoryFactory = Factory(
         repositories.TranslationRepository,
     )
-    progress_repository = providers.Factory(
+    progress_repository: ProgressRepositoryFactory = Factory(
         repositories.Progress,
     )
-
-    cache_client = providers.Factory(DjangoCache[StoryDomainResult])
-    cache_storage = providers.Factory(
+    cache_client = Factory(
+        DjangoCache[StoryDomainResult],
+    )
+    cache_storage: StorageFactory = Factory(
         storage.TaskStorage[StoryDomainResult],
         storage=cache_client,
-        ttl=CASE_STORAGE_TTL,
+        ttl=config.case_storage_ttl,
     )
 
     # --------
     # Services
     # --------
 
-    eng_service = providers.Factory(
+    regular_exercise_service: RegularServiceFactory = Factory(
         services.TestService,
-        repository=repository,
+        repository=translation_repository,
         storage=cache_storage,
         config=config,
     )
-    eng_service_progress = providers.Factory(
+    assigned_exercise_service: AssignedServiceFactory = Factory(
+        services.AssignedTestService,
+        repository=translation_repository,
+        storage=cache_storage,
+        config=config,
+    )
+
+    progress_service: ProgressServiceFactory = Factory(
         services.TestProgressService,
-        repository=repository,
+        repository=translation_repository,
         progress_repository=progress_repository,
         storage=cache_storage,
         config=config,
     )
-    # Assigned test exercise service
-    eng_service_assigned = providers.Factory(
-        services.AssignedTestService,
-        repository=repository,
-        storage=cache_storage,
-        config=config,
-    )
 
     # -----------------
-    # Response adapters
+    # Response Adapters
     # -----------------
 
-    web_adapter = providers.Factory(
+    web_adapter: WebAdapterFactory = Factory(
         adapters.WebTestAdapter,
     )
 
-    # ----------------------------------------
-    # Translation study test exercise UseCases
-    # ----------------------------------------
+    # --------
+    # UseCases
+    # --------
 
-    web_use_case = providers.Factory(
+    web_regular_use_case: WebRegularFactory = Factory(
         use_cases.UseCase,
-        validator=web_validator,
-        service=eng_service,
+        validator=web_regular_validator,
+        service=regular_exercise_service,
         response_adapter=web_adapter,
     )
-
-    # Adds tracking of the translation study progress.
-    web_use_case_progress = providers.Factory(
-        use_cases.UseCase,
-        validator=web_validator,
-        service=eng_service_progress,
-        response_adapter=web_adapter,
-    )
-
-    # Test assigned by the mentor.
-    web_use_case_assigned = providers.Factory(
+    web_assigned_use_case: WebAssignedFactory = Factory(
         use_cases.DetailUseCase,
-        validator=web_validator_assigned,
-        service=eng_service_assigned,
+        validator=web_assigned_validator,
+        service=assigned_exercise_service,
+        response_adapter=web_adapter,
+    )
+
+    # Translation study have study progress tracking.
+    web_progress_use_case: WebProgressFactory = Factory(
+        use_cases.UseCase,
+        validator=web_regular_validator,
+        service=progress_service,
         response_adapter=web_adapter,
     )
