f0ed16f3296a3287c2de8b173dc51c2ff4494669
Sergei Mikurov <454004@mail.ru>
2026-01-27 21:56:27 +0500

    chore(apps/core): Add package export

--- apps/core/storage/__init__.py
+++ apps/core/storage/__init__.py
@@ -1,1 +1,15 @@
 """Contains storage services."""
+
+__all__ = [
+    # Abstract / Protocol
+    'StorageClient',
+    'TaskStorageABC',
+    # Real
+    'DjangoCache',
+    'TaskStorage',
+]
+
+from .clients import DjangoCache
+from .clients.iabc import StorageClient
+from .services.iabc import TaskStorageABC
+from .services.task import TaskStorage
