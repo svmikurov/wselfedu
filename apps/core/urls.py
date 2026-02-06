cfdbf83e5d58ed4d1ba5b8056d96d5e201003889
Sergei Mikurov <454004@mail.ru>
2026-02-06 12:10:02 +0500

    feat(core/urls): Add source list path

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,6 +3,33 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views import source
+
+app_name = 'core'
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    # -------------------------------------------
+    # Source
+    # -------------------------------------------
+    path(
+        'source/',
+        source.SourceListView.as_view(),
+        name='source_list',
+    ),
+    path(
+        'source/create/',
+        source.SourceCreateView.as_view(),
+        name='source_create',
+    ),
+    path(
+        'source/<int:pk>/update/',
+        source.SourceUpdateView.as_view(),
+        name='source_update',
+    ),
+    path(
+        'source/<int:pk>/delete/',
+        source.SourceDeleteView.as_view(),
+        name='source_delete',
+    ),
 ]
