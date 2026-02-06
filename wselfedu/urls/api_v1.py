7148f9a3adf3023b947affdbcc57110baaf6849a
Sergei Mikurov <454004@mail.ru>
2026-02-06 12:11:44 +0500

    fix(core/api/urls): Update space names

--- wselfedu/urls/api_v1.py
+++ wselfedu/urls/api_v1.py
@@ -11,11 +11,11 @@ from apps.users.api.v1.urls import router as users_router
 
 # fmt: off
 urlpatterns = [
-    path('core/', include((core_router.urls, 'core'), namespace='core')),
-    path('math/', include((math_router.urls, 'math'), namespace='math')),
-    path('study/', include((study_router.urls, 'study'), namespace='study')),
+    path('core/', include((core_router.urls, 'core'), namespace='core_api')),
+    path('math/', include((math_router.urls, 'math'), namespace='math_api')),
+    path('study/', include((study_router.urls, 'study'), namespace='study_api')),  # noqa: E501
     path('users/', include(users_router.urls)),
-    path('glossary/', include((terms_router.urls, 'glossary_api'), namespace='glossary_api')),  # noqa: E501
-    path('lang/', include((lang_router.urls, 'lang_api'), namespace='lang_api')),  # noqa: E501
+    path('glossary/', include((terms_router.urls, 'glossary'), namespace='glossary_api')),  # noqa: E501
+    path('lang/', include((lang_router.urls, 'lang'), namespace='lang_api')),  # noqa: E501
 ]
 # fmt: on
