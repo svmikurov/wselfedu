87e96fd797642618edd7f54d406a40ee279316f5
Sergei Mikurov <454004@mail.ru>
2026-02-06 16:15:24 +0500

    feat(lang/filters): Add filter by past n days

--- apps/lang/filters/translation.py
+++ apps/lang/filters/translation.py
@@ -1,6 +1,12 @@
 """Translation filters."""
 
+from datetime import timedelta
+from decimal import Decimal
+
 import django_filters
+from django.db.models import QuerySet
+from django.utils import timezone
+from django.utils.translation import gettext as _
 
 from apps.core import models as core_models
 
@@ -12,20 +18,26 @@ class TranslationFilter(django_filters.FilterSet):
 
     category = django_filters.ModelChoiceFilter(
         queryset=models.Category.objects.all(),
-        empty_label='Все категории',
-        label='Категория',
+        empty_label=_('All categories'),
+        label=_('Category'),
     )
     source = django_filters.ModelChoiceFilter(
         queryset=core_models.Source.objects.all(),
-        empty_label='Все источники',
-        label='Источник',
+        empty_label=_('All sources'),
+        label=_('Source'),
+    )
+
+    days = django_filters.NumberFilter(
+        field_name=_('Date'),
+        method='get_past_n_days',
+        label=_('Past n days'),
     )
 
     class Meta:
         """Filter configuration."""
 
         model = models.EnglishTranslation
-        fields = ['category', 'source']
+        fields = ['category', 'source', 'days']
 
     def __init__(self, *args: object, **kwargs: object) -> None:
         """Configure the filter."""
@@ -39,3 +51,13 @@ class TranslationFilter(django_filters.FilterSet):
             self.filters[  # type: ignore[attr-defined]
                 'source'
             ].queryset = core_models.Source.objects.filter(user=user)  # type: ignore[misc]
+
+    def get_past_n_days(
+        self,
+        queryset: QuerySet[models.EnglishTranslation],
+        field_name: str,
+        value: Decimal,
+    ) -> QuerySet[models.EnglishTranslation]:
+        """Filter by past n days."""
+        time_threshold = timezone.now() - timedelta(days=int(value))
+        return queryset.filter(created_at__gte=time_threshold)
