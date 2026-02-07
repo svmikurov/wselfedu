93aa7da328ec9988e2ddcf2812006e2ba29beacf
Sergei Mikurov <454004@mail.ru>
2026-02-07 13:32:49 +0500

    feat(math/url): Add calculation path

--- apps/math/urls.py
+++ apps/math/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from .views.exercise import calculation
+
 app_name = 'math'
 
 urlpatterns = [
@@ -10,5 +12,10 @@ urlpatterns = [
         '',
         TemplateView.as_view(template_name='math/index.html'),
         name='index',
+    ),
+    path(
+        '',
+        calculation.CalculationView.as_view(),
+        name='exercise_calculation',
     ),
 ]
