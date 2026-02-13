3a0cdd4e0d07ffa557b30ca138e649cda4c59998
Sergei Mikurov <454004@mail.ru>
2026-02-13 22:53:27 +0500

    feat(core/use_case): Add abstract use case

---
+++ apps/core/use_cases/abstract.py
@@ -0,0 +1,20 @@
+"""Abstract base classes for use cases."""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Generic, TypeVar
+
+if TYPE_CHECKING:
+    from apps.users.models import Person
+
+ResponseData = TypeVar('ResponseData')
+RequestData = TypeVar('RequestData')
+
+
+class AbstractUseCase(ABC, Generic[RequestData, ResponseData]):
+    """ABC for use case."""
+
+    @abstractmethod
+    def execute(self, user: Person, request_data: RequestData) -> ResponseData:
+        """Execute use case."""
