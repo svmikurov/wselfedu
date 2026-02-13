99a12e2910b403301bf1c02cf6bb7e99462cc7a1
Sergei Mikurov <454004@mail.ru>
2026-02-13 14:53:19 +0500

    feat(core/view): Add mixin with exercise hendlers

--- apps/core/views/mixins.py
+++ apps/core/views/mixins.py
@@ -3,6 +3,8 @@
 from typing import Generic, TypeVar
 
 T = TypeVar('T')
+StartHandler = TypeVar('StartHandler')
+LoopHandler = TypeVar('LoopHandler')
 
 
 class GetUseCaseMixin(Generic[T]):
@@ -55,3 +57,28 @@ class GetHandlerMixin(Generic[T]):
         if self._handler is None:
             raise AttributeError('Request handler not initialized')
         return self._handler
+
+
+class GetExerciseHandlersMixin(Generic[StartHandler, LoopHandler]):
+    """Mixin provides exercise start and loop handlers."""
+
+    _start_handler: StartHandler | None
+    _loop_handler: LoopHandler | None
+
+    @property
+    def start_handler(self) -> StartHandler:
+        """Get start exercise request handler."""
+        if self._start_handler is None:
+            raise AttributeError(
+                'Start exercise request handler not initialized'
+            )
+        return self._start_handler
+
+    @property
+    def loop_handler(self) -> LoopHandler:
+        """Get exercise loop request handler."""
+        if self._loop_handler is None:
+            raise AttributeError(
+                'Exercise loop request handler not initialized'
+            )
+        return self._loop_handler
