fa6b6dba49c45a63255be8fee790cd70daa5711e
Sergei Mikurov <454004@mail.ru>
2026-02-16 11:05:35 +0500

    feat(core/handlers): Add request DTOs

---
+++ apps/core/handlers/dto.py
@@ -0,0 +1,48 @@
+"""Request handler DTOs."""
+
+from typing import Any
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from apps.users.models import Person
+
+
+class RequestContext(BaseModel):
+    """Request context."""
+
+    user: Person
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        extra='forbid',
+        frozen=True,
+    )
+
+
+class DetailParams(BaseModel):
+    """Detail request parameters."""
+
+    pk: int = Field(
+        description='Resource pk',
+    )
+
+    model_config = ConfigDict(
+        extra='forbid',
+        frozen=True,
+    )
+
+
+class RequestData(BaseModel):
+    """Request data."""
+
+    query: dict[str, Any] = Field(
+        description=Field('Request data'),
+    )
+
+
+class RequestResult(BaseModel):
+    """Result of request handling."""
+
+    context: dict[str, Any] = Field(
+        description=Field('Response context'),
+    )
