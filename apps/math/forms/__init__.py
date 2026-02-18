a0f1727c3fbb26eb7a7c6babff44e5f82372f09a
Sergei Mikurov <454004@mail.ru>
2026-02-18 14:40:09 +0500

    feat(math/form): Add assign calculation to student

--- apps/math/forms/__init__.py
+++ apps/math/forms/__init__.py
@@ -1,12 +1,19 @@
 """Mathematical application forms."""
 
 __all__ = [
+    # Calculation CRUD
     'CreateCalculationConditionsForm',
     'UpdateCalculationConditionsForm',
     'RegularCalculationConditionsForm',
+    # Calculation assignation
+    'AssignCalculationForm',
+    # Calculation exercise
     'NumberInputForm',
 ]
 
+from .assignation import (
+    AssignCalculationForm,
+)
 from .calculation import (
     CreateCalculationConditionsForm,
     NumberInputForm,
