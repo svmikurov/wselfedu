4685642753eac92eab81ee255533eacbacca6a51
Sergei Mikurov <454004@mail.ru>
2026-02-20 01:24:01 +0500

    refactor(users/view): Update profile page

--- apps/users/views/profile.py
+++ apps/users/views/profile.py
@@ -1,16 +1,11 @@
-"""Defines user profile view."""
+"""User's profile view."""
 
-from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import DetailView
+from django.views.generic import TemplateView
+
+from apps.core.views import ProfileMixin
 
 
-class ProfileView(UserPassesTestMixin, DetailView):  # type: ignore[type-arg]
-    """User profile view."""
+class ProfileView(ProfileMixin, TemplateView):
+    """User's profile view."""
 
-    model = get_user_model()
     template_name = 'users/profile.html'
-
-    def test_func(self) -> bool:
-        """Check if the user is the owner of the object."""
-        return bool(self.request.user == self.get_object())
