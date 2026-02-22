0f29d2522b7951be0ebe3c0b84d8d74e71741d45
Sergei Mikurov <454004@mail.ru>
2026-02-23 02:41:35 +0500

    feat(math/view): Update queryset

--- apps/math/views/exercise/calculation/student.py
+++ apps/math/views/exercise/calculation/student.py
@@ -1,10 +1,18 @@
 """Student's calculation exercises."""
 
-from django.db.models import QuerySet
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import (
+    CharField,
+    IntegerField,
+    OuterRef,
+    QuerySet,
+    Subquery,
+)
 from django.views.generic import ListView
 
 from apps.core.views import UserLoginRequiredMixin
 from apps.math.models import StudentCalculationCondition
+from apps.study.models import ExerciseAvailability, ExerciseReward
 
 
 class AssignedCalculationExerciseStudentListVew(
@@ -18,15 +26,41 @@ class AssignedCalculationExerciseStudentListVew(
     context_object_name = 'exercises'
 
     def get_queryset(self) -> QuerySet[StudentCalculationCondition]:
-        """Get student's assigned calculations."""
-        return (
-            super()
-            .get_queryset()
-            .filter(
-                mentorship__student=self.user,
+        """Return mentor's assignations for students."""
+        content_type = ContentType.objects.get_for_model(
+            StudentCalculationCondition
+        )
+
+        reward_subquery = ExerciseReward.objects.filter(
+            exercise_content_type=content_type,
+            exercise_object_id=OuterRef('pk'),
+        ).values('amount')[:1]
+        availability_subquery = ExerciseAvailability.objects.filter(
+            exercise_content_type=content_type,
+            exercise_object_id=OuterRef('pk'),
+        ).values('count', 'period')[:1]
+
+        exercises = (
+            StudentCalculationCondition.objects.filter(
+                mentorship__student=self.user
             )
             .select_related(
-                'mentorship__mentor',
+                'mentorship__student',
                 'calculation_condition',
             )
-        )
+            .annotate(
+                reward_amount=Subquery(
+                    reward_subquery, output_field=IntegerField()
+                ),
+                availability_count=Subquery(
+                    availability_subquery.values('count')[:1],
+                    output_field=IntegerField(),
+                ),
+                availability_period=Subquery(
+                    availability_subquery.values('period')[:1],
+                    output_field=CharField(),
+                ),
+            )
+        ).order_by('created_at')
+
+        return exercises
