6f71836a75ce0dc5c9ef83be41f0fa2bd78c0506
Sergei Mikurov <454004@mail.ru>
2026-02-23 04:38:59 +0500

    feat(core/model): Extend abstract package export

--- apps/core/models/abstract/__init__.py
+++ apps/core/models/abstract/__init__.py
@@ -1,6 +1,13 @@
-"""Contains base models."""
+"""Abstract base models."""
 
-__all__ = [
+__all__ = (
+    'AbstractBaseModel',
+    'AbstractCategory',
+    'AbstractMark',
     'BaseExercise',
-]
+)
+
+from .base import AbstractBaseModel
+from .category import AbstractCategory
 from .exercise import BaseExercise
+from .mark import AbstractMark
