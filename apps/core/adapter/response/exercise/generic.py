ce061812d67ab16f6fff73c8cdd0393ee98ac620
Sergei Mikurov <454004@mail.ru>
2026-02-26 23:38:09 +0500

    refactor(core/adapter): Update typing and error

--- apps/core/adapter/response/exercise/generic.py
+++ apps/core/adapter/response/exercise/generic.py
@@ -4,37 +4,35 @@ from typing import Generic, TypeVar
 
 from apps.core.domain.exercise.enums import ExerciseStatusEnum
 from apps.core.domain.exercise.types import ExerciseStatus
+from apps.core.handlers.protocol import (
+    RequestResultProtocol,
+    SimpleResponseAdapter,
+)
 
-from ..abc import AbstractResponseAdapter
-
-DomainType = TypeVar('DomainType', bound=ExerciseStatus)
-NewCaseResponse = TypeVar('NewCaseResponse')
-ExplainResponse = TypeVar('ExplainResponse')
-ResponseType = TypeVar('ResponseType')
+DomainResultDTO = TypeVar('DomainResultDTO', bound=ExerciseStatus)
 
 
-# HACK: Fix type ignore
 class ResultStrategyAdapter(
-    AbstractResponseAdapter[DomainType, ResponseType],
-    Generic[DomainType, ResponseType, NewCaseResponse, ExplainResponse],
+    SimpleResponseAdapter[DomainResultDTO],
+    Generic[DomainResultDTO],
 ):
     """Router for new case or explain current case adapter select."""
 
     def __init__(
         self,
-        new_case_adapter: AbstractResponseAdapter[DomainType, NewCaseResponse],
-        explain_adapter: AbstractResponseAdapter[DomainType, ExplainResponse],
+        new_case_adapter: SimpleResponseAdapter[DomainResultDTO],
+        explain_adapter: SimpleResponseAdapter[DomainResultDTO],
     ) -> None:
         """Construct the adapter."""
         self._new_case_adapter = new_case_adapter
         self._explain_adapter = explain_adapter
 
-    def to_response(self, schema: DomainType) -> ResponseType:
+    def to_response(self, schema: DomainResultDTO) -> RequestResultProtocol:
         """Convert domain data to response representation."""
         match schema.exercise_status:
             case ExerciseStatusEnum.NEW_CASE:
-                return self._new_case_adapter.to_response(schema)  # type: ignore[return-value]
+                return self._new_case_adapter.to_response(schema)
             case ExerciseStatusEnum.EXPLAIN:
-                return self._explain_adapter.to_response(schema)  # type: ignore[return-value]
-            case _:
-                raise RuntimeError('Unexpected case status')
+                return self._explain_adapter.to_response(schema)
+            case _ as unexpected:
+                raise ValueError(f'Unexpected exercise status: {unexpected}')
