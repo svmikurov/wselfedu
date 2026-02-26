51e5f8ca242a203d052c15a37c3386b711f7aa36
Sergei Mikurov <454004@mail.ru>
2026-02-27 00:20:59 +0500

    feat(users/service): Implement reward

--- apps/users/services/reward.py
+++ apps/users/services/reward.py
@@ -2,23 +2,36 @@
 
 from typing import override
 
+from django.db import transaction
+from django.db.models import F
+
+from apps.users.domains.dto import RewardDTO
+from apps.users.models import Balance, Transaction
 from utils import decorators
 
 from .abstract import AbstractRewardService
 
-__all__ = ('CalculationRewardService',)
+__all__ = ('RewardService',)
 
 
-# NOTE: It's experimental reward service definition
-class CalculationRewardService(AbstractRewardService):
-    """Calculation exercise reward service."""
+class RewardService(AbstractRewardService):
+    """Exercise reward service."""
 
     @override
     @decorators.log_unimplemented_call
-    def increment(self, resource_pk: int, mentorship_pk: int) -> None:
+    def increment(self, reward: RewardDTO) -> None:
         """Add reward."""
-
-    @override
-    @decorators.log_unimplemented_call
-    def decrement(self, resource_pk: int, mentorship_pk: int) -> None:
-        """Remove reward."""
+        with transaction.atomic():
+            Balance.objects.update_or_create(
+                pk=reward.student_pk,
+                defaults={'total': F('total') + reward.amount},
+                create_defaults={
+                    'total': reward.amount,
+                    'user_id': reward.student_pk,
+                },
+            )
+            Transaction.objects.create(
+                user_id=reward.student_pk,
+                amount=reward.amount,
+                type=Transaction.Operation.REWARD,
+            )
