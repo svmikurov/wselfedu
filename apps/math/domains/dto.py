30f7acc78fb7da831cb1a274cd8919c449781581
Sergei Mikurov <454004@mail.ru>
2026-03-01 09:29:49 +0500

    feat(math/domain): Add exercise DTOs

--- apps/math/domains/dto.py
+++ apps/math/domains/dto.py
@@ -2,10 +2,11 @@
 
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from apps.core.domains.base_dto import BaseDTO
 from apps.core.domains.exercise.enums import ExerciseStatusEnum
+from apps.study.models.exercise.reward import RewardType
 
 Operation = Literal['add', 'sub', 'mul', 'div']
 
@@ -34,17 +35,50 @@ class CalculationCaseDTO(BaseModel):
 
 
 class CalculationSolutionDTO(BaseModel):
-    """Calculation exercise case solution."""
+    """Calculation exercise case correct solution."""
 
     solution_text: str
 
 
-class CalculationMetaDTO(BaseModel):
+class CalculationMetaDTO(CalculationCaseDTO):
     """Calculation exercise metadata to story."""
 
     question_text: str
     correct_answer: int
-    conditions: CalculationConditionDTO
+
+
+# NOTE: It's experimental implementation
+class ExerciseAvailabilityDTO(BaseModel):
+    """Exercise availability DTO."""
+
+
+# NOTE: It's experimental implementation
+class ExerciseMilestoneDTO(BaseModel):
+    """Exercise milestone DTO."""
+
+    reward_amount: int = Field(
+        description='Reward amount for success exercise completion',
+    )
+    reward_type: RewardType = Field(
+        description='Reward type for success exercise completion',
+    )
+
+
+# NOTE: It's experimental implementation
+class ExerciseParametersDTO(BaseModel):
+    """Exercise parameters DTO."""
+
+    conditions: CalculationConditionDTO = Field(
+        description='Create exercise conditions',
+    )
+    availability: ExerciseAvailabilityDTO | None = Field(
+        description='Exercise availability data',
+        default=None,
+    )
+    milestone: ExerciseMilestoneDTO | None = Field(
+        description='Current milestone data',
+        default=None,
+    )
 
 
 class CalculationDataDTO(BaseDTO):
@@ -63,6 +97,15 @@ class CalculationAnswerDTO(BaseDTO):
     """Calculation exercise user's answer."""
 
     user_answer: str
+
+
+# ===============================================
+# Regular exercise loop
+# ===============================================
+
+
+class CalculationLoopDTO(CalculationConditionDTO, CalculationAnswerDTO):
+    """Regular calculation exercise loop DTO."""
 
 
 # ===============================================
