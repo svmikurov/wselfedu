63c8bc77af6003321bad616fcae2a6893c493301
Sergei Mikurov <454004@mail.ru>
2026-03-04 06:20:09 +0500

    test(fixture): Add mentorship with student, mentor

--- tests/fixtures/user.py
+++ tests/fixtures/user.py
@@ -5,41 +5,64 @@ from unittest.mock import Mock
 import pytest
 from django.contrib.auth.models import AnonymousUser
 
-from apps.users.models import Person
+from apps.users.models import Mentorship, Person
 
+# ===============================================
 # Database fixtures
-# -----------------
+# ===============================================
 
 
 @pytest.fixture
 def user() -> Person:
     """Provide user."""
-    return Person.objects.create_user(
-        username='test_user',
-        password='test_pass',
-    )
+    return Person.objects.create_user(username='user', password='pass')
 
 
-@pytest.fixture
-def user_not_owner() -> Person:
-    """Provide user that is not the owner."""
-    return Person.objects.create_user(
-        username='other_test_user',
-        password='other_test_pass',
-    )
+# -----------------------------------------------
+# Ownership
+# -----------------------------------------------
 
 
 @pytest.fixture
 def owner() -> Person:
-    """Provide owner of object."""
-    return Person.objects.create_user(
-        username='owner_user',
-        password='owner_pass',
-    )
+    """Provide owner of DB object."""
+    return Person.objects.create_user(username='owner', password='pass')
 
 
+@pytest.fixture
+def not_owner() -> Person:
+    """Provide user that is not the owner of DB object."""
+    return Person.objects.create_user(username='not_owner', password='pass')
+
+
+# -----------------------------------------------
+# Mentorship
+# -----------------------------------------------
+
+
+@pytest.fixture
+def mentorship() -> Mentorship:
+    """Provide mentorship."""
+    student = Person.objects.create_user(username='student', password='pass')
+    mentor = Person.objects.create_user(username='mentor', password='pass')
+    return Mentorship.objects.create(mentor=mentor, student=student)
+
+
+@pytest.fixture
+def student(mentorship: Mentorship) -> Person:
+    """Provide student."""
+    return mentorship.student
+
+
+@pytest.fixture
+def mentor(mentorship: Mentorship) -> Person:
+    """Provide mentor."""
+    return mentorship.mentor
+
+
+# ===============================================
 # Mock user fixtures
-# ------------------
+# ===============================================
 
 
 @pytest.fixture
@@ -55,9 +78,7 @@ def mock_user() -> Mock:
 
 
 @pytest.fixture
-def mock_auth_user(
-    mock_user: Mock,
-) -> Mock:
+def mock_auth_user(mock_user: Mock) -> Mock:
     """Provide authenticated user mock."""
     mock_user.is_authenticated = True
     return mock_user
