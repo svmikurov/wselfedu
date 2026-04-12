860f7758020586ac7c9b73338754ab66e1fb5736
Sergei Mikurov <454004@mail.ru>
2026-04-12 23:19:42 +0500

    fix(core/domain): Rename exercise parameters protocol

--- apps/core/domains/exercise/protocol.py
+++ apps/core/domains/exercise/protocol.py
@@ -66,7 +66,7 @@ class GenericExerciseParameters(
     """
 
 
-class ExerciseParameters(
+class ExerciseParametersProtocol(
     GenericExerciseParameters[
         ExerciseConditions,
         ExerciseConfig,
