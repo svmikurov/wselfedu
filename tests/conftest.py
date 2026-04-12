13e8ca27eacc7454792f633c003fc8f155b7322f
Sergei Mikurov <454004@mail.ru>
2026-04-12 20:16:33 +0500

    test(fixture): Add exercise DTO fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,6 @@
 pytest_plugins = [
     'tests.fixtures.di.container',
     'tests.fixtures.db_user',
+    'tests.fixtures.lang.db.exercise',
     'tests.fixtures.lang.db.translations',
 ]
---
+++ tests/fixtures/lang/db/exercise.py
@@ -0,0 +1,32 @@
+"""Exercise fixtures."""
+
+import pytest
+
+from apps.core.assemblers.command import UserDataCommand
+from apps.core.assemblers.protocol import UserDataCommandProtocol
+from apps.core.domains.exercise.enums import ExerciseProcessEnum
+from apps.core.validators.request.dto import ExerciseActionWebDTO
+from apps.core.validators.request.protocol import HasExerciseProcessAction
+from apps.users.models import Person
+
+CommandT = UserDataCommandProtocol[HasExerciseProcessAction]
+
+
+@pytest.fixture
+def validated() -> HasExerciseProcessAction:
+    """Provide new case validated dto."""
+    return ExerciseActionWebDTO(
+        action=ExerciseProcessEnum.CREATE_CASE,
+    )
+
+
+@pytest.fixture
+def new_case_command(
+    user: Person,
+    validated: HasExerciseProcessAction,
+) -> CommandT:
+    """Provide new case request fixture."""
+    return UserDataCommand(
+        user=user,
+        data=validated,
+    )
