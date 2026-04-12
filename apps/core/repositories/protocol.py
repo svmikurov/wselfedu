bc337d1566f4121767ee34e66a3afcada06ab163
Sergei Mikurov <454004@mail.ru>
2026-04-12 22:20:32 +0500

    style(core/repository): Rename type vars

--- apps/core/repositories/protocol.py
+++ apps/core/repositories/protocol.py
@@ -6,12 +6,12 @@ from apps.users.models import Person
 
 FilterData_contra = TypeVar('FilterData_contra', contravariant=True)
 UpdateData_contra = TypeVar('UpdateData_contra', contravariant=True)
-Query_cov = TypeVar('Query_cov', covariant=True)
-Result_cov = TypeVar('Result_cov', covariant=True)
+Query_co = TypeVar('Query_co', covariant=True)
+Result_co = TypeVar('Result_co', covariant=True)
 
 
 class UpdateRepositoryProtocol(
-    Protocol[FilterData_contra, UpdateData_contra, Result_cov],
+    Protocol[FilterData_contra, UpdateData_contra, Result_co],
 ):
     """Protocol for repository interface."""
 
@@ -20,23 +20,23 @@ class UpdateRepositoryProtocol(
         user: Person,
         filter: FilterData_contra,
         updates: UpdateData_contra,
-    ) -> Result_cov:
+    ) -> Result_co:
         """Update."""
 
 
-class UserRepositoryProtocol(Protocol[FilterData_contra, Result_cov]):
+class UserRepositoryProtocol(Protocol[FilterData_contra, Result_co]):
     """Protocol for user's resource repository provides DTO."""
 
     def fetch(
         self,
         user: Person,
         filter: FilterData_contra,
-    ) -> Result_cov:
+    ) -> Result_co:
         """Fetch data."""
 
 
-class ModelRepositoryProtocol(Protocol[FilterData_contra, Query_cov]):
+class ModelRepositoryProtocol(Protocol[FilterData_contra, Query_co]):
     """Protocol for repository provides ORM model query."""
 
-    def get_query(self, filter: FilterData_contra) -> Query_cov:
+    def get_query(self, filter: FilterData_contra) -> Query_co:
         """Fetch model."""
