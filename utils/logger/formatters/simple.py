d83b633b22d9d4bd958bb22e1f3d73fad6dfce0b
Sergei Mikurov <454004@mail.ru>
2026-04-26 00:45:18 +0500

    perf(utils/logger): Add log separation

--- utils/logger/formatters/simple.py
+++ utils/logger/formatters/simple.py
@@ -9,12 +9,16 @@ class SimpleSQLFormatter(logging.Formatter):
     """SQL formatter."""
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format."""
-        if hasattr(record, 'sql'):
-            sql = record.sql
-            formated: str = sqlparse.format(
-                sql, reindent=True, keyword_case='upper'
-            )
+        """Format SQL query with timestamp."""
+        sql = getattr(record, 'sql', record.getMessage())
 
-            return formated
-        return ''
+        formatted_sql = sqlparse.format(
+            sql, reindent=True, keyword_case='upper'
+        )
+
+        asctime = self.formatTime(record, self.datefmt)
+
+        return (
+            f'{asctime} [{record.levelname}] {record.name}\n{formatted_sql}\n'
+            f'{"=" * 80}'
+        )
