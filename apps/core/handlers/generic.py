7a1af9818c693b9703f9c773e260b6aa3059090d
Sergei Mikurov <454004@mail.ru>
2026-04-30 21:28:10 +0500

    feat(core/handler): Inject auditor

--- apps/core/handlers/generic.py
+++ apps/core/handlers/generic.py
@@ -6,6 +6,8 @@ from apps.core.adapters.response.protocol import AdapterProtocol
 from apps.core.assemblers.protocol import AssemblerProtocol
 from apps.core.use_cases.protocol import UseCaseProtocol
 from apps.core.validators.request.protocol import RequestValidatorProtocol
+from utils.audit.impl import NullAuditor
+from utils.audit.protocol import AuditorProtocol
 from utils.logger.decorators import log_errors_to_file
 
 # External data
@@ -49,12 +51,14 @@ class RequestHandler(
         ],
         use_case: UseCaseProtocol[CommandData, DomainResult],
         adapter: AdapterProtocol[DomainResult, RequestContext, ResponseData],
+        auditor: AuditorProtocol | None = None,
     ) -> None:
         """Construct the handler."""
         self._validator = validator
         self._assembler = assembler
         self._use_case = use_case
         self._adapter = adapter
+        self._auditor = auditor or NullAuditor()
 
     @log_errors_to_file()
     def execute(
@@ -64,8 +68,39 @@ class RequestHandler(
         data: RequestData,
     ) -> ResponseData:
         """Execute."""
+        self._auditor.record(
+            'handler.start',
+            params=params,
+            context=context,
+            data=data,
+        )
+
         validated = self._validator.validate(data)
+        self._auditor.record(
+            'validation.ok',
+            obj=self._validator,
+            validated=validated,
+        )
+
         command = self._assembler.prepare(params, context, validated)
+        self._auditor.record(
+            'assembler.ok',
+            obj=self._assembler,
+            command=command,
+        )
+
         domain_result = self._use_case.execute(command)
+        self._auditor.record(
+            'use_case.ok',
+            obj=self._use_case,
+            domain_result=domain_result,
+        )
+
         adapted = self._adapter.to_response(domain_result, context)
+        self._auditor.record(
+            'handler.finish',
+            obj=self._adapter,
+            adapted=adapted,
+        )
+
         return adapted
