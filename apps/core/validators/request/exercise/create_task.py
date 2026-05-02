6cf2860600a1f056e1118b4ab71f9aaa71a1aff3
Sergei Mikurov <454004@mail.ru>
2026-05-03 00:21:30 +0500

    fix(core/validator): Fix exercise action request validator

--- apps/core/validators/request/exercise/create_task.py
+++ apps/core/validators/request/exercise/create_task.py
@@ -1,44 +1,45 @@
-"""Exercise task action validator."""
+"""Exercise task action request data validator."""
 
-import logging
+from __future__ import annotations
 
-from pydantic import ValidationError
+from typing import TYPE_CHECKING, Callable
 
 from apps.core.handlers.protocol import RequestDataProtocol
 from contracts.entity.domain.general import ActionTyped, HasAction
 from contracts.enums.exercise import ExerciseAction
-from contracts.schemas.request.exercise import ExerciseRequestDTO
 from utils.audit.base import BaseAuditable
 
 from ..abstract import AbstractRequestValidator
 
-log = logging.getLogger(__name__)
+if TYPE_CHECKING:
+    from utils.audit.protocol import AuditorProtocol
 
-type _DataT = RequestDataProtocol[ActionTyped]
-type _ValidatedT = HasAction[ExerciseAction]
+    type RegistryT = dict[
+        ExerciseAction,
+        Callable[..., HasAction[ExerciseAction]],
+    ]
+
+type DataT = RequestDataProtocol[ActionTyped[ExerciseAction]]
+type ValidatedT = HasAction[ExerciseAction]
 
 
 class CreateExerciseTaskValidator(
     BaseAuditable,
-    AbstractRequestValidator[_DataT, _ValidatedT],
+    AbstractRequestValidator[DataT, ValidatedT],
 ):
-    """Request data action validator."""
+    """Exercise task action request data validator."""
 
-    def validate(self, data: _DataT) -> _ValidatedT:
+    def __init__(
+        self,
+        schema_class_registry: RegistryT,
+        name: str | None = None,
+        auditor: AuditorProtocol | None = None,
+    ) -> None:
+        """Construct the validator."""
+        super().__init__(name, auditor)
+        self._schema_class_registry = schema_class_registry
+
+    def validate(self, data: DataT) -> ValidatedT:
         """Validate the request data."""
-        try:
-            return ExerciseRequestDTO(
-                action=ExerciseAction(data.data['action']),
-            )
-        except ValidationError as exc:
-            log.error(
-                f'WEB request data validation error: {exc}\n'
-                f'Request data: {data!r}'
-            )
-            raise
-        except Exception as exc:
-            log.error(
-                f'WEB request data unexpected error: {exc}\n'
-                f'Request data: {data!r}'
-            )
-            raise
+        schema_cls = self._schema_class_registry[data.data['action']]
+        return schema_cls(**data.data)
