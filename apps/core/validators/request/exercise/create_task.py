0784b9da20b5840b7e0faca6828ffbbb809ec2d0
Sergei Mikurov <454004@mail.ru>
2026-05-02 22:35:38 +0500

    tmp: Refactor task validator

--- apps/core/validators/request/exercise/create_task.py
+++ apps/core/validators/request/exercise/create_task.py
@@ -1,14 +1,11 @@
-"""Create exercise task validator."""
+"""Exercise task action validator."""
 
 import logging
 
 from pydantic import ValidationError
 
 from apps.core.handlers.protocol import RequestDataProtocol
-from contracts.entity.domain.general import (
-    ActionTyped,
-    HasAction,
-)
+from contracts.entity.domain.general import ActionTyped, HasAction
 from contracts.enums.exercise import ExerciseAction
 from contracts.schemas.request.exercise import ExerciseRequestDTO
 from utils.audit.base import BaseAuditable
@@ -25,23 +22,23 @@ class CreateExerciseTaskValidator(
     BaseAuditable,
     AbstractRequestValidator[_DataT, _ValidatedT],
 ):
-    """Create exercise task WEB request data validator."""
+    """Request data action validator."""
 
     def validate(self, data: _DataT) -> _ValidatedT:
-        """Validate test exercise user's answer data."""
+        """Validate the request data."""
         try:
             return ExerciseRequestDTO(
                 action=ExerciseAction(data.data['action']),
             )
         except ValidationError as exc:
             log.error(
-                f'WEB request data parameters validation error: {exc}\n'
+                f'WEB request data validation error: {exc}\n'
                 f'Request data: {data!r}'
             )
             raise
         except Exception as exc:
             log.error(
-                f'WEB request data parameters unexpected error: {exc}\n'
+                f'WEB request data unexpected error: {exc}\n'
                 f'Request data: {data!r}'
             )
             raise
