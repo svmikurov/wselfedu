b359fe0ea7c21506f18df9fc5b79edeae475abcc
Sergei Mikurov <454004@mail.ru>
2026-05-03 00:32:20 +0500

    test: Update exercise request validator

--- tests/validator/exercise/test_test.py
+++ tests/validator/exercise/test_test.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 
 import pytest
 
@@ -11,19 +11,20 @@ from apps.core.validators.request.exercise.create_task import (
     CreateExerciseTaskValidator,
 )
 from contracts.enums.exercise import ExerciseAction
-from contracts.schemas.request.exercise import ExerciseRequestDTO
+from interfaces.schemas.validator.task import CreateTaskWebValidated
 
 if TYPE_CHECKING:
     from apps.core.handlers.protocol import RequestDataProtocol
     from apps.core.validators.request.protocol import RequestValidatorProtocol
-    from contracts.entity.domain.general import (
-        ActionTyped,
-        HasAction,
-    )
+    from contracts.entity.domain.general import ActionTyped, HasAction
 
-    type CreateDtoT = RequestDataProtocol[ActionTyped]
-    type ValidatorT = RequestValidatorProtocol[CreateDtoT, ValidatedT]
+    type CreateDtoT = RequestDataProtocol[ActionTyped[ExerciseAction]]
     type ValidatedT = HasAction[ExerciseAction]
+    type ValidatorT = RequestValidatorProtocol[CreateDtoT, ValidatedT]
+    type RegistryT = dict[
+        ExerciseAction,
+        Callable[..., HasAction[ExerciseAction]],
+    ]
 
 
 @pytest.fixture
@@ -32,21 +33,31 @@ def create_exercise_dto() -> CreateDtoT:
     # Request data DTO creates in view's method with GET request.
     return RequestData(
         data={
-            'action': 'create_task',
+            'action': ExerciseAction.CREATE_TASK,
         },
     )
 
 
 @pytest.fixture
-def validator() -> ValidatorT:
+def schema_registry() -> RegistryT:
+    """Provide exercise action request validator schema registry."""
+    return {
+        ExerciseAction.CREATE_TASK: CreateTaskWebValidated,
+    }
+
+
+@pytest.fixture
+def validator(
+    schema_registry: RegistryT,
+) -> ValidatorT:
     """Provide test exercise request validator."""
-    return CreateExerciseTaskValidator()
+    return CreateExerciseTaskValidator(schema_class_registry=schema_registry)
 
 
 @pytest.fixture
 def validated() -> ValidatedT:
     """Provide test exercise request validator."""
-    return ExerciseRequestDTO(
+    return CreateTaskWebValidated(
         action=ExerciseAction.CREATE_TASK,
     )
 
