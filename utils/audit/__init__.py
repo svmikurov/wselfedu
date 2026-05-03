076d7a52917787b7451fd7d5b41148c6e92a9e28
Sergei Mikurov <454004@mail.ru>
2026-05-03 17:52:07 +0500

    feat(utils/audit): Extend package export

--- utils/audit/__init__.py
+++ utils/audit/__init__.py
@@ -1,5 +1,14 @@
 """Audit utils."""
 
-__all__ = ('Auditor',)
+__all__ = (
+    # Auditor
+    'Auditor',
+    'AuditorProtocol',
+    # Auditable object
+    'Auditable',
+    'BaseAuditable',
+)
 
+from .base import BaseAuditable
 from .impl import Auditor
+from .protocol import Auditable, AuditorProtocol
