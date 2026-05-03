b73949bc818d53322a5175b9acb02b20b6d2bc39
Sergei Mikurov <454004@mail.ru>
2026-05-04 02:16:21 +0500

    test: Reorganize

--- tests/handler/test_regular_presentation.py
+++ tests/handler/test_regular_presentation.py
@@ -2,8 +2,8 @@
 
 import pytest
 
-from contracts.aliases import CandidatesAlias
 from di import MainContainer
+from interfaces.schemas.domain.exercise import TaskItem
 
 from .._types.handler import (
     HandlerT,
@@ -13,43 +13,60 @@ from .._types.handler import (
 )
 
 # =================================================
-# Tested handler
+# Fixtures
 # =================================================
 
 
 @pytest.fixture
-def regular_presentation_handler(
-    main_container: MainContainer,
-) -> HandlerT:
+def regular_presentation_handler(main_container: MainContainer) -> HandlerT:
     """Provide translation regular presentation exercise handler."""
     return (  # type: ignore
-        main_container.lang.handlers.process_regular_translation_test()  # type: ignore
+        main_container.lang.handlers.regular_translation_presentation()  # type: ignore
     )
+
+
+# =================================================
+# Tests
+# =================================================
+
+
+def test_handler_initialized(regular_presentation_handler: HandlerT) -> None:
+    """Test that presentation exercise handle initialized."""
+    assert regular_presentation_handler is not None
 
 
 @pytest.mark.django_db
 def test_create_new_case(
-    # Request parameters
+    translations: list[TaskItem],
     request_params: RequestParamsT,
     request_context: RequestContextT,
-    request_data_create_task: RequestDataT,
-    # Populate DB
-    translation_candidates_db: CandidatesAlias,
-    # Tested handler
+    request_data_create_task: RequestDataT,  # Create task request data
     regular_presentation_handler: HandlerT,
 ) -> None:
-    """Test that translations presentation exercise handler created."""
-    # Act
-    regular_presentation_handler.execute(
-        request_params,
-        request_context,
-        request_data_create_task,
+    """Test *create task* handler action completed successfully."""
+    assert (
+        regular_presentation_handler.execute(
+            request_params,
+            request_context,
+            request_data_create_task,
+        )
+    ) is not None
+
+
+@pytest.mark.django_db
+def test_update_progress(
+    translations: list[TaskItem],
+    null_request_params: RequestParamsT,
+    user_request_context: RequestContextT,
+    update_progress_request_data: RequestDataT,  # Update progress request data
+    regular_presentation_handler: HandlerT,
+) -> None:
+    """Test *update progress* handler action completed successfully."""
+    assert (
+        regular_presentation_handler.execute(
+            null_request_params,
+            user_request_context,
+            update_progress_request_data,
+        )
+        is not None
     )
-
-
-def test_handler_initialized(
-    regular_presentation_handler: HandlerT,
-) -> None:
-    """Test that presentation exercise handle initialized."""
-    # Assert
-    assert regular_presentation_handler is not None
--- tests/repository/test_di_candidates.py
+++ tests/repository/test_di_candidates.py
@@ -29,3 +29,40 @@ def test_fetch_resource_successfully(
     """Test that repository fetch resource called successfully."""
     # Act & assert
     assert repository.fetch(user, NullDTO()) == translations
+
+
+@pytest.mark.django_db
+def test_fetch_not_owner(
+    not_owner: Person,
+    repository: _Repository,
+    translations: _Translations,  # Populate DB
+) -> None:
+    """Fetch translations by not owner test."""
+    # Act
+    candidates = repository.fetch(not_owner, NullDTO())
+
+    # Assert
+    assert len(candidates) == 0
+
+
+@pytest.mark.django_db
+def test_candidate_interface(
+    user: Person,
+    repository: _Repository,
+    translations: _Translations,  # Populate DB
+) -> None:
+    """Has exercise candidates interface test."""
+    # Act
+    candidates = repository.fetch(user, NullDTO())
+
+    # Assert
+    candidate = candidates[0]
+
+    assert hasattr(candidate, 'define')
+    assert candidate.define is not None
+
+    assert hasattr(candidate, 'mean')
+    assert candidate.mean is not None
+
+    assert hasattr(candidate, 'progress_value')
+    assert candidate.progress_value is not None
