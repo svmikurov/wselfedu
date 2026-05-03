3f27fbf21c2ff1242dafe9e2c27262746b8f1823
Sergei Mikurov <454004@mail.ru>
2026-05-03 13:30:56 +0500

    test: Add translations fixture type alias

--- tests/_types/resource.py
+++ tests/_types/resource.py
@@ -1,7 +1,9 @@
 """Resource type aliases."""
 
-from typing import TypeAlias
+from typing import Iterable, TypeAlias
 
+from apps.lang.models import EnglishTranslation
 from contracts.entity.domain.exercise.fields import Candidates
 
 TranslationCandidates: TypeAlias = Candidates
+TranslationsT: TypeAlias = Iterable[EnglishTranslation]
