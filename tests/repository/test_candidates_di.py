62418da2104429d1edd3c634e0a917363def4e9e
Sergei Mikurov <454004@mail.ru>
2026-05-03 23:04:58 +0500

    test(repository): Add candidates partial test

---
+++ tests/repository/test_candidates_di.py
@@ -0,0 +1,47 @@
+"""Exercise candidates repository tests."""
+
+from typing import TypeAlias
+
+import pytest
+
+from apps.core.repositories.protocol import UserRepositoryProtocol
+from apps.users.models import Person
+from contracts.schemas.base import NullDTO
+from di import MainContainer
+from interfaces.schemas.domain.exercise import CandidateSchema
+
+_Translations: TypeAlias = list[CandidateSchema]
+_Repository: TypeAlias = UserRepositoryProtocol[object, _Translations]
+
+
+@pytest.fixture
+def repository(main_container: MainContainer) -> _Repository:
+    """Provide DI translation candidates fixture."""
+    return main_container.lang.repositories.translation_candidates()  # type: ignore
+
+
+def test_initialize(repository: _Repository) -> None:
+    """Test that repository initialized successfully."""
+    # Act & assert
+    assert repository is not None
+
+
+@pytest.mark.django_db
+def test_fetch_empty_successfully(
+    user: Person,
+    repository: _Repository,
+) -> None:
+    """Test that repository fetch empty called successfully."""
+    # Act & assert
+    assert repository.fetch(user, NullDTO()) is not None
+
+
+@pytest.mark.django_db
+def test_fetch_resource_successfully(
+    translations: _Translations,  # Populate DB
+    user: Person,
+    repository: _Repository,
+) -> None:
+    """Test that repository fetch resource called successfully."""
+    # Act & assert
+    assert repository.fetch(user, NullDTO()) == translations
