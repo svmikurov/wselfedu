9ab9efb642113a7353885425a49edc060f05e643
Sergei Mikurov <454004@mail.ru>
2026-05-06 01:04:44 +0500

    refactor(lang/view): Update presentation view

--- apps/lang/views/exercise/translation/presentation.py
+++ apps/lang/views/exercise/translation/presentation.py
@@ -7,11 +7,9 @@ from typing import TYPE_CHECKING
 from dependency_injector.wiring import Provide, inject
 
 from apps.core.views.exercise import ExercisePerformView
-from apps.core.views.exercise.base import IdeaExercisePerformView
-from apps.core.views.exercise.mixins import ExerciseLoopMixin
 from di import MainContainer
 
-from ._types import HandlerT, ResponseDtoT
+from ._types import HandlerT
 
 if TYPE_CHECKING:
     from django.http import HttpRequest, HttpResponseBase
@@ -21,31 +19,7 @@ __all__ = ('TranslationPresentationView',)
 HANDLERS = MainContainer.lang.handlers  # type: ignore[unused-ignore]
 
 
-class TranslationPresentationView(
-    ExerciseLoopMixin,
-    ExercisePerformView[HandlerT, ResponseDtoT],  # type: ignore
-):
-    """Regular translation test exercise performing view."""
-
-    TEMPLATE_PATH = 'lang/exercise/presentation/'
-    template_name = 'lang/exercise/presentation/index.html'
-
-    @inject
-    def dispatch(
-        self,
-        request: HttpRequest,
-        *args: object,
-        handler: HandlerT = Provide[
-            HANDLERS.regular_translation_presentation  # type: ignore[attr-defined]
-        ],
-        **kwargs: object,
-    ) -> HttpResponseBase:
-        """Inject request handler."""
-        self._handler = handler
-        return super().dispatch(request, *args, **kwargs)
-
-
-class IdeaTranslationPresentationView(IdeaExercisePerformView[HandlerT]):
+class TranslationPresentationView(ExercisePerformView[HandlerT]):
     """Regular translation test exercise performing view."""
 
     template_name = 'lang/exercise/presentation/index.html'
