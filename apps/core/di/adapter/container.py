7cb5a5b3de31435388f01e8d7afa8b8fbb194564
Sergei Mikurov <454004@mail.ru>
2026-05-05 21:57:09 +0500

    feat(core/di): Add response adapters DI container

---
+++ apps/core/di/adapter/container.py
@@ -0,0 +1,57 @@
+"""DI container for adapters."""
+
+from dependency_injector.containers import DeclarativeContainer
+from dependency_injector.providers import Dependency, Dict, Factory
+
+from apps.core.adapters.response import (
+    PresentationTaskWebAdapter,
+    ProcessExerciseAdapterStrategy,
+)
+from apps.core.adapters.response.null import NullResponseAdapter
+from contracts.enums import ExerciseStatus
+
+presentation_oob_htmls: list[str] = [
+    'core/exercise/presentation/task.html',
+]
+
+
+class ResponseAdaptersContainer(DeclarativeContainer):
+    """Core response adapters DI container."""
+
+    # ================================================
+    # External dependencies
+    # ================================================
+
+    auditor = Dependency()  # type: ignore
+
+    # ================================================
+    # Adapters
+    # ================================================
+
+    regular_presentation = Factory(
+        PresentationTaskWebAdapter,
+        oob_templates=presentation_oob_htmls,
+        name='Presentation response adapter',
+        auditor=auditor,
+    )
+    null_adapter = Factory(NullResponseAdapter)
+
+    # =============================================
+    # Presentation registries
+    # =============================================
+
+    presentation_registries = Dict(
+        {
+            ExerciseStatus.NEW_TASK: regular_presentation,
+            ExerciseStatus.UPDATED_PROGRESS: null_adapter,
+        },
+    )
+
+    # =============================================
+    # Presentation exercise strategy
+    # =============================================
+
+    presentation_strategy = Factory(
+        ProcessExerciseAdapterStrategy,
+        registry=presentation_registries,
+    )
