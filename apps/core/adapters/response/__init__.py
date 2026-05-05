afae0759afd6db42aa0c764e243245d77be7e25c
Sergei Mikurov <454004@mail.ru>
2026-05-05 21:50:37 +0500

    feat(core/adapter): Extend package export

--- apps/core/adapters/response/__init__.py
+++ apps/core/adapters/response/__init__.py
@@ -1,1 +1,14 @@
 """Response core adapters."""
+
+__all__ = (
+    # Null adapter
+    'NullResponseAdapter',
+    # Web adapters
+    'PresentationTaskWebAdapter',
+    # Adapter strategies
+    'ProcessExerciseAdapterStrategy',
+)
+
+from .exercise.presentation.web import PresentationTaskWebAdapter
+from .exercise.strategy import ProcessExerciseAdapterStrategy
+from .null import NullResponseAdapter
