09406b0ad17a4caedfd3f2473d8758cb05fa7bfc
Sergei Mikurov <454004@mail.ru>
2026-05-09 17:04:27 +0500

    test: Reorganize exercise tests

---
+++ tests/exercise/test/_types.py
@@ -0,0 +1,31 @@
+"""Exercise test types."""
+
+from typing import Callable
+
+from apps.core.handlers.protocol import RequestDataProtocol
+from apps.core.validators.request.protocol import RequestValidatorProtocol
+from contracts.entity.domain.general import HasAction
+from contracts.enums import ExerciseAction
+from interfaces.typed.exercise import TypedCheckTestAnswer, TypedCreateTask
+
+# Request data (protocols with generic typed dict)
+type CreateRequestDataT = RequestDataProtocol[TypedCreateTask]
+type CheckRequestDataT = RequestDataProtocol[TypedCheckTestAnswer]
+
+# Validated data (protocols)
+type ValidatedCreateT = HasAction[ExerciseAction]
+type ValidatedCheckT = HasAction[ExerciseAction]
+
+# Validator
+type RegistryT = dict[
+    ExerciseAction,
+    Callable[..., HasAction[ExerciseAction]],
+]
+type CreateValidatorT = RequestValidatorProtocol[
+    CreateRequestDataT,
+    ValidatedCreateT,
+]
+type CheckValidatorT = RequestValidatorProtocol[
+    CheckRequestDataT,
+    ValidatedCheckT,
+]
---
+++ tests/exercise/test/test_validator.py
@@ -0,0 +1,73 @@
+"""Test exercise request validator tests."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+import pytest
+
+from apps.core.validators.request.exercise import ExerciseRequestValidator
+from contracts.enums import ExerciseAction
+from interfaces.schemas.validator.task import (
+    ValidatedCheckTestAnswer,
+    ValidatedCreateTask,
+)
+
+if TYPE_CHECKING:
+    from apps.core.validators.request.protocol import RequestValidatorProtocol
+
+    from ._types import (
+        CheckRequestDataT,
+        CheckValidatorT,
+        CreateRequestDataT,
+        CreateValidatorT,
+        RegistryT,
+        ValidatedCheckT,
+        ValidatedCreateT,
+    )
+
+# =================================================
+# Fixtures
+# =================================================
+
+
+@pytest.fixture
+def schema_registry() -> RegistryT:
+    """Provide exercise action request validator schema registry."""
+    return {
+        ExerciseAction.CREATE_TASK: ValidatedCreateTask,
+        ExerciseAction.CHECK_ANSWER: ValidatedCheckTestAnswer,
+    }
+
+
+@pytest.fixture
+def validator(
+    schema_registry: RegistryT,
+) -> RequestValidatorProtocol[Any, Any]:
+    """Provide test exercise request validator."""
+    return ExerciseRequestValidator(schema_class_registry=schema_registry)
+
+
+# =================================================
+# Tests
+# =================================================
+
+
+def test_validate_create_action_data(
+    validator: CreateValidatorT,
+    create_request_data: CreateRequestDataT,
+    validated_create: ValidatedCreateT,
+) -> None:
+    """Test that create exercise action data validated."""
+    # Act & Assert
+    assert validator.validate(create_request_data) == validated_create
+
+
+def test_validate_check_action_data(
+    validator: CheckValidatorT,
+    check_request_data: CheckRequestDataT,
+    validated_check: ValidatedCheckT,
+) -> None:
+    """Test that create exercise action data validated."""
+    # Act & Assert
+    assert validator.validate(check_request_data) == validated_check
