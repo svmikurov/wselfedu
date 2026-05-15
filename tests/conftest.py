6692563e4a172d38ee516a3b8d587084889362d8
Sergei Mikurov <454004@mail.ru>
2026-05-15 21:14:34 +0500

    test: Add Dajngo ORM user mock

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 """Pytest configuration."""
 
+from unittest.mock import Mock
+
+import pytest
+
+from apps.users.models import Person
+
 pytest_plugins = [
     'tests.fixtures.db_user',
     'tests.fixtures.di',
@@ -8,3 +14,11 @@ pytest_plugins = [
     'tests.fixtures.exercise.lang.db.translations',
     'tests.fixtures.exercise.lang.no_db.params',
 ]
+
+
+@pytest.fixture
+def mock_user() -> Person:
+    """Provide user mock."""
+    mock = Mock(spec=Person)
+    mock.pk = 1
+    return mock
