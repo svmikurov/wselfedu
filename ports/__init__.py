efd2fb7a51f261cce5b4c5c1a1316d251afa5ad0
Sergei Mikurov <454004@mail.ru>
2026-05-17 02:32:37 +0500

    docs(port): Add docstring

--- ports/__init__.py
+++ ports/__init__.py
@@ -1,1 +1,9 @@
-"""Ports."""
+"""Ports.
+
+Temporary combines:
+    - base abstract classes
+    - contracts for DTO, implementation
+    - types for generic implementation
+    - interfaces for reqest, response
+    - shcema interfaces nad schemas for entity DTO, layer DTO
+"""
