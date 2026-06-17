5e9ae864f94260ebff4f42326a14e0580a00a08d
Sergei Mikurov <454004@mail.ru>
2026-06-17 22:44:19 +0500

    test(domain): Add check testing answer service

--- tests/domain/test_testing_service.py
+++ tests/domain/test_testing_service.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wse.domain.services import create_testing_task
+from wse.domain import services, values
 
 if TYPE_CHECKING:
     from wse.domain.protocols import UniqueLearnable
@@ -14,7 +14,7 @@ def test_create_testing_task(
     task_candidates: list[UniqueLearnable],
 ) -> None:
     # Act
-    task = create_testing_task(task_candidates)
+    task = services.create_testing_task(task_candidates)
 
     # Assert
     assert hasattr(task, 'question_text')
@@ -27,3 +27,42 @@ def test_create_testing_task(
     assert isinstance(option.option_value, int)
     assert hasattr(option, 'option_text')
     assert isinstance(option.option_text, str)
+
+
+def test_check_correct_user_answer(
+    task_candidates: list[UniqueLearnable],
+) -> None:
+    # Arrange
+    task = services.create_testing_task(task_candidates)
+    correct_answer = task.question_value
+    answer_spec = values.AnswerChecking(
+        question_value=correct_answer,
+        answer_value=correct_answer,
+    )
+
+    # Act
+    checking_result = services.check_testing_answer(answer_spec)
+
+    # Assert
+    assert checking_result.is_correct is True
+
+
+def test_check_wrong_user_answer(
+    task_candidates: list[UniqueLearnable],
+) -> None:
+    # Arrange
+    task = services.create_testing_task(task_candidates)
+    correct_answer = task.question_value
+    wrong_answer = (
+        correct_answer + 1 if correct_answer == 1 else correct_answer - 1
+    )
+    answer_spec = values.AnswerChecking(
+        question_value=correct_answer,
+        answer_value=wrong_answer,
+    )
+
+    # Act
+    checking_result = services.check_testing_answer(answer_spec)
+
+    # Assert
+    assert checking_result.is_correct is False
