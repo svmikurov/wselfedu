f2e4e95094c74ae0634bd8ab3b055c317016a51f
Sergei Mikurov <454004@mail.ru>
2026-06-17 23:37:49 +0500

    test(domain): Fix testing service signature

--- tests/domain/test_testing_service.py
+++ tests/domain/test_testing_service.py
@@ -7,14 +7,14 @@ from typing import TYPE_CHECKING
 from wse.domain import services, values
 
 if TYPE_CHECKING:
-    from wse.domain.protocols import UniqueLearnable
+    from wse.domain.protocols import HasLearnables, UniqueLearnable
 
 
 def test_create_testing_task(
-    task_candidates: list[UniqueLearnable],
+    create_testing_spec: HasLearnables[list[UniqueLearnable]],
 ) -> None:
     # Act
-    task = services.create_testing_task(task_candidates)
+    task = services.create_testing_task(create_testing_spec)
 
     # Assert
     assert hasattr(task, 'question_text')
@@ -30,10 +30,10 @@ def test_create_testing_task(
 
 
 def test_check_correct_user_answer(
-    task_candidates: list[UniqueLearnable],
+    create_testing_spec: HasLearnables[list[UniqueLearnable]],
 ) -> None:
     # Arrange
-    task = services.create_testing_task(task_candidates)
+    task = services.create_testing_task(create_testing_spec)
     correct_answer = task.question_value
     answer_spec = values.AnswerChecking(
         question_value=correct_answer,
@@ -48,13 +48,13 @@ def test_check_correct_user_answer(
 
 
 def test_check_wrong_user_answer(
-    task_candidates: list[UniqueLearnable],
+    create_testing_spec: HasLearnables[list[UniqueLearnable]],
 ) -> None:
     # Arrange
-    task = services.create_testing_task(task_candidates)
+    task = services.create_testing_task(create_testing_spec)
     correct_answer = task.question_value
     wrong_answer = (
-        correct_answer + 1 if correct_answer == 1 else correct_answer - 1
+        correct_answer - 1 if correct_answer > 1 else correct_answer + 1
     )
     answer_spec = values.AnswerChecking(
         question_value=correct_answer,
