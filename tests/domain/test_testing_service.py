5be85bbe3d8c14fb3a2365bc8aafd5e1478e2ec4
Sergei Mikurov <454004@mail.ru>
2026-06-18 21:06:07 +0500

    test(domain): Update testing services

--- tests/domain/test_testing_service.py
+++ tests/domain/test_testing_service.py
@@ -2,27 +2,57 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TypeAlias
+
+import pytest
 
 from wse.domain import services, values
 
 if TYPE_CHECKING:
-    from wse.domain.protocols import HasLearnables, UniqueLearnable
+    from wse.domain.protocols import (
+        AnswerCheckable,
+        CheckableOption,
+        ExerciseCreatable,
+        HasIsCorrect,
+        HasLearnables,
+        Testable,
+        UniqueLearnable,
+    )
+
+    SpecificationT: TypeAlias = HasLearnables[list[UniqueLearnable]]
+    CreateServiceT: TypeAlias = ExerciseCreatable[SpecificationT, Testable]
+    CheckServiceT: TypeAlias = AnswerCheckable[CheckableOption, HasIsCorrect]
 
 
-def test_create_testing_task(
-    create_testing_spec: HasLearnables[list[UniqueLearnable]],
-) -> None:
-    # Act
-    task = services.create_testing_task(create_testing_spec)
+@pytest.fixture
+def create_testing_service() -> CreateServiceT:
+    """Provide a create testing service."""
+    return services.CreateTestingService()
 
+
+@pytest.fixture
+def check_answer_service() -> CheckServiceT:
+    """Provide a check testing  answer service."""
+    return services.CheckTestingService()
+
+
+@pytest.fixture
+def testing_task(
+    create_testing_service: CreateServiceT,
+    create_testing_spec: SpecificationT,
+) -> Testable:
+    """Provide a testing task."""
+    return create_testing_service.create(create_testing_spec)
+
+
+def test_create_testing_task(testing_task: Testable) -> None:
     # Assert
-    assert hasattr(task, 'question_text')
-    assert isinstance(task.question_text, str)
-    assert hasattr(task, 'options')
-    assert isinstance(task.options, list)
+    assert hasattr(testing_task, 'question_text')
+    assert isinstance(testing_task.question_text, str)
+    assert hasattr(testing_task, 'options')
+    assert isinstance(testing_task.options, list)
 
-    option = task.options[0]
+    option = testing_task.options[0]
     assert hasattr(option, 'option_value')
     assert isinstance(option.option_value, int)
     assert hasattr(option, 'option_text')
@@ -30,29 +60,29 @@ def test_create_testing_task(
 
 
 def test_check_correct_user_answer(
-    create_testing_spec: HasLearnables[list[UniqueLearnable]],
+    testing_task: Testable,
+    check_answer_service: CheckServiceT,
 ) -> None:
     # Arrange
-    task = services.create_testing_task(create_testing_spec)
-    correct_answer = task.question_value
+    correct_answer = testing_task.question_value
     answer_spec = values.AnswerChecking(
         question_value=correct_answer,
         answer_value=correct_answer,
     )
 
     # Act
-    checking_result = services.check_testing_answer(answer_spec)
+    checking_result = check_answer_service.check(answer_spec)
 
     # Assert
     assert checking_result.is_correct is True
 
 
 def test_check_wrong_user_answer(
-    create_testing_spec: HasLearnables[list[UniqueLearnable]],
+    testing_task: Testable,
+    check_answer_service: CheckServiceT,
 ) -> None:
     # Arrange
-    task = services.create_testing_task(create_testing_spec)
-    correct_answer = task.question_value
+    correct_answer = testing_task.question_value
     wrong_answer = (
         correct_answer - 1 if correct_answer > 1 else correct_answer + 1
     )
@@ -62,7 +92,7 @@ def test_check_wrong_user_answer(
     )
 
     # Act
-    checking_result = services.check_testing_answer(answer_spec)
+    checking_result = check_answer_service.check(answer_spec)
 
     # Assert
     assert checking_result.is_correct is False
