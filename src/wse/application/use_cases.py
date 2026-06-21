287d0ab57e90204a1400ae1dc3a492d938262efb
Sergei Mikurov <454004@mail.ru>
2026-06-21 10:59:55 +0500

    feat(app): Add a check testing answer use case

--- src/wse/application/use_cases.py
+++ src/wse/application/use_cases.py
@@ -2,45 +2,79 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeAlias
+from typing import TYPE_CHECKING
 
 from wse.domain import values
 from wse.domain.protocols import Testable
 
 from . import dto
-from .abstract import AbstractCreateTaskUseCase
-from .protocols import CerateTestingCommandProto, TaskProto
+from .abstract import AbstractUseCase
+from .protocols import (
+    CerateTestingCommandProto,
+    CheckResultDtoProto,
+    CheckTestingCommandProto,
+    TaskDtoProto,
+)
 
 if TYPE_CHECKING:
     from wse.domain.protocols import (
+        AnswerCheckable,
+        CheckableOption,
         ExerciseCreatable,
         HasLearnables,
         Repository,
         UniqueLearnable,
     )
 
-    ServiceT: TypeAlias = ExerciseCreatable[
-        HasLearnables[list[UniqueLearnable]],
-        Testable,
-    ]
-
 
 class CreateTestingUseCase(
-    AbstractCreateTaskUseCase[CerateTestingCommandProto, TaskProto[Testable]]
+    AbstractUseCase[CerateTestingCommandProto, TaskDtoProto[Testable]]
 ):
     """Use case for testing task creation."""
 
     def __init__(
         self,
-        repo: Repository,
-        service: ServiceT,
+        repo: Repository[int, UniqueLearnable],
+        service: ExerciseCreatable[
+            HasLearnables[list[UniqueLearnable]],
+            Testable,
+        ],
     ) -> None:
         self._repo = repo
         self._service = service
 
-    def execute(self, cmd: CerateTestingCommandProto) -> TaskProto[Testable]:
+    def execute(
+        self, cmd: CerateTestingCommandProto
+    ) -> TaskDtoProto[Testable]:
         """Create the testing task."""
         learnables = self._repo.list()
+        # TODO: Inject a specification builder
         spec = values.TaskCreating(learnables)
         task = self._service.create(spec)
-        return dto.Task(task)
+        # TODO: Inject a DTO mapper
+        return dto.Task(task=task, session_id=cmd.session_id)
+
+
+class CheckTestingUseCase(
+    AbstractUseCase[CheckTestingCommandProto, CheckResultDtoProto]
+):
+    """Use case for testing answer check."""
+
+    def __init__(
+        self,
+        repo: Repository[str, TaskDtoProto[Testable]],
+        service: AnswerCheckable[CheckableOption, CheckResultDtoProto],
+    ) -> None:
+        self._repo = repo
+        self._service = service
+
+    def execute(self, cmd: CheckTestingCommandProto) -> CheckResultDtoProto:
+        """Create the testing task."""
+        task = self._repo.get(cmd.session_id)
+        spec = values.AnswerChecking(
+            question_value=task.task.question_value,
+            answer_value=cmd.answer_value,
+        )
+        result = self._service.check(spec)
+        # FIXME: Fix type ignore
+        return result  # type: ignore[no-any-return]
