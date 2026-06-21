64ca52b2bb26f5d9845d050161c5c382d0b996b7
Sergei Mikurov <454004@mail.ru>
2026-06-21 11:10:25 +0500

    test(app): Extend task check answer test

--- tests/application/test_testing_uc.py
+++ tests/application/test_testing_uc.py
@@ -11,50 +11,113 @@ from wse.domain import services
 from wse.infrastructure import repository
 
 if TYPE_CHECKING:
-    from wse.application.protocols import CerateTestingCommandProto, Executable
+    from wse.application.protocols import (
+        CerateTestingCommandProto,
+        CheckResultDtoProto,
+        CheckTestingCommandProto,
+        Executable,
+        TaskDtoProto,
+    )
     from wse.domain.protocols import (
+        AnswerCheckable,
         ExerciseCreatable,
+        HasIdentifier,
+        HasSessionIdentifier,
         Repository,
+        Testable,
         UniqueLearnable,
     )
 
 
+###################################################
+# Fixtures
+###################################################
+
+
 @pytest.fixture
-def repo(learnables: list[UniqueLearnable]) -> Repository:
-    """Provide learnables repository."""
-    repo = repository.InMemoryRepository()
+def learnable_repo(
+    learnables: list[UniqueLearnable],
+) -> Repository[int, HasIdentifier]:
+    """Provide a learnables repository."""
+    repo = repository.InMemoryLearnableRepository()
     for item in learnables:
         repo.add(item)
     return repo
 
 
 @pytest.fixture
-def service() -> ExerciseCreatable:
-    """Provide a create testing domain service."""
+def testable_repo() -> Repository[str, HasSessionIdentifier]:
+    """Provide a testable repository."""
+    return repository.InMemoryTaskRepository()
+
+
+@pytest.fixture
+def create_service() -> ExerciseCreatable:
+    """Provide a create testing task domain service."""
     return services.CreateTestingService()
 
 
 @pytest.fixture
-def create_use_case(
-    repo: Repository,
-    service: ExerciseCreatable,
-) -> Executable:
-    """Provide a create testing task use case."""
-    return use_cases.CreateTestingUseCase(repo, service)
+def check_service() -> AnswerCheckable:
+    """Provide a check testing answer domain service."""
+    return services.CheckTestingService()
 
 
 @pytest.fixture
-def create_cmd() -> CerateTestingCommandProto:
-    """Provide a create testing task command."""
-    return commands.CreateTestingTask()
+def create_use_case(
+    learnable_repo: Repository[int, UniqueLearnable],
+    create_service: ExerciseCreatable,
+) -> Executable[CerateTestingCommandProto, TaskDtoProto[Testable]]:
+    """Provide a create testing task use case."""
+    return use_cases.CreateTestingUseCase(learnable_repo, create_service)
 
 
-def test_create_testing_task(
-    create_use_case: Executable,
-    create_cmd: CerateTestingCommandProto,
-) -> None:
-    # Act
-    task = create_use_case.execute(create_cmd)
+@pytest.fixture
+def check_use_case(
+    testable_repo: Repository[str, TaskDtoProto[Testable]],
+    check_service: AnswerCheckable,
+) -> Executable[CheckTestingCommandProto, CheckResultDtoProto]:
+    """Provide a create testing task use case."""
+    return use_cases.CheckTestingUseCase(testable_repo, check_service)
 
+
+@pytest.fixture
+def task(create_use_case: Executable) -> TaskDtoProto[Testable]:
+    """Provide a testing task."""
+    create_cmd = commands.CreateTestingTask(
+        session_id='test session',
+    )
+    return create_use_case.execute(cmd=create_cmd)
+
+
+###################################################
+# Tests
+###################################################
+
+
+def test_create_testing_task(task: TaskDtoProto[Testable]) -> None:
     # Assert
     assert task is not None
+    assert task.task is not None
+    assert isinstance(task.session_id, str)
+
+
+def test_check_testing_answer(
+    task: TaskDtoProto[Testable],
+    testable_repo: Repository[str, HasSessionIdentifier],
+    check_use_case: Executable,
+) -> None:
+    # Arrange
+    correct_answer = task.task.question_value
+    check_cmd = commands.CheckTestingAnswer(
+        session_id=task.session_id,
+        answer_value=correct_answer,
+    )
+    testable_repo.add(task)
+
+    # Act
+    result = check_use_case.execute(check_cmd)
+
+    # Assert
+    assert result is not None
+    assert result.is_correct is True
