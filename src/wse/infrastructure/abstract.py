c6c6a801d4ae13a346143719be4bddfbe9f1b7a2
Sergei Mikurov <454004@mail.ru>
2026-06-21 11:05:30 +0500

    refactor(infra): Extract in memory abstract repo

--- src/wse/infrastructure/abstract.py
+++ src/wse/infrastructure/abstract.py
@@ -1,22 +1,47 @@
 """Abstract base class for infrastructure interface."""
 
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, override
 
-T = TypeVar('T')
+ItemT = TypeVar('ItemT')
+KeyT = TypeVar('KeyT')
 
 
-class AbstractRepository(ABC, Generic[T]):
+class AbstractRepository(ABC, Generic[KeyT, ItemT]):
     """ABC for repository."""
 
     @abstractmethod
-    def add(self, item: T) -> None:
+    def add(self, item: ItemT) -> None:
         """Add item."""
 
     @abstractmethod
-    def get(self, key: str) -> T:
+    def get(self, key: KeyT) -> ItemT:
         """Get item."""
 
     @abstractmethod
-    def list(self) -> list[T]:
+    def list(self) -> list[ItemT]:
         """Get items."""
+
+
+class AbstractInMemoryRepository(
+    AbstractRepository[KeyT, ItemT], Generic[KeyT, ItemT]
+):
+    """ABC for in memory repository."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._items: set[ItemT] = set()
+
+    @override
+    def add(self, item: ItemT) -> None:
+        """Add item."""
+        self._items.add(item)
+
+    @override
+    def list(self) -> list[ItemT]:
+        """Get items."""
+        return list(self._items)
+
+    @abstractmethod
+    def get(self, key: KeyT) -> ItemT:
+        """Get item."""
