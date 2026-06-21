3864015248fb7e9cd1c805c32243ed208b98968f
Sergei Mikurov <454004@mail.ru>
2026-06-21 10:55:35 +0500

    feat(app): Implement a check testing answer cmd

--- src/wse/application/commands.py
+++ src/wse/application/commands.py
@@ -1,5 +1,18 @@
 """Commands."""
 
+from dataclasses import dataclass
 
+
+@dataclass(frozen=True, slots=True)
 class CreateTestingTask:
-    """Create a testing task."""
+    """Command for create a testing task."""
+
+    session_id: str
+
+
+@dataclass(frozen=True, slots=True)
+class CheckTestingAnswer:
+    """Command for check a testing answer."""
+
+    session_id: str
+    answer_value: int
