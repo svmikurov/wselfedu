b503dc3fb5b9c645d47a4f3f0bc8a7561c627ce8
Sergei Mikurov <454004@mail.ru>
2026-06-25 23:17:11 +0500

    test(browser): Apply test dependency factory

--- tests/browser/test_exercise_page.py
+++ tests/browser/test_exercise_page.py
@@ -5,11 +5,8 @@ from playwright.sync_api import expect
 from tests.browser.pom.page.testing_exercise import TestingExercisePage
 from tests.factories.mock import (
     create_learnable_repo_mock,
-    create_mock_task_repo,
 )
 from tests.factories.model import (
-    create_testing_task,
-    create_testing_task_dto,
     get_learnables,
 )
 from wse.di.application import ApplicationContainer
@@ -23,24 +20,15 @@ class TestTestingExercisePage(BaseTest):
     def setUp(self) -> None:
         super().setUp()
         self.page = TestingExercisePage(self._page)
+        self.container = ApplicationContainer()
+        self.repositories = self.container.repositories
+        self.mock_learnable_repo = create_learnable_repo_mock(get_learnables())
 
     def test_page_have_question_text(self) -> None:
-        # Arrange
-        container = ApplicationContainer()
-
-        learnables = get_learnables()
-        task = create_testing_task(learnables)
-        task_do = create_testing_task_dto(task)
-
-        mock_learnable_repo = create_learnable_repo_mock(learnables)
-        mock_task_repo = create_mock_task_repo(task_do)
-
         # Act
-        with (
-            container.repositories.learnable.override(mock_learnable_repo),
-            container.repositories.task.override(mock_task_repo),
-        ):
+        with self.repositories.learnable.override(self.mock_learnable_repo):
             self.page.open()
 
+            # Assert
             expect(self.page.question_text).to_be_visible()
-            expect(self.page.question_text).to_have_text('Question text')
+            expect(self.page.question_text).to_have_text('define')
