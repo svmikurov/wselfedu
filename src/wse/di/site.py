a068cf5816d81f78030fd75c9dff639a4e9e3f46
Sergei Mikurov <454004@mail.ru>
2026-06-26 00:45:34 +0500

    feat(di/site): Add use cases

--- src/wse/di/site.py
+++ src/wse/di/site.py
@@ -5,14 +5,21 @@ instance.
 """
 
 from dependency_injector.containers import DeclarativeContainer
-from dependency_injector.providers import Factory
+from dependency_injector.providers import Container, Factory
 
 from wse.site.handlers import ExerciseHandler
+
+from . import application
 
 
 class DjangoSiteContainer(DeclarativeContainer):
     """Django site entrypoint DI container."""
 
+    use_cases = Container(
+        application.ApplicationContainer,
+    )
+
     testing = Factory(
         ExerciseHandler,
+        use_case=use_cases.create_testing,
     )
