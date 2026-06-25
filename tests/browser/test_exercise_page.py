3a57178a57efae3487201da5390b1da22438b413
Sergei Mikurov <454004@mail.ru>
2026-06-26 00:42:40 +0500

    test(browser): Mock learnable repo

--- tests/browser/test_exercise_page.py
+++ tests/browser/test_exercise_page.py
@@ -3,13 +3,9 @@
 from playwright.sync_api import expect
 
 from tests.browser.pom.page.testing_exercise import TestingExercisePage
-from tests.factories.mock import (
-    create_learnable_repo_mock,
-)
-from tests.factories.model import (
-    get_learnables,
-)
-from wse.di.application import ApplicationContainer
+from tests.factories.mock import create_learnable_repo_mock
+from tests.factories.model import get_learnables
+from wse.di import django_site_container
 
 from .pom.test.base import BaseTest
 
@@ -20,15 +16,15 @@ class TestTestingExercisePage(BaseTest):
     def setUp(self) -> None:
         super().setUp()
         self.page = TestingExercisePage(self._page)
-        self.container = ApplicationContainer()
-        self.repositories = self.container.repositories
         self.mock_learnable_repo = create_learnable_repo_mock(get_learnables())
 
     def test_page_have_question_text(self) -> None:
         # Act
-        with self.repositories.learnable.override(self.mock_learnable_repo):
+        with django_site_container.use_cases.repositories.learnable.override(
+            self.mock_learnable_repo
+        ):
             self.page.open()
 
             # Assert
             expect(self.page.question_text).to_be_visible()
-            expect(self.page.question_text).to_have_text('define')
+            expect(self.page.question_text).to_contain_text('define')
