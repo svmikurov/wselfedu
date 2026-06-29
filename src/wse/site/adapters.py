04d43c792df8a7edfb867e6e1b6763fbd5e79d16
Sergei Mikurov <454004@mail.ru>
2026-06-29 22:41:31 +0500

    feat(site/adapter): Add testing task adapter

--- src/wse/site/adapters.py
+++ src/wse/site/adapters.py
@@ -1,14 +1,39 @@
 """Application layer request handler result adapter."""
 
+from wse.application.protocols import HasTask
+from wse.domain.protocols import Testable
+
+from . import dtos
 from .abstract import AbstractAdapter
-from .protocols import NullProto
+from .context_contract import CreateTestingTaskContext
+from .protocols import HasContext, NullProto
 
 
 class NullAdapter(
     AbstractAdapter[NullProto, NullProto, NullProto],
 ):
-    """Null validator."""
+    """Null adapter."""
 
     def to_response(self, source: NullProto, context: NullProto) -> NullProto:
         """Return original data."""
         return source
+
+
+class CreateTestingAdapter(
+    AbstractAdapter[
+        HasTask[Testable],
+        NullProto,
+        HasContext[CreateTestingTaskContext],
+    ],
+):
+    """Adapter for create testing task web response context."""
+
+    def to_response(
+        self,
+        source: HasTask[Testable],
+        context: NullProto,
+    ) -> HasContext[CreateTestingTaskContext]:
+        """Adapt for web response context."""
+        return dtos.ResponseDto(
+            context={'question_text': source.task.question_text},
+        )
