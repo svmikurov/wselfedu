1e7bf44cc9b04e32b427b3a9fc0930a6ec59cda5
Sergei Mikurov <454004@mail.ru>
2026-06-29 21:35:09 +0500

    feat(site): Add inerface for request handler deps

--- src/wse/site/protocols.py
+++ src/wse/site/protocols.py
@@ -2,14 +2,27 @@
 
 from typing import Any, Protocol, TypeVar
 
+T_contra = TypeVar('T_contra', contravariant=True)
 T_cov = TypeVar('T_cov', covariant=True)
 
-Context_cov = TypeVar('Context_cov', covariant=True)
-Data_cov = TypeVar('Data_cov', covariant=True)
+RequestParams_contra = TypeVar('RequestParams_contra', contravariant=True)
+RequestContext_cov = TypeVar('RequestContext_cov', covariant=True)
+RequestContext_contra = TypeVar('RequestContext_contra', contravariant=True)
+RequestData_cov = TypeVar('RequestData_cov', covariant=True)
+RequestData_contra = TypeVar('RequestData_contra', contravariant=True)
+
+Validated_cov = TypeVar('Validated_cov', covariant=True)
+Validated_contra = TypeVar('Validated_contra', contravariant=True)
+Command_cov = TypeVar('Command_cov', covariant=True)
+Command_contra = TypeVar('Command_contra', contravariant=True)
+Result_cov = TypeVar('Result_cov', covariant=True)
+Result_contra = TypeVar('Result_contra', contravariant=True)
+
+Adapted_cov = TypeVar('Adapted_cov', covariant=True)
 
 
 ###################################################
-# Components
+# DTO components
 ###################################################
 
 
@@ -51,12 +64,52 @@ class HtmlResponsible(
 
 
 ###################################################
-# Compositions
+# DTO compositions
 ###################################################
 
 
 class SimpleRequestParamsProto(
     HasQuery[NullProto],
-    HasContext[Context_cov],
-    HasData[Data_cov],
+    HasContext[RequestContext_cov],
+    HasData[RequestData_cov],
 ): ...
+
+
+###################################################
+# Request handling
+###################################################
+
+
+class Validatable(
+    Protocol[
+        RequestData_contra,
+        Validated_cov,
+    ]
+):
+    def validate(self, data: RequestData_contra) -> Validated_cov: ...
+
+
+class Preparable(
+    Protocol[
+        RequestParams_contra,
+        RequestContext_contra,
+        Validated_contra,
+        Command_cov,
+    ]
+):
+    def prepare(
+        self,
+        params: RequestParams_contra,
+        context: RequestContext_contra,
+        data: Validated_contra,
+    ) -> Command_cov: ...
+
+
+class ResponseAdaptable(
+    Protocol[Result_contra, RequestData_contra, Adapted_cov],
+):
+    def to_response(
+        self,
+        source: Result_contra,
+        context: RequestData_contra,
+    ) -> Adapted_cov: ...
