3b1b6ec1e2c9da4fa818364f55337271bbafd113
Sergei Mikurov <454004@mail.ru>
2026-06-29 22:45:06 +0500

    feat(site/assembler): Add create testing command

--- src/wse/site/assemblers.py
+++ src/wse/site/assemblers.py
@@ -1,17 +1,45 @@
 """Application layer assemblers."""
 
+from typing import override
+
+from wse.application.commands import CreateTestingTask
+
 from .abstract import AbstractAssembler
 from .dtos import NullDTO
-from .protocols import NullProto
+from .protocols import HasSessionIdentifier, NullProto
 
 
 class NullAssembler(
     AbstractAssembler[NullProto, NullProto, NullProto, NullProto],
 ):
-    """Null validator."""
+    """Null assembler."""
 
+    @override
     def prepare(
         self, params: NullProto, context: NullProto, data: NullProto
     ) -> NullProto:
         """Return null command."""
         return NullDTO()
+
+
+class CreateTestingTaskAssembler(
+    AbstractAssembler[
+        NullProto,
+        HasSessionIdentifier,
+        NullProto,
+        HasSessionIdentifier,
+    ],
+):
+    """Assembler for create testing exercise task command."""
+
+    @override
+    def prepare(
+        self,
+        params: NullProto,
+        context: HasSessionIdentifier,
+        validated: NullProto,
+    ) -> HasSessionIdentifier:
+        """Return null command."""
+        return CreateTestingTask(
+            session_id=context.session_id,
+        )
