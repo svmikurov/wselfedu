2cd32c184ec5319688505cdaa9c7d2e3bc73e6a1
Sergei Mikurov <454004@mail.ru>
2026-07-08 22:12:31 +0500

    test(app/unit): Add exercise use case

---
+++ tests/application/unit/test_exercise.py
@@ -0,0 +1,11 @@
+"""Exercise use case test."""
+
+import pytest
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_should_advance_to_next_question_on_correct_answer() -> None: ...
+
+
+@pytest.mark.skip(reason='Not implemented yet')
+def test_should_show_explanation_on_incorrect_answer() -> None: ...
