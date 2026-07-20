477e6f0b3579dc07d70e9d5cd36f590e26bb7a46
Sergei Mikurov <454004@mail.ru>
2026-07-20 23:26:06 +0500

    feat(app): Add abstract use case

---
+++ src/wse/app/abstract.py
@@ -0,0 +1,15 @@
+"""Abstract base classes for application layer."""
+
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+CommandT = TypeVar('CommandT')
+ResultT = TypeVar('ResultT')
+
+
+class AbstractUseCase(ABC, Generic[CommandT, ResultT]):
+    """ABC for use case."""
+
+    @abstractmethod
+    def execute(self, cmd: CommandT) -> ResultT:
+        """Execute command."""
