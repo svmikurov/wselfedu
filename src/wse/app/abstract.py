0d9d900b5aafa328f63b5e458deb8012f458c13b
Sergei Mikurov <454004@mail.ru>
2026-07-27 23:08:20 +0500

    feat(app): Add ABC for result collector

--- src/wse/app/abstract.py
+++ src/wse/app/abstract.py
@@ -3,13 +3,36 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
+from wse.domain.events import Event
+
 CommandT = TypeVar('CommandT')
-ResultT = TypeVar('ResultT')
+UseCaseResultT = TypeVar('UseCaseResultT')
+
+# HACK: Update handler result type after result definition refactor
+HandlerResultT = TypeVar('HandlerResultT')
 
 
-class AbstractUseCase(ABC, Generic[CommandT, ResultT]):
+class AbstractUseCase(
+    ABC,
+    Generic[CommandT, UseCaseResultT],
+):
     """ABC for use case."""
 
     @abstractmethod
-    def execute(self, cmd: CommandT) -> ResultT:
+    def execute(self, cmd: CommandT) -> UseCaseResultT:
         """Execute command."""
+
+
+class AbstractResultCollector(
+    ABC,
+    Generic[HandlerResultT, UseCaseResultT],
+):
+    """ABC for use case result collector."""
+
+    @abstractmethod
+    def collect(
+        self,
+        events: list[Event],
+        handler_results: list[HandlerResultT],
+    ) -> UseCaseResultT:
+        """Collect result with domain events and application results."""
