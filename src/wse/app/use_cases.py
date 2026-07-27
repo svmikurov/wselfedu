116b4d328f4ec91c61de0e515e26fb9c3c116573
Sergei Mikurov <454004@mail.ru>
2026-07-27 23:10:19 +0500

    feat(app): Add result collector to exercise use case

--- src/wse/app/use_cases.py
+++ src/wse/app/use_cases.py
@@ -1,29 +1,31 @@
 """Use cases."""
 
+from __future__ import annotations
+
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, TypeAlias
+
+from wse.domain.protocols import ExerciseProtocol, HasSessionIdentifier
 
 from .abstract import AbstractUseCase
 
+CommandT: TypeAlias = HasSessionIdentifier
+ResultT: TypeAlias = Any
+
 if TYPE_CHECKING:
-    from typing import Any, Callable, Type, TypeAlias
+    from typing import Callable, Type
 
     from wse.domain.abstract import AbstractExerciseFactory
     from wse.domain.events import Event
-    from wse.domain.protocols import (
-        EventSourcedAggregate,
-        HasSessionIdentifier,
-    )
 
-    CommandT: TypeAlias = HasSessionIdentifier
-    ResultT: TypeAlias = Any
-
-    AggregateT: TypeAlias = EventSourcedAggregate[CommandT, ResultT]
-    FactoryT: TypeAlias = AbstractExerciseFactory[AggregateT]
+    ExerciseT: TypeAlias = ExerciseProtocol[Any, Any, Any, Any, Any]
+    FactoryT: TypeAlias = AbstractExerciseFactory[ExerciseT]
+    ResultCollectorT: TypeAlias = Any
+    HandlerResultT: TypeAlias = Any
 
     HandlersT: TypeAlias = dict[
         Type[Event],
-        Callable[[AggregateT, Event], None],
+        Callable[[Event], ResultCollectorT],
     ]
 
 __all__ = ('ExerciseUseCase',)
@@ -31,33 +33,47 @@ __all__ = ('ExerciseUseCase',)
 log = logging.getLogger(__name__)
 
 
-class ExerciseUseCase(AbstractUseCase[CommandT, ResultT]):
+class ExerciseUseCase(
+    AbstractUseCase[CommandT, ResultT],
+):
     """Exercise use case."""
 
     def __init__(
         self,
         aggregate_factory: FactoryT,
-        handlers: HandlersT,
-    ) -> None:
+        handler_registry: HandlersT,
+        result_collector: ResultCollectorT,
+    ):
         self._aggregate_factory = aggregate_factory
-        self._handlers = handlers
+        self._handler_registry = handler_registry
+        self._result_collector = result_collector
+        self._events: list[Event] = []
+        self._handler_results: list[HandlerResultT] = []
 
     def execute(self, cmd: CommandT) -> ResultT:
-        """Execute exercise command and handle domain events."""
+        """Execute exercise command."""
         aggregate = self._aggregate_factory.create(cmd.session_id)
-        result = aggregate.handle(cmd)
+        aggregate.handle(cmd)
+
+        self._events.extend(aggregate.events)
 
         while aggregate.has_events():
             event = aggregate.pop_event()
-            handler = self._handlers.get(type(event))
+            handler = self._handler_registry.get(type(event))
 
             if handler:
                 try:
-                    handler(aggregate, event)
+                    result = handler(event)
+                    if result is not None:
+                        self._handler_results.append(result)
+
                 except Exception as e:
                     log.error(
                         f'Error in handler for {type(event).__name__}: {e}',
                         exc_info=True,
                     )
 
-        return result
+        return self._result_collector.collect(
+            self._events,
+            self._handler_results,
+        )
