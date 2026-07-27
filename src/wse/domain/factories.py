2c1d019b8c0867d1cd732269b7e3901145e2213a
Sergei Mikurov <454004@mail.ru>
2026-07-27 22:28:58 +0500

    refactor(domain): Update type hint

--- src/wse/domain/factories.py
+++ src/wse/domain/factories.py
@@ -5,28 +5,34 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, override
 
 from .abstract import AbstractExerciseFactory
-from .models import Exercise
+from .model import Exercise
 from .protocols import ExerciseProtocol
 
+ExerciseT = ExerciseProtocol[Any, Any, Any, Any, Any]
+
 if TYPE_CHECKING:
+    from typing import TypeAlias
+
     from .protocols import Executable
+
+    StrategyT: TypeAlias = Executable[Any, Any]
 
 __all__ = ('ExerciseFactory',)
 
 
-class ExerciseFactory(AbstractExerciseFactory[ExerciseProtocol[Any]]):
+class ExerciseFactory(AbstractExerciseFactory[ExerciseT]):
     """Exercise factory."""
 
     def __init__(
         self,
-        create_strategy: Executable[Any],
-        check_strategy: Executable[Any],
+        create_strategy: StrategyT,
+        check_strategy: StrategyT,
     ) -> None:
         self._create_strategy = create_strategy
         self._check_strategy = check_strategy
 
     @override
-    def create(self, session_id: str) -> ExerciseProtocol[Any]:
+    def create(self, session_id: str) -> ExerciseT:
         """Create an exercise."""
         return Exercise(
             create_strategy=self._create_strategy,
