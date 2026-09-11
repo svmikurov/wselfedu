288465d8ce58f3a421a513c6184a7e6e6ccfc5c1
Sergei Mikurov <454004@mail.ru>
2026-09-12 02:34:43 +0500

    chore(Makefile): Add & extract infra commands help

--- mk/infrastructure.mk
+++ mk/infrastructure.mk
@@ -1,6 +1,9 @@
 # =============================================================================
-# INFRASTURCTURE
+# INFRASTRUCTURE
 # =============================================================================
+
+# Docker
+# ~~~~~~
 
 run-django-orm:  ## Run Docker Compose with a build Django ORM admin
 	docker compose -f docker-compose.dev.django_orm.yml up --build
@@ -10,3 +13,27 @@ rebuild-django-orm:  ## Rebuild Docker Compose with a clean Django ORM admin
 	docker rmi wse-django-orm wse-postgres 2>/dev/null || true
 	docker builder prune -f || true
 	docker compose -f docker-compose.dev.django_orm.yml up --build
+
+# Socket
+# ~~~~~~
+
+run-socket-server:  ## Run socket server example
+	poetry run python3 src/wse/infrastructure/endpoints/server.py
+
+run-socket-client:  ## Run socket client example
+	poetry run python3 src/wse/infrastructure/endpoints/client.py
+
+help-infra:
+	@echo "=============================================="
+	@echo "Infrastructure layer development Make commands"
+	@echo "=============================================="
+	@echo ""
+	@echo "Docker commands"
+	@echo "~~~~~~~~~~~~~~~"
+	@echo "run-django-orm 		- Run docker compose with build Django ORM administration"
+	@echo "rebuild-django-orm	- Rebuild docker compose with clean Django ORM administration"
+	@echo ""
+	@echo "Socket commands"
+	@echo "~~~~~~~~~~~~~~~"
+	@echo "run-socket-server	- Run socket *server* example"
+	@echo "run-socket-client	- Run socket *client* example"
